9c16d5ab7bb151a9eb3d36645665e61cff3443ce
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-18 18:51:31 +0530

    Time: 7 ms (90.01%), Space: 12.5 MB (5.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>v;
-        for(int i = 0; i < nums.size(); i++)
+        // vector<int>v;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     for(int j = i + 1 ; j < nums.size();j++)
+        //     {
+        //         if(nums[i] + nums[j] == target)
+        //         {
+        //             v = {i,j};
+        //             break;
+        //         }
+        //     }
+        //     if(!v.empty())
+        //     {
+        //         break;
+        //     }
+        // }
+
+        // return v;
+
+        unordered_map<int, int> mp;
+        int n = nums.size();
+
+        // two paas hash map
+        for(int i = 0; i < n ; i++)
         {
-            for(int j = i + 1 ; j < nums.size();j++)
+            mp[nums[i]] = i;
+        }
+
+        for(int i = 0; i < n ; i++)
+        {
+            int complement = target - nums[i];
+            if(mp.count(complement) and mp[complement] != i)
             {
-                if(nums[i] + nums[j] == target)
-                {
-                    v = {i,j};
-                    break;
-                }
-            }
-            if(!v.empty())
-            {
-                break;
+                return {i,mp[complement]};
             }
         }
 
-        return v;
+        return {};
+        
     }
 };
