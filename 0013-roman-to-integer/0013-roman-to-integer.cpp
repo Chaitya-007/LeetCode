b547e554807ec3ba815ddbc3d0f075f1094da2cc
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-18 17:49:43 +0530

    Time: 20 ms (11.55%), Space: 10.7 MB (5.58%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -14,30 +14,53 @@ public:
         long long num = 0;
         int i = 0;
 
-        while(i < n)
-        {
-            if(i + 1 == n)
-            {
-                num += (mp[s[i]]);
-                i++;
+        // while(i < n)
+        // {
+        //     if(i + 1 == n)
+        //     {
+        //         num += (mp[s[i]]);
+        //         i++;
                 
-            }
+        //     }
 
-            else if(mp[s[i]] >= mp[s[i+1]])
-            {
+        //     else if(mp[s[i]] >= mp[s[i+1]])
+        //     {
                 
-                num += (mp[s[i]]);
-                i++;
+        //         num += (mp[s[i]]);
+        //         i++;
+        //     }
+
+        //     else if(mp[s[i]] < mp[s[i+1]])
+        //     {
+        //         num += ( (mp[s[i+1]]) - (mp[s[i]]) );
+        //         i = i + 2;
+
+        //     }
+        // }
+
+        // return num;
+
+        unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
             }
-
-            else if(mp[s[i]] < mp[s[i+1]])
-            {
-                num += ( (mp[s[i+1]]) - (mp[s[i]]) );
-                i = i + 2;
-
+            else{
+                ans += m[s[i]];
             }
         }
-
-        return num;
+        return ans;
     }
+    
 };
