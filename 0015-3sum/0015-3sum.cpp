3d78d977e7dfc05c61a4c9d553eb04a8fdb298ef
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-19 23:48:50 +0530

    Time: 140 ms (52.59%), Space: 26.2 MB (50.71%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,7 +3,135 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
 
         // using set method
-        vector<vector<int>> ans;
+//         vector<vector<int>> ans;
+//         sort(nums.begin(),nums.end());
+
+//         // base case 
+//         if(nums.size() < 3)
+//         {
+//             // There doesn't exists any triplet
+//             return {};
+//         }
+
+//         if(nums[0] > 0)
+//         {
+//             // all elements further will be > 0 
+//             // that means no negative integers
+//             // no triple can be formed such that sum = 0
+//             return {};
+//         }
+
+//         // you cannot use this because
+//         // The error you're encountering is related to the default constructor of unordered_set<vector<int>>. The error message suggests that the default constructor is implicitly deleted because one of its base classes has a deleted default constructor.
+
+// // To fix this issue, you can explicitly define the default constructor for unordered_set<vector<int>> before using it. Here's an updated version of your code with the explicit default constructor:
+
+// // unordered_set<vector<int>> sec;
+
+
+// // struct VectorHash {
+// //     size_t operator()(const std::vector<int>& vec) const {
+// //         size_t seed = vec.size();
+// //         for(auto& i : vec) {
+// //             seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+// //         }
+// //         return seed;
+// //     }
+// // };
+
+// // unordered_set<vector<int>, VectorHash> sec;
+
+        
+//         set<vector<int>> sec;
+
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             // if(nums[i] > 0)
+//             // {
+//             //     // because all elements after it will be positive
+//             //     // because of condition that the sum is zero this is used
+//             //     // or else it wouldn't have been used
+//             //     // and no triplet can be formed
+//             //     break;
+//             // }
+//             // if(i != 0 and nums[i] == nums[i-1]) 
+//             // {
+//             //     continue;
+//             // }
+//                 int l = i + 1;
+//                 int r = nums.size() - 1;
+//                 while(l < r)
+//                 {
+//                     if(nums[i] + nums[l] + nums[r] == 0)
+//                     {
+//                         vector<int>vec = {nums[i],nums[l],nums[r]};
+//                         sort(vec.begin(),vec.end());
+//                         sec.insert(vec);
+//                         // ans.emplace_back(vec);
+//                         l++;
+//                         r--;
+//                         // while(l<r and nums[l] == nums[l-1]){l++;}
+//                         // while(l<r and nums[r] == nums[r+1]){r--;}
+//                     }
+                    
+//                     else if(nums[i] + nums[l] + nums[r] < 0)
+//                     {
+//                         l++;
+//                     }
+
+//                     else
+//                     {
+//                         r--;
+//                     }
+//                 }
+//         }
+
+//         ans = {sec.begin(),sec.end()};
+//     //     sort(ans.begin(),ans.end());
+//     //    ans.erase(unique(ans.begin(),ans.end()),ans.end());
+//         return  ans;
+
+
+// *************************************************************
+
+    // using hashmap
+    // set<vector<int>> ans;
+
+    // for(int i = 0; i < nums.size(); i++)
+    // {
+    //     set<int> hashset;
+    //     for(int j = i + 1; j < nums.size(); j++)
+    //     {
+    //         int third = -(nums[i] + nums[j]);
+    //         if(hashset.find(third) != hashset.end())
+    //         {
+    //             vector<int>vec = {nums[i],nums[j],third};
+    //             sort(vec.begin(),vec.end());
+    //             ans.insert(vec);
+    //         }
+    //         hashset.insert(nums[j]);
+    //     }
+    // }
+
+    // vector<vector<int>> res {ans.begin(),ans.end()};
+    // return res;
+
+// tc = n2 + logm
+// sc = at max => n + o(no.of unique vectors * 2)
+// better soln
+
+
+
+
+
+
+// *************************************************************************
+
+        // using two pointer
+        // best approach
+
+
+         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
 
         // base case 
@@ -21,43 +149,21 @@ public:
             return {};
         }
 
-        // you cannot use this because
-        // The error you're encountering is related to the default constructor of unordered_set<vector<int>>. The error message suggests that the default constructor is implicitly deleted because one of its base classes has a deleted default constructor.
-
-// To fix this issue, you can explicitly define the default constructor for unordered_set<vector<int>> before using it. Here's an updated version of your code with the explicit default constructor:
-
-// unordered_set<vector<int>> sec;
-
-
-// struct VectorHash {
-//     size_t operator()(const std::vector<int>& vec) const {
-//         size_t seed = vec.size();
-//         for(auto& i : vec) {
-//             seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-//         }
-//         return seed;
-//     }
-// };
-
-// unordered_set<vector<int>, VectorHash> sec;
-
-        
-        set<vector<int>> sec;
 
         for(int i = 0; i < nums.size(); i++)
         {
-            // if(nums[i] > 0)
-            // {
-            //     // because all elements after it will be positive
-            //     // because of condition that the sum is zero this is used
-            //     // or else it wouldn't have been used
-            //     // and no triplet can be formed
-            //     break;
-            // }
-            // if(i != 0 and nums[i] == nums[i-1]) 
-            // {
-            //     continue;
-            // }
+            if(nums[i] > 0)
+            {
+                // because all elements after it will be positive
+                // because of condition that the sum is zero this is used
+                // or else it wouldn't have been used
+                // and no triplet can be formed
+                break;
+            }
+            if(i != 0 and nums[i] == nums[i-1]) 
+            {
+                continue;
+            }
                 int l = i + 1;
                 int r = nums.size() - 1;
                 while(l < r)
@@ -65,13 +171,13 @@ public:
                     if(nums[i] + nums[l] + nums[r] == 0)
                     {
                         vector<int>vec = {nums[i],nums[l],nums[r]};
-                        sort(vec.begin(),vec.end());
-                        sec.insert(vec);
-                        // ans.emplace_back(vec);
+                        // sort(vec.begin(),vec.end());
+                        // s.insert(vec);
+                        ans.emplace_back(vec);
                         l++;
                         r--;
-                        // while(l<r and nums[l] == nums[l-1]){l++;}
-                        // while(l<r and nums[r] == nums[r+1]){r--;}
+                        while(l<r and nums[l] == nums[l-1]){l++;}
+                        while(l<r and nums[r] == nums[r+1]){r--;}
                     }
                     
                     else if(nums[i] + nums[l] + nums[r] < 0)
@@ -86,74 +192,7 @@ public:
                 }
         }
 
-        ans = {sec.begin(),sec.end()};
-    //     sort(ans.begin(),ans.end());
-    //    ans.erase(unique(ans.begin(),ans.end()),ans.end());
         return  ans;
-
-        // using two pointer
-        //  vector<vector<int>> ans;
-        // sort(nums.begin(),nums.end());
-
-        // // base case 
-        // if(nums.size() < 3)
-        // {
-        //     // There doesn't exists any triplet
-        //     return {};
-        // }
-
-        // if(nums[0] > 0)
-        // {
-        //     // all elements further will be > 0 
-        //     // that means no negative integers
-        //     // no triple can be formed such that sum = 0
-        //     return {};
-        // }
-
-
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     // if(nums[i] > 0)
-        //     // {
-        //     //     // because all elements after it will be positive
-        //     //     // because of condition that the sum is zero this is used
-        //     //     // or else it wouldn't have been used
-        //     //     // and no triplet can be formed
-        //     //     break;
-        //     // }
-        //     // if(i != 0 and nums[i] == nums[i-1]) 
-        //     // {
-        //     //     continue;
-        //     // }
-        //         int l = i + 1;
-        //         int r = nums.size() - 1;
-        //         while(l < r)
-        //         {
-        //             if(nums[i] + nums[l] + nums[r] == 0)
-        //             {
-        //                 vector<int>vec = {nums[i],nums[l],nums[r]};
-        //                 sort(vec.begin(),vec.end());
-        //                 s.insert(vec);
-        //                 // ans.emplace_back(vec);
-        //                 l++;
-        //                 r--;
-        //                 // while(l<r and nums[l] == nums[l-1]){l++;}
-        //                 // while(l<r and nums[r] == nums[r+1]){r--;}
-        //             }
-                    
-        //             else if(nums[i] + nums[l] + nums[r] < 0)
-        //             {
-        //                 l++;
-        //             }
-
-        //             else
-        //             {
-        //                 r--;
-        //             }
-        //         }
-        // }
-
-        // return  ans;
 
 
 
