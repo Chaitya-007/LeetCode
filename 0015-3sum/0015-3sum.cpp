8f917012f4b7f55cabfd2725426f515231935b78
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-19 19:29:42 +0530

    Time: 1435 ms (8.01%), Space: 190.1 MB (27.77%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -20,18 +20,21 @@ public:
             return {};
         }
 
+        set<vector<int>> s;
         for(int i = 0; i < nums.size(); i++)
         {
-            if(nums[i] > 0)
-            {
-                // because all elements after it will be positive
-                // and no triplet can be formed
-                break;
-            }
-            if(i != 0 and nums[i] == nums[i-1]) 
-            {
-                continue;
-            }
+            // if(nums[i] > 0)
+            // {
+            //     // because all elements after it will be positive
+            //     // because of condition that the sum is zero this is used
+            //     // or else it wouldn't have been used
+            //     // and no triplet can be formed
+            //     break;
+            // }
+            // if(i != 0 and nums[i] == nums[i-1]) 
+            // {
+            //     continue;
+            // }
                 int l = i + 1;
                 int r = nums.size() - 1;
                 while(l < r)
@@ -39,11 +42,13 @@ public:
                     if(nums[i] + nums[l] + nums[r] == 0)
                     {
                         vector<int>vec = {nums[i],nums[l],nums[r]};
-                        ans.emplace_back(vec);
+                        sort(vec.begin(),vec.end());
+                        s.insert(vec);
+                        // ans.emplace_back(vec);
                         l++;
                         r--;
-                        while(l<r and nums[l] == nums[l-1]){l++;}
-                        while(l<r and nums[r] == nums[r+1]){r--;}
+                        // while(l<r and nums[l] == nums[l-1]){l++;}
+                        // while(l<r and nums[r] == nums[r+1]){r--;}
                     }
                     
                     else if(nums[i] + nums[l] + nums[r] < 0)
@@ -58,6 +63,7 @@ public:
                 }
         }
 
+        ans = {s.begin(),s.end()};
     //     sort(ans.begin(),ans.end());
     //    ans.erase(unique(ans.begin(),ans.end()),ans.end());
         return  ans;
