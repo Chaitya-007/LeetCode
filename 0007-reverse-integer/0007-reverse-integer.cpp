c85309887fea0d714067945d6cceae82f2dbd69f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-20 16:52:22 +0530

    Time: 2 ms (38.17%), Space: 6.3 MB (21.89%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+        bool negative = false;
+        long long temp = abs(x);
+        long long rev = 0;
+        int rem = 0;
+        if(x<0)
+        {
+            negative = true;
+        }
+
+        while(temp > 0)
+        {
+            rem = temp%10;
+            temp = temp/10;
+            rev = rem + rev*10;
+        }
+
+        if(negative)
+        {
+            rev = (-1)*rev;
+        }
+
+        if(rev < INT_MIN || rev > INT_MAX)
+        {
+            return 0;
+        }
+
+        return rev;
+    }
+};
