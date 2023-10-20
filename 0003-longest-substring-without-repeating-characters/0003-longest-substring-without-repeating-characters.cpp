f860d1eba01b017b79eee7b7268ac9cb46ae6289
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-21 00:04:59 +0530

    Time: 9 ms (78.81%), Space: 8.7 MB (57.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -41,28 +41,55 @@ public:
         // return maxo;
 
         // ******************************************
-        int right = 0;
+        // using set
+
+        // int right = 0;
+        // int left = 0;
+        // int maxLength = 0;
+        // unordered_set<char> hashset;
+        // int n = s.length();
+
+        // while(right < n)
+        // {
+        //     if(hashset.count(s[right]) == 0)
+        //     {
+        //         hashset.insert(s[right]);
+        //         maxLength = max(maxLength, right - left + 1);
+        //         right++;
+        //     }
+        //     else
+        //     {
+        //         while(hashset.count(s[right]))
+        //         {
+        //             hashset.erase(s[left]);
+        //             left++;
+        //         }
+        //     }
+        // }
+        // return maxLength;
+
+        // ********************************
+        // using unordered map
         int left = 0;
+        int right = 0;
         int maxLength = 0;
-        unordered_set<char> hashset;
-        int n = s.length();
-        while(right < n)
+        unordered_map<char, int> map;
+
+        while(right < s.length())
         {
-            if(hashset.count(s[right]) == 0)
+            if(map.count(s[right]) == 0 || map[s[right]] < left)
             {
-                hashset.insert(s[right]);
+                map[s[right]] = right;
                 maxLength = max(maxLength, right - left + 1);
-                right++;
             }
             else
             {
-                while(hashset.count(s[right]))
-                {
-                    hashset.erase(s[left]);
-                    left++;
-                }
+                left = map[s[right]] + 1;
+                map[s[right]] = right;
             }
+            right++;
         }
+
         return maxLength;
         
     }
