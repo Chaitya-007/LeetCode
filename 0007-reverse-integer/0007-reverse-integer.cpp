203352e8fde7ce09b1fe274046f1fb065d5bf200
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-20 17:00:41 +0530

    Time: 0 ms (100%), Space: 6.2 MB (42.26%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,15 +2,25 @@ class Solution {
 public:
     int reverse(int x) {
 
+        bool negative = false;
+        long long temp = abs(x);
         long long rev = 0;
         int rem = 0;
-       
-
-        while(x) // this works for both positive and negative
+        if(x<0)
         {
-            rem = x%10;
-            x = x/10;
+            negative = true;
+        }
+
+        while(temp) // this works for both positive and
+        {
+            rem = temp%10;
+            temp = temp/10;
             rev = rem + rev*10;
+        }
+
+        if(negative)
+        {
+            rev = (-1)*rev;
         }
 
         if(rev < INT_MIN || rev > INT_MAX)
