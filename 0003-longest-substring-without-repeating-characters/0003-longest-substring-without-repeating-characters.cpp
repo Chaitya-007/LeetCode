6262afb0b56811ae4780e455175e0d55e7c57588
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-20 21:35:10 +0530

    Time: 20 ms (44%), Space: 11.3 MB (24.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -13,32 +13,57 @@ public:
             return 1;
         }
 
-        int maxo = 0;
+        // int maxo = 0;
 
-        for(int i = 0; i < s.length(); i++)
-        {
-            unordered_set<char> hashset;
-            hashset.insert(s[i]);
-            // int count = 1;
-            for(int j = i + 1; j < s.length(); j++)
-            {
-                if(hashset.find(s[j]) != hashset.end())
-                {
-                    break;
-                }
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     unordered_set<char> hashset;
+        //     hashset.insert(s[i]);
+        //     // int count = 1;
+        //     for(int j = i + 1; j < s.length(); j++)
+        //     {
+        //         if(hashset.find(s[j]) != hashset.end())
+        //         {
+        //             break;
+        //         }
                 
-                hashset.insert(s[j]);
-                // count++;
-            }
+        //         hashset.insert(s[j]);
+        //         // count++;
+        //     }
 
-            // sec.insert(hashset.size());
-            int n = hashset.size();
-            maxo = max(maxo,n);
+        //     // sec.insert(hashset.size());
+        //     int n = hashset.size();
+        //     maxo = max(maxo,n);
+        // }
+        // // auto itr = sec.end();
+        // // itr--;
+        // // return (*itr);
+        // return maxo;
+
+        // ******************************************
+        int right = 0;
+        int left = 0;
+        int maxLength = 0;
+        unordered_set<char> hashset;
+        int n = s.length();
+        while(right < n)
+        {
+            if(hashset.count(s[right]) == 0)
+            {
+                hashset.insert(s[right]);
+                maxLength = max(maxLength, right - left + 1);
+                right++;
+            }
+            else
+            {
+                while(hashset.count(s[right]))
+                {
+                    hashset.erase(s[left]);
+                    left++;
+                }
+            }
         }
-        // auto itr = sec.end();
-        // itr--;
-        // return (*itr);
-        return maxo;
+        return maxLength;
         
     }
 };
