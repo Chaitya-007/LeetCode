96165f206dc9832b1ba855b94de7cabc2fd24245
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-26 09:50:23 +0530

    Time: 6 ms (87.78%), Space: 8.6 MB (65.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -92,20 +92,36 @@ public:
 
         // return maxLength;
 
-        int n = s.length();
-        int maxLength = 0;
-        vector<int> charIndex(128, -1);
-        int left = 0;
+        // int n = s.length();
+        // int maxLength = 0;
+        // vector<int> charIndex(128, -1);
+        // int left = 0;
         
-        for (int right = 0; right < n; right++) {
-            if (charIndex[s[right]] >= left) {
+        // for (int right = 0; right < n; right++) {
+        //     if (charIndex[s[right]] >= left) {
+        //         left = charIndex[s[right]] + 1;
+        //     }
+        //     charIndex[s[right]] = right;
+        //     maxLength = max(maxLength, right - left + 1);
+        // }
+        
+        // return maxLength;
+
+        int n = s.length();
+        int maxlength = 0;
+        int left = 0;
+        vector<int> charIndex(256,-1);
+        for(int right = 0; right < n; right++)
+        {
+            if(charIndex[s[right]] >= left)
+            {
                 left = charIndex[s[right]] + 1;
             }
             charIndex[s[right]] = right;
-            maxLength = max(maxLength, right - left + 1);
+            maxlength = max(maxlength,(right - left + 1));
         }
-        
-        return maxLength;
+
+        return maxlength;
         
     }
 };
