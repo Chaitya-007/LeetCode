135e0c81816a1d0812604990ef6b6271d83a8e68
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-27 23:11:40 +0530

    Time: 0 ms (100%), Space: 9.8 MB (19.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        // string res;
+        // int flag = 1;
+        // // we can also sort
+        // sort(strs.begin(),strs.end());
+
+        // for(int j = 0; j < strs[0].size(); j++)
+        // {
+        //     for(int i = 0; i < (strs.size() - 1); i++)
+        //     {
+        //         if(strs[i][j] == strs[i+1][j])
+        //         {
+
+        //         }
+        //         else
+        //         {
+        //             flag = 2;
+        //         }
+        //     }
+
+        //     if(flag == 2)
+        //     {
+        //         break;
+        //     }
+        //     else
+        //     {
+        //         res += strs[0][j];
+        //     }
+        // }
+        
+        // return res;
+
+        sort(strs.begin(),strs.end());
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
         string res;
-        int flag = 1;
+        long count = 0;
 
-        for(int j = 0; j < strs[0].size(); j++)
+        while(count < first.length() and count < last.length())
         {
-            for(int i = 0; i < (strs.size() - 1); i++)
+            if(first[count] == last[count])
             {
-                if(strs[i][j] == strs[i+1][j])
-                {
-
-                }
-                else
-                {
-                    flag = 2;
-                }
-            }
-
-            if(flag == 2)
-            {
-                break;
+                count++;
             }
             else
             {
-                res += strs[0][j];
+                break;
             }
         }
-        
-        return res;
+
+        return first.substr(0,count);
     }
 };
