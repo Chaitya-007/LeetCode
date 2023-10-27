4df95d43cbdf06007e12ae7a416ec59df4594f53
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-27 21:40:11 +0530

    Time: 3 ms (86.8%), Space: 15 MB (56.52%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,42 +11,91 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int>vec;
+        // vector<int>vec;
 
-        while(list1 != nullptr)
+        // while(list1 != nullptr)
+        // {
+        //     vec.emplace_back(list1->val);
+        //     list1 = list1->next;
+        // }
+
+        // while(list2 != nullptr)
+        // {
+        //     vec.emplace_back(list2->val);
+        //     list2 = list2->next;
+        // }
+
+        // sort(vec.begin(),vec.end());
+        
+        // ListNode *t = nullptr;
+        // ListNode *head = nullptr;
+
+        // for(int i = 0; i < vec.size(); i++)
+        // {
+        //     if(head == nullptr)
+        //     {
+        //         t = new ListNode(vec[i]);
+        //         head = t;
+        //     }
+        //     else
+        //     {
+        //         ListNode *p = new ListNode(vec[i]);
+        //         t->next = p;
+        //         t = t->next;
+        //         p = nullptr;
+        //     }
+        // }
+
+        // return head;
+        
+        // if list 1 is empty return list 2 simply
+        if(list1 == nullptr)
         {
-            vec.emplace_back(list1->val);
+            return list2;
+        }
+
+        // if list 2 is empty return list 1 simply
+        if(list2 == nullptr)
+        {
+            return list1;
+        }
+
+        ListNode *ptr = list1;
+        if(list1->val < list2->val)
+        {
             list1 = list1->next;
         }
-
-        while(list2 != nullptr)
+        else
         {
-            vec.emplace_back(list2->val);
+            ptr = list2;
             list2 = list2->next;
         }
+        ListNode *curr = ptr;
 
-        sort(vec.begin(),vec.end());
-        
-        ListNode *t = nullptr;
-        ListNode *head = nullptr;
-
-        for(int i = 0; i < vec.size(); i++)
+        while(list1 and list2)
         {
-            if(head == nullptr)
+            if(list1->val < list2->val)
             {
-                t = new ListNode(vec[i]);
-                head = t;
+                curr->next = list1;
+                list1 = list1->next;
             }
             else
             {
-                ListNode *p = new ListNode(vec[i]);
-                t->next = p;
-                t = t->next;
-                p = nullptr;
+                curr->next = list2;
+                list2 = list2->next;
             }
+            curr = curr->next;
         }
 
-        return head;
-        
+        if(!list1)
+        {
+            curr->next = list2;
+        }
+        else
+        {
+            curr->next = list1;
+        }
+
+        return ptr;
     }
 };
