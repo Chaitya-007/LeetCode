ebe989be3b7ff389ac6ff1c82f1374ba0e9204ec
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-27 22:12:15 +0530

    Time: 3 ms (45.06%), Space: 6.6 MB (42.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+            {
+                st.push(s[i]);
+            }
+            if(s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            if(s[i] == '{')
+            {
+                st.push(s[i]);
+            }
+
+            if(s[i] == ')')
+            {
+                if(!st.empty() and st.top() == '(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if(s[i] == ']')
+            {
+                if(!st.empty() and st.top() == '[')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            if(s[i] == '}')
+            {
+                if(!st.empty() and st.top() == '{')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        
+        if(st.empty())
+        {
+            return true;
+        }
+
+        return false;
+        
+    }
+};
