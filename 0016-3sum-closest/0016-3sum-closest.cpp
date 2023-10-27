5e7ffe336f2cd40399591cd834bda4c710717827
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-27 19:25:00 +0530

    Time: 33 ms (80.84%), Space: 10.5 MB (57.53%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -18,15 +18,11 @@ public:
         //     {
         //         continue;
         //     }
-        //     for(int l = i + 1; l < nums.size(); l++)
-        //     {
-        //         if(l != i + 1 and nums[l] == nums[l - 1])
-        //         {
-        //             continue;
-        //         }
-            
-        //     int r = l + 1;
-        //     while(r < nums.size())
+        //    
+        //         
+        //     int l = i + 1;
+        //     int r = nums.size() - 1;;
+        //     while(l < r)
         //     {
         //         sum = nums[i] + nums[l] + nums[r];
         //         if(sum == target)
@@ -49,31 +45,53 @@ public:
         //         }
         //     }
 
-        //     }
+        //     
         // }
         // return result;
 
-            if(nums.size() < 3) return 0;
-    int closest = nums[0]+nums[1]+nums[2];
-    sort(nums.begin(), nums.end());
-    for(int first = 0 ; first < nums.size()-2 ; ++first) {
-        if(first > 0 && nums[first] == nums[first-1]) continue;
-        int second = first+1;
-        int third = nums.size()-1;            
-        while(second < third) {
-            int curSum = nums[first]+nums[second]+nums[third];
-            if(curSum == target) return curSum;
-            if(abs(target-curSum)<abs(target-closest)) {
-                closest = curSum;
-            }
-            if(curSum > target) {
-                --third;
-            } else {
-                ++second;
-            }
+    //         if(nums.size() < 3) return 0;
+    // int closest = nums[0]+nums[1]+nums[2];
+    // sort(nums.begin(), nums.end());
+    // for(int first = 0 ; first < nums.size()-2 ; ++first) {
+    //     if(first > 0 && nums[first] == nums[first-1]) continue;
+    //     int second = first+1;
+    //     int third = nums.size()-1;            
+    //     while(second < third) {
+    //         int curSum = nums[first]+nums[second]+nums[third];
+    //         if(curSum == target) return curSum;
+    //         if(abs(target-curSum)<abs(target-closest)) {
+    //             closest = curSum;
+    //         }
+    //         if(curSum > target) {
+    //             --third;
+    //         } else {
+    //             ++second;
+    //         }
+    //     }
+    // }
+    // return closest;
+
+    sort(nums.begin(),nums.end());
+        int n=nums.size(),ans;
+        int min=INT_MAX;
+        for(int i=0;i<n-2;i++){
+               int l=i+1, r= n-1;
+               while(l<r){
+                   int sum =nums[i]+nums[l]+nums[r];
+                   if(abs(sum-target)<min){  // updating the sum if sum  so far. is closest to target
+                      min=abs(sum-target);
+                      ans=sum;
+                   }
+                   if(sum<target) l++;        //
+                   else if(sum>target)r--;
+                   else {
+                       ans=sum;  // we have sum equal to target which is closest so no need to check further 
+                       i=n-2;  // for breaking the outer for loop
+                       break;  // for breaking current while loop
+                   }
+               }
         }
-    }
-    return closest;
+        return ans;
 
     }
 };
