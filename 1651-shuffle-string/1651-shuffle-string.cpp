76e69bd72c2b311fb50dc1ad70a75d38022e728b
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-28 17:21:11 +0530

    Time: 6 ms (63.44%), Space: 15.5 MB (43.76%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.cpp
+++ 1651-shuffle-string/1651-shuffle-string.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        map<int,char> mp;
-        for(int i = 0; i < s.length(); i++)
-        {
-            mp[indices[i]] = s[i];
-        }
+        // map<int,char> mp;
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     mp[indices[i]] = s[i];
+        // }
 
         // string res;
-        s = "";
-        for(auto itr = mp.begin(); itr != mp.end(); itr++)
-        {
-            // res += (itr->second);
-            s += (itr->second);
-        }
+        // for(auto itr = mp.begin(); itr != mp.end(); itr++)
+        // {
+        //     res += (itr->second);
+            
+        // }
         // return res;
+
+         for(int i=0;i<indices.size();i++)
+        {
+            
+              while(indices[i]!=i)
+              {
+                  swap(s[i],s[indices[i]]);
+                  swap(indices[i],indices[indices[i]]);
+                       
+              }
+        }
+        
+       
         return s;
         
     }
