0156771ed8b37c767649069061bff71469d22fc5
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-06-01 13:45:16 +0530

    Time: 13 ms (17.19%), Space: 22 MB (5.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,57 +1,29 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // set<int>s(nums.begin(),nums.end());
+        set<int>s(nums.begin(),nums.end());
 
-        // nums = {s.begin(),s.end()};
-        // int res = s.size();
-        // return res;
+        nums = {s.begin(),s.end()};
+        int res = s.size();
+        return res;
 
-    //     int l = 0;
-    //     int r = 1;
-    //     int n = nums.size();
-    //     long count = 1;
+    // int cnt = 1;
+    // int l = 0;
+    // int r = 0;
+    // int n = nums.size();
 
-    //     if(nums.size() == 1)
+    // while(r < n)
+    // {
+    //     if(nums[l] != nums[r])
     //     {
-    //         return 1;
+    //         cnt++;
+    //         nums[l+1] = nums[r];
+    //         l++;
     //     }
+    //     r++;
+    // }
 
-    //     while(r<n)
-    //     {
-    //         if(nums[r] == nums[r-1])
-    //         {
-    //             r++;
-    //         }
-    //         else
-    //         {
-    //             count++;
-    //             nums[l+1] = nums[r];
-    //             l++;
-    //             r++;
-    //         }
-    //     }
-
-    // return count;
-
-
-    int cnt = 1;
-    int l = 0;
-    int r = 0;
-    int n = nums.size();
-
-    while(r < n)
-    {
-        if(nums[l] != nums[r])
-        {
-            cnt++;
-            nums[l+1] = nums[r];
-            l++;
-        }
-        r++;
-    }
-
-    return cnt;
+    // return cnt;
 
     // using STL
     // set<int> hashset;
