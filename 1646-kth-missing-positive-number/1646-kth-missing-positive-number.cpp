68808a086a3cff014e6db458f792d940bff436e1
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-06-05 12:46:08 +0530

    Time: 0 ms (100%), Space: 12.2 MB (13.42%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -2,23 +2,58 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         // Brute Force
-        unordered_set<int> st(arr.begin(),arr.end());
+        
+        // ***********************************
+        // M-1 
+        // T.C -> O(N)
+        // S.C -> O(N)
+        // unordered_set<int> st(arr.begin(),arr.end());
         
 
+        // int low = 0;
+        // int cnt = 0;
+
+        // while(cnt < k)
+        // {
+        //     low++;
+        //     if(st.find(low) == st.end())
+        //     {
+        //         cnt++;
+        //     }
+            
+        // }
+
+        // return low;
+
+        // *******************************************************
+        // M-2
         int low = 0;
         int cnt = 0;
+        int ind = 0;
+        int n = arr.size();
 
         while(cnt < k)
         {
             low++;
-            if(st.find(low) == st.end())
+            if(ind < n)
+            {
+                if(low != arr[ind])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    ind++;
+                }
+            }
+            else
             {
                 cnt++;
             }
-            
         }
 
         return low;
+    
         
         
     }
