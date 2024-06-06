99776e0a4028869392a33d3d56d25dd29335ddc5
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-06-06 12:01:59 +0530

    Time: 15 ms (94.88%), Space: 95.4 MB (25.66%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         // Uniion of two sorted arrays
-        // Using STL
-        // set<int> st = {nums1.begin(),nums1.end()};
-
-        // st.insert(nums2.begin(),nums2.end());
-
-        // vector<int> vec(st.begin(),st.end());
 
 // *********************************************************************
         // Union Of Two Sorted Arrays
@@ -47,46 +41,23 @@ public:
         }
 
         int n = vec.size();
+        double ans = 0;
 
         if(n%2 == 1)
         {
-            return static_cast<double>(vec[n/2]);
+            // return static_cast<double>(vec[n/2]);
+            ans = vec[n/2];
         }
         else
         {
-            int middle1 = vec[(n/2) - 1];
-            int middle2 = vec[(n/2)];
+            // int middle1 = vec[(n/2) - 1];
+            // int middle2 = vec[(n/2)];
 
-            return (static_cast<double>(middle1) + static_cast<double>(middle2))/ 2.0;
+            // return (static_cast<double>(middle1) + static_cast<double>(middle2))/ 2.0;
+            ans = ( (double)(vec[(n/2) - 1]) + (double) (vec[(n/2)]) ) / 2.0;
         }
 
-        // *******************************************************
-        //  int n = nums1.size();
-        // int m = nums2.size();
+        return ans;
 
-        // // Merge the arrays into a single sorted array.
-        // vector<int> merged;
-        // for (int i = 0; i < n; i++) {
-        //     merged.push_back(nums1[i]);
-        // }
-        // for (int i = 0; i < m; i++) {
-        //     merged.push_back(nums2[i]);
-        // }
-
-        // // Sort the merged array.
-        // sort(merged.begin(), merged.end());
-
-        // // Calculate the total number of elements in the merged array.
-        // int total = merged.size();
-
-        // if (total % 2 == 1) {
-        //     // If the total number of elements is odd, return the middle element as the median.
-        //     return static_cast<double>(merged[total / 2]);
-        // } else {
-        //     // If the total number of elements is even, calculate the average of the two middle elements as the median.
-        //     int middle1 = merged[total / 2 - 1];
-        //     int middle2 = merged[total / 2];
-        //     return (static_cast<double>(middle1) + static_cast<double>(middle2)) / 2.0;
-        // }
     }
 };
