d07d58ec94491c2a4bee19d81600a696bd15e41c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-17 20:46:54 +0530

    Time: 0 ms (100%), Space: 41.8 MB (38.63%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void sortColors(int[] nums) {
+    //   Set<Integer> st = new TreeSet<Integer>();
+
+    //   for(int e : nums)
+    //   {
+    //     st.add(e);
+    //   }  
+
+    //   int i = 0;
+    //   for(Integer num : st)
+    //   {
+    //     nums[i] = num;
+    //     i++;
+    //   }
+
+    Arrays.sort(nums);
+
+    }
+}
