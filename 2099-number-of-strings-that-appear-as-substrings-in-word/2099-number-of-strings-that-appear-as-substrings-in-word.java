155144bb13450463aeb2dab6a4deaf57718af3ba
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-24 14:22:32 +0530

    Time: 0 ms (100%), Space: 41.7 MB (79.18%) - LeetHub

--- 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.java
@@ -4,7 +4,8 @@ class Solution {
         int cnt = 0;
         for(String str : patterns)
         {
-            if(word.contains(str))
+            // if(word.contains(str))
+            if(word.indexOf(str) >= 0)
             {
                 cnt++;
             }
