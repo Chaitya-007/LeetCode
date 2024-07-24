4b02024f7aa719c397ae92ad555fb67972acbfbb
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-24 13:41:46 +0530

    Time: 3 ms (68.7%), Space: 43.5 MB (39.01%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
@@ -6,33 +6,54 @@ class Solution {
         return st.toString();
     }
     public int maximumNumberOfStringPairs(String[] words) {
-        int cnt_pair = 0;
-        int[] visited = new int[words.length];
-        Arrays.fill(visited,0);
+        // int cnt_pair = 0;
+        // int[] visited = new int[words.length];
+        // Arrays.fill(visited,0);
 
-        for(int i = 0; i < words.length; i++)
-        {
-            if(visited[i] == 0)
-            {
-            String revstr = reversestring(words[i]);
+        // for(int i = 0; i < words.length; i++)
+        // {
+        //     if(visited[i] == 0)
+        //     {
+        //     String revstr = reversestring(words[i]);
             
-            for(int j = i + 1; j < words.length; j++)
+        //     for(int j = i + 1; j < words.length; j++)
+        //     {
+        //         if(visited[j] == 0 && revstr.equals(words[j]))
+        //         {
+        //             cnt_pair++;
+        //             visited[i] = 1;
+        //             visited[j] = 1;
+        //             break;
+        //         }
+        //     }
+
+        //     }
+
+
+        // }
+
+        // return cnt_pair;
+
+        // M 2
+        int cnt = 0;
+        Map<String,Integer> mp = new HashMap<String,Integer>();
+
+        for(String str : words)
+        {
+            String rev = reversestring(str);
+            if(mp.containsKey(str)) // check the string of array => do not reverse it and check it
             {
-                if(visited[j] == 0 && revstr.equals(words[j]))
-                {
-                    cnt_pair++;
-                    visited[i] = 1;
-                    visited[j] = 1;
-                    break;
-                }
+
+                cnt++;
+                mp.remove(rev);
             }
-
+            else
+            {
+                mp.put(rev,1);
             }
-
-
         }
 
-        return cnt_pair;
+        return cnt;
 
     }
 }
