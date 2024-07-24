1a81dfab986ce2b8422e383f802b12cfddfe8e65
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-24 17:35:45 +0530

    Time: 5 ms (62.4%), Space: 41.4 MB (90.62%) - LeetHub

---
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        // String left = s.substring(0,(s.length())/2).toLowerCase();
+        // String right = s.substring(s.length()/2,s.length()).toLowerCase();
+        // System.out.println(left);
+
+        int leftcnt = 0;
+        int rightcnt = 0;
+
+        String vowel = "aeiouAEIOU";
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(i < s.length()/2)
+            {
+                if(vowel.indexOf(s.charAt(i)) >= 0)
+                {
+                    leftcnt++;
+                }
+            }
+            else
+            {
+                if(vowel.indexOf(s.charAt(i)) >= 0)
+                {
+                    rightcnt++;
+                }
+            }
+        }
+
+        if(leftcnt == rightcnt)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
