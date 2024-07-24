dd2b381e569a3aca6a3d48147cf7ff96f0eb2af6
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-24 16:47:15 +0530

    Time: 3 ms (51.9%), Space: 45.2 MB (12.51%) - LeetHub

---
+++ 0979-di-string-match/0979-di-string-match.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+
+        // When I == inc++
+        // when D == dec--
+
+        int inc = 0;
+        int dec = s.length();
+        int[] arr = new int[s.length() + 1];
+        arr[0] = 0;
+        int index = 0;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == 'I')
+            {
+                
+                arr[index] = inc;
+                index++;
+                inc++;
+            }
+            else
+            {
+                arr[index] = dec;
+                index++;
+                dec--;
+            }
+        }
+
+        arr[s.length()] = inc; // = dec can also be here
+
+        return arr;
+    }
+}
