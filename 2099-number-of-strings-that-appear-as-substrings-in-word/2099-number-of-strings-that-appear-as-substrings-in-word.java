b336c119e246a5967ed3e0de67b03fb5e33661ad
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-24 14:57:10 +0530

    Time: 0 ms (100%), Space: 41.8 MB (51.52%) - LeetHub

--- 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.java
@@ -2,6 +2,7 @@ class Solution {
     public int numOfStrings(String[] patterns, String word) {
         
         int cnt = 0;
+        // using stl
         for(String str : patterns)
         {
             // if(word.contains(str))
@@ -10,6 +11,49 @@ class Solution {
                 cnt++;
             }
         }
+
+        // for(String str : patterns)
+        // {
+        //     if(str.length() > word.length())
+        //     {
+        //         continue;
+        //     }
+
+        //     boolean flag = false;
+        //     int k = 0;
+            
+        //     for(int i = 0; i < word.length(); i++)
+        //     {
+        //         if(str.charAt(k) == word.charAt(i))
+        //         {
+        //             flag = true;
+        //             k++;
+        //             i++;
+        //             while(k < str.length())
+        //             {
+                        
+        //                 if( str.charAt(k) == word.charAt(i))
+        //                 {
+        //                 i++;
+        //                 k++;
+        //                 }
+        //                 else if(str.charAt(k) != word.charAt(i))
+        //                 {
+        //                     flag = false;
+        //                     break;
+        //                 }
+        //             }
+        //             break;
+        //         }
+            
+        //     }
+
+        //     if(flag)
+        //     {
+        //         System.out.println(str);
+        //         cnt++;
+        //     }
+        // }
 
         return cnt;
     }
