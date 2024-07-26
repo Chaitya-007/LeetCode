c361a3bf483da5f7ea30eec9733b6fe7a507b589
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-26 23:17:28 +0530

    Time: 0 ms (100%), Space: 41 MB (93.83%) - LeetHub

---
+++ 2365-percentage-of-letter-in-string/2365-percentage-of-letter-in-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int percentageLetter(String s, char letter) {
+        int cnt = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == letter)
+            {
+                cnt++;
+            }
+        }
+
+        return (cnt*100)/(s.length());
+    }
+}
