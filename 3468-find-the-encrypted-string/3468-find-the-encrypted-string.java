77f44b95afed2cd6a888e1160ddfd21710b4e011
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-29 21:14:52 +0530

    Time: 1 ms (100%), Space: 42.6 MB (67.78%) - LeetHub

---
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        StringBuilder sb = new StringBuilder("");
+        int n = s.length();
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            sb.append(s.charAt((i + k)%n));
+        }
+
+        return sb.toString();
+    }
+}
