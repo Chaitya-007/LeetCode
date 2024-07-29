516f8c7612a837ae02bcf3768c7a448b7b955067
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-29 21:10:46 +0530

    Time: 4 ms (81.85%), Space: 43.4 MB (33.57%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,35 +1,63 @@
 class Solution {
     public int minLength(String s) {
 
-        int cnt = 0;
-        StringBuilder sb = new StringBuilder("");
+        // int cnt = 0;
+        // StringBuilder sb = new StringBuilder("");
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     int len = sb.length();
+
+        //     if(len > 0)
+        //     {
+        //         if(s.charAt(i) == 'B' && sb.charAt(len - 1) == 'A')
+        //         {
+        //             sb.deleteCharAt(len - 1);
+        //         }
+        //         else if(s.charAt(i) == 'D' && sb.charAt(len - 1) == 'C')
+        //         {
+        //             sb.deleteCharAt(len - 1);
+        //         }
+        //         else
+        //         {
+        //             sb.append(s.charAt(i));
+        //         }
+        //     }
+        //     else
+        //     {
+        //         sb.append(s.charAt(i));
+        //     }
+        // }
+
+        // // System.out.println(sb);
+
+        // return sb.length();
+
+        Stack<Character> st = new Stack<Character>();
+
         for(int i = 0; i < s.length(); i++)
         {
-            int len = sb.length();
-
-            if(len > 0)
+            char ch = s.charAt(i);
+            if(st.isEmpty())
             {
-                if(s.charAt(i) == 'B' && sb.charAt(len - 1) == 'A')
-                {
-                    sb.deleteCharAt(len - 1);
-                }
-                else if(s.charAt(i) == 'D' && sb.charAt(len - 1) == 'C')
-                {
-                    sb.deleteCharAt(len - 1);
-                }
-                else
-                {
-                    sb.append(s.charAt(i));
-                }
+                st.push(ch);
             }
             else
             {
-                sb.append(s.charAt(i));
+                if(s.charAt(i) == 'B' && st.peek() == 'A')
+                {
+                    st.pop();
+                }
+                else if(s.charAt(i) == 'D' && st.peek() == 'C')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(ch);
+                }
             }
         }
 
-        // System.out.println(sb);
-
-        return sb.length();
+        return st.size();
     }
 }
