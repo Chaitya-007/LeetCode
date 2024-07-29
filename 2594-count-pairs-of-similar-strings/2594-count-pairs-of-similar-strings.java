6024932c7bd5236593b1130776316427d18d3ef9
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-29 19:59:57 +0530

    Time: 274 ms (35.86%), Space: 44.8 MB (42.62%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.java
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.java
@@ -1,60 +1,32 @@
 class Solution {
     public int similarPairs(String[] words) {
-        int[] visited = new int[words.length];
-        Arrays.fill(visited,0);
+
         int cnt = 0;
-        
+
         for(int i = 0; i < words.length; i++)
         {
-
-            // if(visited[i] == 1)
-            // {
-            //     continue;
-            // }
-
-            HashSet<Character> set1 = new HashSet<Character>();
-
+        Set<Character> s1 = new HashSet<Character>();
+            
             for(int k = 0; k < words[i].length(); k++)
             {
-                set1.add(words[i].charAt(k));
+                s1.add(words[i].charAt(k));
             }
 
             for(int j = i + 1; j < words.length; j++)
             {
-                // if(visited[j] == 1)
-                // {
-                //     continue;
-                // }
-                HashSet<Character> set2 = new HashSet<Character>();
+                Set<Character> s2 = new HashSet<Character>();
 
-                for(int m = 0; m < words[j].length(); m++)
+                for(int u = 0; u < words[j].length(); u++)
                 {
-                   set2.add(words[j].charAt(m));
+                    s2.add(words[j].charAt(u));
                 }
 
-                if(set1.equals(set2))
+                if(s1.equals(s2))
                 {
-                    System.out.println("set1 " + set1 + " set2 " + set2);
-                    // visited[i] = 1;
-                    // visited[j] = 1;
                     cnt++;
                 }
-
             }
         }
-
-        for(int num : visited)
-        {
-            System.out.print(num + " ");
-        }
-
-        // for(int i = 0; i < visited.length; i++)
-        // {
-        //     if(visited[i] == 1)
-        //     {
-        //         cnt++;
-        //     }
-        // }
 
         return cnt;
     }
