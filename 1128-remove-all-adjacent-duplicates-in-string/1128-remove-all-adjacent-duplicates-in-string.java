69eb3a846576fa8d892b8175e9f276bb8ef6dd58
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-29 20:45:17 +0530

    Time: 10 ms (90.65%), Space: 45.2 MB (60.12%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,36 +1,55 @@
 class Solution {
     public String removeDuplicates(String s) {
         
-        Stack<Character> st = new Stack<Character>();
+        // Stack<Character> st = new Stack<Character>();
 
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(st.isEmpty())
-            {
-                st.push(s.charAt(i));
-            }
-            else 
-            {
-                if(st.peek() == s.charAt(i))
-                {
-                    st.pop();
-                }
-                else
-                {
-                    st.push(s.charAt(i));
-                }
-            }
-        }
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     if(st.isEmpty())
+        //     {
+        //         st.push(s.charAt(i));
+        //     }
+        //     else 
+        //     {
+        //         if(st.peek() == s.charAt(i))
+        //         {
+        //             st.pop();
+        //         }
+        //         else
+        //         {
+        //             st.push(s.charAt(i));
+        //         }
+        //     }
+        // }
+
+        // StringBuilder sb = new StringBuilder("");
+
+        // while(!st.isEmpty())
+        // {
+        //     char ch = st.pop();
+        //     sb.append(ch);
+        // }
+
+        // sb.reverse();
+
+        // return sb.toString();
 
         StringBuilder sb = new StringBuilder("");
 
-        while(!st.isEmpty())
-        {
-            char ch = st.pop();
-            sb.append(ch);
-        }
 
-        sb.reverse();
+        for(char ch : s.toCharArray())
+        {
+            int len = sb.length();
+
+            if(len > 0 && ch == sb.charAt(len - 1))
+            {
+                sb.deleteCharAt(len - 1);
+            }
+            else
+            {
+                sb.append(ch);
+            }
+        }
 
         return sb.toString();
     }
