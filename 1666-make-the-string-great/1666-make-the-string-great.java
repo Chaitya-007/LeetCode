974af797591e7edf0c2c79d5966fdaaaedb37e64
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-07-30 17:03:03 +0530

    Time: 3 ms (57.93%), Space: 42 MB (71.85%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.java
+++ 1666-make-the-string-great/1666-make-the-string-great.java
@@ -5,30 +5,66 @@ class Solution {
             return s;
         }
 
-        StringBuilder sb = new StringBuilder("");
+        // StringBuilder sb = new StringBuilder("");
+
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     int len = sb.length();
+
+        //     if(len > 0)
+        //     {
+        //         int num = Math.abs(s.charAt(i) - sb.charAt(len - 1));
+        //         if(num == 32)
+        //         {
+        //             sb.deleteCharAt(len - 1);
+        //         }
+        //         else
+        //         {
+        //             sb.append(s.charAt(i));
+        //         }
+        //     }
+        //     else
+        //     {
+        //         sb.append(s.charAt(i));
+        //     }
+        // }
+
+        // return sb.toString();
+
+        Stack<Character> st = new Stack<Character>();
 
         for(int i = 0; i < s.length(); i++)
         {
-            int len = sb.length();
+            char ch = s.charAt(i);
 
-            if(len > 0)
+            if(st.isEmpty())
             {
-                int num = Math.abs(s.charAt(i) - sb.charAt(len - 1));
-                if(num == 32)
-                {
-                    sb.deleteCharAt(len - 1);
-                }
-                else
-                {
-                    sb.append(s.charAt(i));
-                }
+                st.push(ch);
             }
             else
             {
-                sb.append(s.charAt(i));
+                int num = Math.abs(ch - st.peek());
+                if(num == 32)
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(ch);
+                }
             }
         }
 
+        StringBuilder sb = new StringBuilder("");
+
+        while(!st.isEmpty())
+        {
+            char ch = st.peek();
+            sb.append(ch);
+            st.pop();
+        }
+
+        sb.reverse();
         return sb.toString();
     }
 }
