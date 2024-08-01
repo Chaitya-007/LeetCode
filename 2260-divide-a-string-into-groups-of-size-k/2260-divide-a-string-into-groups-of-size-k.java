03ce9b2aa8419b06aa2be8d811889575ec64f4c2
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-01 18:53:13 +0530

    Time: 7 ms (19.59%), Space: 42.6 MB (40.64%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
@@ -1,41 +1,29 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
+
         List<String> list = new ArrayList<String>();
-        int cnt = 0;
-        StringBuilder sb = new StringBuilder("");
 
-        for(int i = 0; i < s.length(); i++)
+     
+        for(int i = 0; i < s.length(); i+=k)
         {
-            sb.append(s.charAt(i));
-            if(++cnt == k)
+            String t = s.substring(i,Math.min(i+k,s.length()));
+            if(t.length() < k)
             {
-                list.add(sb.toString());
-                sb.setLength(0);
-                cnt = 0;
+                while(t.length() < k)
+                {
+                    t += fill;
+                }
             }
-            
+            list.add(t);
         }
 
+        String[] sarray = new String[list.size()];
 
-        while(cnt != 0 && cnt != k)
+        for(int i = 0; i < list.size(); i++)
         {
-            sb.append(fill);
-            cnt++;
+            sarray[i] = list.get(i);
         }
 
-        if(sb.length() > 0)
-        {
-            list.add(sb.toString());
-        }
-
-        System.out.println(list);
-        int len = list.size();
-        String[] str = new String[len];
-
-        for(int i = 0; i < len; i++)
-        {
-            str[i] = list.get(i);
-        }
-        return str;
+        return sarray;
     }
 }
