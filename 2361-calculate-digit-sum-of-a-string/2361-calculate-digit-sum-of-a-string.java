2d6cc977636c9f10e3110048ac3549871a724eb5
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-01 18:38:40 +0530

    Time: 6 ms (23.31%), Space: 41.8 MB (26.82%) - LeetHub

--- 2361-calculate-digit-sum-of-a-string/2361-calculate-digit-sum-of-a-string.java
+++ 2361-calculate-digit-sum-of-a-string/2361-calculate-digit-sum-of-a-string.java
@@ -1,18 +1,22 @@
 class Solution {
 
     public String digitSum(String s, int k) {
-         while(s.length()>k){
-            String ns=""; // replace the old string with updated one
-            for(int i=0;i<s.length();i+=k){
-                String t=s.substring(i,Math.min(i+k,s.length())); // form the string of k size
-                int sum=0;
-                for(int l=0;l<t.length();l++){ // to find the character sum of string t
-                    sum+=t.charAt(l)-'0';
+        while(s.length() > k)
+        {
+            String ns = "";
+            for(int i = 0; i < s.length(); i+=k)
+            {
+                String t = s.substring(i,Math.min(i+k,s.length()));
+                int sum = 0;
+                for(int j = 0; j < t.length(); j++)
+                {
+                    sum += (t.charAt(j) - '0');
                 }
-                ns+="" + sum;    //update the string with sum of k size string character                 
+                ns += sum;
             }
-            s=ns; //update the old string with updated one
+            s = ns;
         }
+
         return s;
     }
 }
