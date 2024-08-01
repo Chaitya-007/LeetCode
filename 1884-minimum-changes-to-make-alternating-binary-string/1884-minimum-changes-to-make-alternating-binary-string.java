496c77cb467db65ebdc8153af2ae0be31cf8e8b6
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-01 20:15:24 +0530

    Time: 4 ms (68.19%), Space: 42.5 MB (23.15%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.java
@@ -1,48 +1,36 @@
 class Solution {
     public int minOperations(String s) {
 
-        StringBuilder sb1 = new StringBuilder("");
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(i%2 == 0)
-            {
-                sb1.append('0');
-            }
-            else
-            {
-                sb1.append('1');
-            }
-        }
-
-        StringBuilder sb2 = new StringBuilder("");
+// check notes section
+        int first = 0;
+        int second = 0;
 
         for(int i = 0; i < s.length(); i++)
         {
             if(i%2 == 0)
             {
-                sb2.append('1');
+                if(s.charAt(i) == '1')
+                {
+                    first++;
+                }
+                else
+                {
+                    second++;
+                }
             }
             else
             {
-                sb2.append('0');
+                if(s.charAt(i) == '1')
+                {
+                    second++;
+                }
+                else
+                {
+                    first++;
+                }
             }
         }
 
-        int diff1 = 0;
-        int diff2 = 0;
-
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(sb1.charAt(i) != s.charAt(i))
-            {
-                diff1 += 1;
-            }
-            if(sb2.charAt(i) != s.charAt(i))
-            {
-                diff2 += 1;
-            }
-        }
-
-        return Math.min(diff1,diff2);
+        return Math.min(first,second);
     }
 }
