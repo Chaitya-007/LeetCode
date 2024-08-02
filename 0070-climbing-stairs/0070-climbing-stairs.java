2ad1424d744b61ce5fe8945a15a7209f0f1d4289
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-02 09:17:49 +0530

    Time: 0 ms (100%), Space: 40.2 MB (43.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -61,15 +61,28 @@ class Solution {
     // return result;
 
     // Tabulation
-    int[] dp = new int[n+1];
-    dp[0] = 1;
-    dp[1] = 1;
+    // int[] dp = new int[n+1];
+    // dp[0] = 1;
+    // dp[1] = 1;
+
+    // for(int i = 2; i <= n; i++)
+    // {
+    //     dp[i] = dp[i-1] + dp[i-2];
+    // }
+
+    // return dp[n];
+
+    // Using Space Optimization
+    int prev = 1;
+    int prev2 = 1;
 
     for(int i = 2; i <= n; i++)
     {
-        dp[i] = dp[i-1] + dp[i-2];
+        int curr = prev + prev2;
+        prev2 = prev;
+        prev = curr;
     }
 
-    return dp[n];
+    return prev;
     }
 }
