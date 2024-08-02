3a6644adec22c59100724e3a57f2f6cbfa430c52
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-02 09:39:40 +0530

    Time: 0 ms (100%), Space: 40.3 MB (43.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -19,13 +19,28 @@ class Solution {
     //         return 1;
     //     }
 
-    //     if(!map.containsKey(ind))
+    //     if(map.containsKey(ind))
     //     {
-    //         map.put(ind,memo(ind - 1,map) + memo(ind - 2,map));
+    //     return map.get(ind);
     //     }
 
-    //     return map.get(ind);
+    //     return  map.put(ind,memo(ind - 1,map) + memo(ind - 2,map));
     // }
+
+    public int memo(int ind, Map<Integer, Integer> map) {
+    if (ind == 0 || ind == 1) {
+        return 1;
+    }
+
+    if (map.containsKey(ind)) {
+        return map.get(ind);
+    }
+
+    int value = memo(ind - 1, map) + memo(ind - 2, map);
+    map.put(ind, value);
+    return value;
+}
+
 
     // Memoization using table 
     // public int memoTable(int ind,int[] dp)
@@ -49,10 +64,10 @@ class Solution {
     //  return result;   
 
     // Memoization using Map
-    // Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+    Map<Integer,Integer> map = new HashMap<Integer,Integer>();
 
-    // int result = memo(n,map);
-    // return result;
+    int result = memo(n,map);
+    return result;
 
     // Memoization using Array
     // int[] dp = new int[n+1];
@@ -73,16 +88,16 @@ class Solution {
     // return dp[n];
 
     // Using Space Optimization
-    int prev = 1;
-    int prev2 = 1;
+    // int prev = 1;
+    // int prev2 = 1;
 
-    for(int i = 2; i <= n; i++)
-    {
-        int curr = prev + prev2;
-        prev2 = prev;
-        prev = curr;
-    }
+    // for(int i = 2; i <= n; i++)
+    // {
+    //     int curr = prev + prev2;
+    //     prev2 = prev;
+    //     prev = curr;
+    // }
 
-    return prev;
+    // return prev;
     }
 }
