5042980fce095e4fd06fc1b1a6d8e7b8fe7a86c1
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-03 17:41:49 +0530

    Time: 0 ms (100%), Space: 44.4 MB (32.7%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,25 +11,48 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode, Integer> map = new HashMap<ListNode,Integer> ();
-
         if(head == null)
         {
             return false;
         }
 
-        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(temp != null)
+        while(fast != null && fast.next != null)
         {
-            if(map.containsKey(temp))
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
             {
                 return true;
             }
-            map.put(temp,1);
-            temp = temp.next;
+
         }
 
         return false;
+
+        // Brute Force
+        // HashMap<ListNode, Integer> map = new HashMap<ListNode,Integer> ();
+
+        // if(head == null)
+        // {
+        //     return false;
+        // }
+
+        // ListNode temp = head;
+
+        // while(temp != null)
+        // {
+        //     if(map.containsKey(temp))
+        //     {
+        //         return true;
+        //     }
+        //     map.put(temp,1);
+        //     temp = temp.next;
+        // }
+
+        // return false;
     }
 }
