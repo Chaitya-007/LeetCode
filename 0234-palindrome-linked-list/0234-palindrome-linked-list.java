879364fe23fe41ed872d9d790a3b267d2471f50a
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-04 00:21:19 +0530

    Time: 4 ms (86.37%), Space: 69.6 MB (8.03%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -30,24 +30,100 @@ class Solution {
         return true;
     }
 
+    public ListNode reverseHead(ListNode head)
+    {
+        ListNode curr = head;
+        ListNode forward = head;
+        ListNode prev = null;
+
+        while(curr != null)
+        {
+            forward = forward.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forward;
+        }
+
+        return prev;
+    }
+
+   
+
     public boolean isPalindrome(ListNode head) {
+
+        // Optimal 
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode newHead = reverseHead(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+
+        while(second != null)
+        {
+            if(first.val != second.val)
+            {
+                reverseHead(newHead);
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        reverseHead(newHead);
+        return true;
+
+        // Brute Force - 1
+        // ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // ListNode ptr = head;
+        // while(ptr != null)
+        // {
+        //     list.add(ptr.val);
+        //     ptr = ptr.next;
+        // }
+
+
+        // if(checkPallindrome(list))
+        // {
+        //     return true;
+        // }
+
+        // return false;
+
+        // Brute Force 2
         // Stack<Integer> st = new Stack<Integer>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        // ListNode temp = head;
 
-        ListNode ptr = head;
-        while(ptr != null)
-        {
-            list.add(ptr.val);
-            ptr = ptr.next;
-        }
+        // if(head.next == null)
+        // {
+        //     return true;
+        // }
 
+        // while(temp != null)
+        // {
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
 
-        if(checkPallindrome(list))
-        {
-            return true;
-        }
+        // temp = head;
+        // while(temp != null)
+        // {
+        //     if(temp.val != st.peek())
+        //     {
+        //         return false;
+        //     }
 
-        return false;
+        //     temp = temp.next;
+        //     st.pop();
+        // }
+
+        // return true;
         
     }
 }
