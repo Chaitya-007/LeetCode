09af548997f440daff85ce3eb8bee846ee75841f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-04 20:23:36 +0530

    Time: 1 ms (99.92%), Space: 48.3 MB (72.33%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -33,32 +33,32 @@ public class Solution {
         // Naive Approach
         // T.C -> O(n1 * logN) + O(n2 * logN)
         // S.C -> O(N1) or O(N2)
-        if(headA == null || headB == null)
-        {
-            return null;
-        }
+        // if(headA == null || headB == null)
+        // {
+        //     return null;
+        // }
 
-        HashMap<ListNode, Integer> map = new HashMap<ListNode, Integer>();
+        // HashMap<ListNode, Integer> map = new HashMap<ListNode, Integer>();
 
-        ListNode ptra = headA;
-        ListNode ptrb = headB;
+        // ListNode ptra = headA;
+        // ListNode ptrb = headB;
 
-        while(ptra != null)
-        {
-            map.put(ptra,1);
-            ptra = ptra.next;
-        }
+        // while(ptra != null)
+        // {
+        //     map.put(ptra,1);
+        //     ptra = ptra.next;
+        // }
 
-        while(ptrb != null)
-        {
-            if(map.containsKey(ptrb))
-            {
-                return ptrb;
-            }
-            ptrb = ptrb.next;
-        }
+        // while(ptrb != null)
+        // {
+        //     if(map.containsKey(ptrb))
+        //     {
+        //         return ptrb;
+        //     }
+        //     ptrb = ptrb.next;
+        // }
 
-        return null;
+        // return null;
 
         // Using Two Approach
         // T.C -> O(N1) + O(N2) + O(n2 - N1) + O(N2 , whichever is small let's consider N2 as small)
@@ -96,35 +96,35 @@ public class Solution {
         // Optimal Solution
         // T.C -> O(N1 + N2)
         // S.C -> O(1)
-        // if(headA == null || headB == null)
-        // {
-        //     return null;
-        // }
+        if(headA == null || headB == null)
+        {
+            return null;
+        }
 
-        // ListNode t1 = headA;
-        // ListNode t2 = headB;
+        ListNode t1 = headA;
+        ListNode t2 = headB;
 
-        // while(t1 != t2)
-        // {
-        //     t1 = t1.next;
-        //     t2 = t2.next;
+        while(t1 != t2)
+        {
+            t1 = t1.next;
+            t2 = t2.next;
 
-        //     if(t1 == t2) // this also holds true for t1 == null && t2 == null
-        //     {
-        //         return t1;
-        //     }
+            if(t1 == t2) // this also holds true for t1 == null && t2 == null
+            {
+                return t1;
+            }
 
-        //     if(t1 == null)
-        //     {
-        //         t1 = headB;
-        //     }
-        //     if(t2 == null)
-        //     {
-        //         t2 = headA;
-        //     }
+            if(t1 == null)
+            {
+                t1 = headB;
+            }
+            if(t2 == null)
+            {
+                t2 = headA;
+            }
 
-        // }
+        }
 
-        // return t1;
+        return t1;
     }
 }
