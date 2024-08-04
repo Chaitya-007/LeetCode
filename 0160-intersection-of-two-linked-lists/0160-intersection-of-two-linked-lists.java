4dfcf80bfeea6cd287ffe77264c3964875355c81
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-04 20:21:58 +0530

    Time: 1 ms (99.92%), Space: 48.2 MB (72.33%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -61,35 +61,70 @@ public class Solution {
         // return null;
 
         // Using Two Approach
-        if(headA == null && headB == null)
+        // T.C -> O(N1) + O(N2) + O(n2 - N1) + O(N2 , whichever is small let's consider N2 as small)
+        // if(headA == null && headB == null)
+        // {
+        //     return null;
+        // }
+        // int n1 = 0;
+        // int n2 = 0;
+        // ListNode tempA = headA;
+        // ListNode tempB = headB;
+
+        // while(tempA != null)
+        // {
+        //     n1++;
+        //     tempA = tempA.next;
+        // }
+
+        // while(tempB != null)
+        // {
+        //     n2++;
+        //     tempB = tempB.next;
+        // }
+
+        // tempA = headA; // Set the pointers to head again
+        // tempB = headB; // Set the pointers to head again
+
+        // if(n1 < n2)
+        // {
+        //     return collisionPoint(tempA,tempB,n2 - n1);
+        // }
+
+        // return collisionPoint(tempB,tempA,n1 - n2);
+
+        // Optimal Solution
+        // T.C -> O(N1 + N2)
+        // S.C -> O(1)
+        if(headA == null || headB == null)
         {
             return null;
         }
-        int n1 = 0;
-        int n2 = 0;
-        ListNode tempA = headA;
-        ListNode tempB = headB;
 
-        while(tempA != null)
+        ListNode t1 = headA;
+        ListNode t2 = headB;
+
+        while(t1 != t2)
         {
-            n1++;
-            tempA = tempA.next;
+            t1 = t1.next;
+            t2 = t2.next;
+
+            if(t1 == t2) // this also holds true for t1 == null && t2 == null
+            {
+                return t1;
+            }
+
+            if(t1 == null)
+            {
+                t1 = headB;
+            }
+            if(t2 == null)
+            {
+                t2 = headA;
+            }
+
         }
 
-        while(tempB != null)
-        {
-            n2++;
-            tempB = tempB.next;
-        }
-
-        tempA = headA; // Set the pointers to head again
-        tempB = headB; // Set the pointers to head again
-
-        if(n1 < n2)
-        {
-            return collisionPoint(tempA,tempB,n2 - n1);
-        }
-
-        return collisionPoint(tempB,tempA,n1 - n2);
+        return t1;
     }
 }
