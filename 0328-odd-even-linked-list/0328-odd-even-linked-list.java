2a2f1b5da44dc84637eef80eccd815b87c51bc2b
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-04 12:03:58 +0530

    Time: 2 ms (5.04%), Space: 44.2 MB (74.48%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,38 +10,80 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+
+        // Brute Force
         if(head == null || head.next == null)
         {
             return head;
         }
-
-        ListNode evenHead = new ListNode(0,null);
-        ListNode evenptr = evenHead;
-
-        ListNode temp = head;
-        ListNode prev = null;
-
-        int cnt = 0;
-        while(temp != null)
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        // for odd positon
+        ListNode fast = head;
+        while(fast != null && fast.next != null)
         {
-            cnt++;
-            if(cnt%2 == 0)
-            {
-                System.out.println(temp.val);
-                System.out.println("count " + cnt);
-                evenptr.next = new ListNode(temp.val,null);
-                evenptr = evenptr.next;
-                prev.next = temp.next;
-                temp = prev.next;
-            }
-            else
-            {
-                prev = temp;
-                temp = temp.next;
-            }
+            list.add(fast.val);
+            fast = fast.next.next;
+        }
+        if(fast != null)
+        {
+            list.add(fast.val);
         }
 
-        prev.next = evenHead.next;
+        // for even position
+        fast = head.next;
+        while(fast != null && fast.next != null)
+        {
+            list.add(fast.val);
+            fast = fast.next.next;
+        }
+        if(fast != null)
+        {
+            list.add(fast.val);
+        }
+
+        fast = head;
+        int i = 0;
+        while(fast != null)
+        {
+            fast.val = list.get(i);
+            i++;
+            fast = fast.next;
+        }
+
         return head;
+
+
+        // Self solution
+        // if(head == null || head.next == null)
+        // {
+        //     return head;
+        // }
+
+        // ListNode evenHead = new ListNode(0,null);
+        // ListNode evenptr = evenHead;
+
+        // ListNode temp = head;
+        // ListNode prev = null;
+
+        // int cnt = 0;
+        // while(temp != null)
+        // {
+        //     cnt++;
+        //     if(cnt%2 == 0)
+        //     {
+        //         evenptr.next = new ListNode(temp.val,null);
+        //         evenptr = evenptr.next;
+        //         prev.next = temp.next;
+        //         temp = prev.next;
+        //     }
+        //     else
+        //     {
+        //         prev = temp;
+        //         temp = temp.next;
+        //     }
+        // }
+
+        // prev.next = evenHead.next;
+        // return head;
     }
 }
