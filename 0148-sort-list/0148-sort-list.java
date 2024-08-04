b6f0d8570db26f7880e5b20798ec050b7febe939
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-04 15:08:21 +0530

    Time: 9 ms (92.16%), Space: 56.6 MB (57.11%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,31 +9,68 @@
  * }
  */
 class Solution {
+
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummyNode = new ListNode(0,null);
+        ListNode temp = dummyNode;
+
+        while(list1 != null && list2 != null)
+        {
+            if(list1.val <= list2.val)
+            {
+                temp.next = list1;
+                temp = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                temp.next = list2;
+                temp = list2;
+                list2 = list2.next;
+            }
+        }
+
+        if(list1 != null)
+        {
+            temp.next = list1;
+        }
+        else
+        {
+            temp.next = list2;
+        }
+
+        return dummyNode.next;
+    }
+
+    public ListNode getMiddle(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head.next; // set it as one step ahead
+        // becase 1 -> 2 -> 3 -> 4
+        //  middle = 2
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null)
         {
             return head;
         }
 
-        ListNode ptr = head;
-        List<Integer> list = new ArrayList<Integer>();
+        ListNode middleNode = getMiddle(head);
+        ListNode leftHead = head;
+        ListNode rightHead = middleNode.next;
+        middleNode.next = null;
 
-        while(ptr != null)
-        {
-            list.add(ptr.val);
-            ptr = ptr.next;
-        }
-
-        Collections.sort(list);
-        ptr = head;
-        int i = 0;
-        while(ptr != null)
-        {
-            ptr.val = list.get(i);
-            i++;
-            ptr = ptr.next;
-        }
-        return head;
-        
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+        return MergeTwoLists(leftHead,rightHead);
     }
 }
