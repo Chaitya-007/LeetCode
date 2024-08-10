ed0de476dbbd380ddfe637db3a87faeb393f3567
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-10 07:58:49 +0530

    Time: 0 ms (100%), Space: 44.8 MB (10.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -28,31 +28,8 @@ public class Solution {
             {
                 return true;
             }
-
         }
 
         return false;
-
-        // Brute Force
-        // HashMap<ListNode, Integer> map = new HashMap<ListNode,Integer> ();
-
-        // if(head == null)
-        // {
-        //     return false;
-        // }
-
-        // ListNode temp = head;
-
-        // while(temp != null)
-        // {
-        //     if(map.containsKey(temp))
-        //     {
-        //         return true;
-        //     }
-        //     map.put(temp,1);
-        //     temp = temp.next;
-        // }
-
-        // return false;
     }
 }
