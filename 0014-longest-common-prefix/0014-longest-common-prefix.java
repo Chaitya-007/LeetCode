3b91bce0a18b9c5ec152db6679f5e7c36799d96a
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-10 15:58:45 +0530

    Time: 9 ms (7.12%), Space: 41.5 MB (48.93%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -9,10 +9,12 @@ class Solution {
         int n = strs.length;
         int len = Math.min(strs[0].length(),strs[n-1].length());
         String temp = "";
+        String first = strs[0];
+        String last = strs[n-1];
 
         for(int i = 0; i < len; i++)
         {
-            if(strs[0].charAt(i) == strs[n - 1].charAt(i))
+            if(first.charAt(i) == last.charAt(i))
             {
                 temp += strs[0].charAt(i);
             }
