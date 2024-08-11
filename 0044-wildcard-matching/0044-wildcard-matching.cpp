68e1e471f8ebab0ea139e3273137fb0af0e031bd
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-11 20:19:48 +0530

    Time: 56 ms (41.32%), Space: 12.1 MB (86.21%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -171,61 +171,30 @@ public:
         vector<bool> prev(m+1,false);
         vector<bool> curr(m+1,false);
 
-        // prev[0] = true;
-
-        // curr[0] = false;
-
-
-
-        //   for(int i = 1; i <= n; i++)
-        // {
-        //     prev[0] = false;
-        //     for(int j = 1; j <= m; j++)
-        //     {
-
-        //           bool flag = true;
-        //     for(int x = 1; x <= j; x++)
-        //     {
-        //         if(p[x-1] != '*')
-        //         {
-        //             flag = false;
-        //             break;
-        //         }
-        //     }
-
-        //     curr[j] = flag;
-
-        //         if(p[j - 1] == '*') // change
-        //       {
-        //          curr[j] = curr[j-1] | prev[j];
-        //       }
-
-        //     else if(p[j - 1] == '?' || s[i - 1] == p[j - 1]) // change
-        //     {
-        //       curr[j] = prev[j-1];
-        //     }
-        //     else
-        //     {
-        //         curr[j] = false;
-
-        //     }
-
-        //     }
-
-        //     prev = curr;
-        // }
-
-        // return prev[m];
-
          prev[0] = true;
 
-    for (int j = 1; j <= m; j++) {
-        if (p[j - 1] == '*') {
-            prev[j] = prev[j - 1];
-        } else {
-            prev[j] = false;
-        }
-    }
+    // for (int j = 1; j <= m; j++) {
+    //     if (p[j - 1] == '*') {
+    //         prev[j] = prev[j - 1];
+    //     } else {
+    //         prev[j] = false;
+    //     }
+    // }
+
+     for(int j = 1; j <= m; j++)
+        {
+            bool flag = true;
+            for(int x = 1; x <= j; x++)
+            {
+                if(p[x-1] != '*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            prev[j] = flag;
+        } 
 
     for (int i = 1; i <= n; i++) {
         curr[0] = false; // Empty pattern can't match non-empty string
