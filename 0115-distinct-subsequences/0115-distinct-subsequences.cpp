e01aa0a6e3eaf17204a5bf51b7893b747da71e8c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-11 10:42:16 +0530

    Time: 1691 ms (18.11%), Space: 422 MB (5.24%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    int fun(int i, int j, string s, string t)
+    {
+        if(j < 0) return 1;
+        if(i < 0) return 0;
+
+        if(s[i] == t[j])
+        {
+            return fun(i-1,j-1,s,t) + fun(i-1,j,s,t);
+        }
+
+        return fun(i-1,j,s,t);
+    }
+
+    int funMemo(int i, int j, string s, string t,vector<vector<int>> &dp)
+    {
+        if(j < 0) return 1;
+        if(i < 0) return 0;
+
+        if(dp[i][j] != -1)
+        return dp[i][j];
+
+
+        if(s[i] == t[j])
+        {
+            return dp[i][j] = funMemo(i-1,j-1,s,t,dp) + funMemo(i-1,j,s,t,dp);
+        }
+
+        return dp[i][j] = funMemo(i-1,j,s,t,dp);
+    }
+
+    int numDistinct(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+
+        // return fun(n-1,m-1,s,t);
+
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        return funMemo(n-1,m-1,s,t,dp); 
+    }
+};
