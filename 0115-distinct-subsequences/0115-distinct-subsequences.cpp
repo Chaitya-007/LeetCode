fa1a9d878888a402bfa3d62d2a712cb0cd9bf0cb
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-11 12:28:55 +0530

    Time: 13 ms (80.84%), Space: 31.7 MB (30.7%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -64,7 +64,7 @@ public:
         vector<vector<double>> dp(n + 1,vector<double> (m + 1,0));
 
         for(int i = 0; i <= n; i++) dp[i][0] = 1;
-        for(int j = 1; j <= m; j++) dp[0][j] = 0;
+        // for(int j = 1; j <= m; j++) dp[0][j] = 0; 
 
         for(int i = 1; i <= n; i++)
         {
