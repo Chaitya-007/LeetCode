785174373966df43caf86883ffabbf909d9eee27
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-11 12:36:29 +0530

    Time: 6 ms (96.2%), Space: 8 MB (85.39%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -61,27 +61,49 @@ public:
         // vector<vector<int>> dp(n + 1,vector<int>(m + 1,-1));
         // return funMemoModified(n,m,s,t,dp);
 
-        vector<vector<double>> dp(n + 1,vector<double> (m + 1,0));
+        // tabulation
+        // vector<vector<double>> dp(n + 1,vector<double> (m + 1,0));
 
-        for(int i = 0; i <= n; i++) dp[i][0] = 1;
-        // for(int j = 1; j <= m; j++) dp[0][j] = 0; 
+        // for(int i = 0; i <= n; i++) dp[i][0] = 1;
+        // // for(int j = 1; j <= m; j++) dp[0][j] = 0; // will also be okay becasue we are already initialising it to 0 while declaring
+
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     for(int j = 1; j <= m; j++)
+        //     {
+        //         if(s[i-1] == t[j-1])
+        //         {
+        //             dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+        //         }
+        //         else
+        //         {
+        //             dp[i][j] = dp[i-1][j];
+        //         }
+        //     }
+        // }
+
+        // return (int)dp[n][m];
+
+
+        // Space Optimization
+        vector<double> prev(m + 1,0);
+        vector<double> curr(m + 1,0);
+
+        prev[0] = curr[0] = 1;
 
         for(int i = 1; i <= n; i++)
         {
-            for(int j = 1; j <= m; j++)
-            {
+             for (int j = 1; j <= m; j++)
+             {
                 if(s[i-1] == t[j-1])
-                {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
+                curr[j] = prev[j-1] + prev[j];
                 else
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
+                curr[j] = prev[j];
+             }
+             prev = curr;
         }
 
-        return (int)dp[n][m];
+        return (int)prev[m];
 
     }
 };
