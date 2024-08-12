d71768922a09421a3f54e604ec228c2f98b7833e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-12 13:30:05 +0530

    Time: 344 ms (10%), Space: 16.4 MB (69.16%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -55,66 +55,66 @@ public:
         // vector<vector<int>>dp (n,vector<int>(totalSum + 1, -1));
         // return fundp(n-1,(totalSum/2),nums,dp);
 
-        // vector<vector<bool>>dp (n,vector<bool>((totalSum/2) + 1, false));
+        vector<vector<bool>>dp (n,vector<bool>((totalSum/2) + 1, false));
         
-        // for(int ind = 0; ind < n; ind++)
-        // {
-        //     dp[ind][0] = true;
-        // }
-
-        // if(nums[0] <= (totalSum/2))
-        // {
-        //     dp[0][nums[0]] = true;
-        // }
-
-        // for(int ind = 1; ind < n; ind++)
-        // {
-        //     for(int target = 1; target <= (totalSum/2); target++)
-        //     {
-        //         bool notTake = dp[ind - 1][target];
-        //         bool take = false;
-
-        //         if(nums[ind] <= target)
-        //         {
-        //             take = dp[ind - 1][target - nums[ind]];
-        //         }
-
-        //       dp[ind][target] = notTake | take;
-        //     }
-        // }
-
-        // return dp[n-1][totalSum/2];
-
-        
-        vector<bool> prev((totalSum/2) + 1,false);
-        vector<bool> curr((totalSum/2) + 1,false);
-
-        prev[0] = true;
-        curr[0] = true;
+        for(int ind = 0; ind < n; ind++)
+        {
+            dp[ind][0] = true;
+        }
 
         if(nums[0] <= (totalSum/2))
         {
-            prev[nums[0]] = true;
+            dp[0][nums[0]] = true;
         }
 
         for(int ind = 1; ind < n; ind++)
         {
             for(int target = 1; target <= (totalSum/2); target++)
             {
-                bool notTake = prev[target];
+                bool notTake = dp[ind - 1][target];
                 bool take = false;
 
                 if(nums[ind] <= target)
                 {
-                    take = prev[target - nums[ind]];
+                    take = dp[ind - 1][target - nums[ind]];
                 }
 
-              curr[target] = notTake | take;
+              dp[ind][target] = notTake | take;
             }
-            prev = curr;
         }
 
-        return prev[totalSum/2];
+        return dp[n-1][totalSum/2];
+
+        
+        // vector<bool> prev((totalSum/2) + 1,false);
+        // vector<bool> curr((totalSum/2) + 1,false);
+
+        // prev[0] = true;
+        // curr[0] = true;
+
+        // if(nums[0] <= (totalSum/2))
+        // {
+        //     prev[nums[0]] = true;
+        // }
+
+        // for(int ind = 1; ind < n; ind++)
+        // {
+        //     for(int target = 1; target <= (totalSum/2); target++)
+        //     {
+        //         bool notTake = prev[target];
+        //         bool take = false;
+
+        //         if(nums[ind] <= target)
+        //         {
+        //             take = prev[target - nums[ind]];
+        //         }
+
+        //       curr[target] = notTake | take;
+        //     }
+        //     prev = curr;
+        // }
+
+        // return prev[totalSum/2];
 
     }
 };
