79063d5c89c71f84d8cba65e1f47e5df58a1ff52
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-13 10:31:02 +0530

    Time: 1727 ms (12.64%), Space: 422 MB (5.51%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -25,13 +25,29 @@ public:
         return dp[i][j] = fundp(i-1,j,s,t,dp);
     }
 
+     int fundpOpt(int i, int j, string s, string t, vector<vector<int>> &dp)
+    {
+        if(j == 0) return 1;
+        if(i == 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i-1] == t[j-1]) 
+        return dp[i][j] = fundpOpt(i-1,j-1,s,t,dp) + fundpOpt(i-1,j,s,t,dp);
+
+        return dp[i][j] = fundpOpt(i-1,j,s,t,dp);
+    }
+
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
 
         // return fun(n-1,m-1,s,t);
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return fundp(n-1,m-1,s,t,dp);
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // return fundp(n-1,m-1,s,t,dp);
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return fundpOpt(n,m,s,t,dp);
     }
 };
