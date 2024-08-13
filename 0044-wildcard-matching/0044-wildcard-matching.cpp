d4ee0bd88febb29d27a525374ea1310ce10fabc8
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-13 11:36:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -83,34 +83,63 @@ public:
         // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         // return fundpOpt(n,m,s,p,dp);
 
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
-        dp[0][0] = true;
-        for(int i = 1; i <= n; i++) dp[i][0] = false;
+        // vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
+        // dp[0][0] = true;
+        // for(int i = 1; i <= n; i++) dp[i][0] = false;
 
-        for(int j = 1; j <= m; j++)
-        {
-            bool flag = true;
-            for(int m = 1; m <= j; m++) // change
-            {
-                if(p[m-1] != '*') // change
-                flag = false;
-            }
-            dp[0][j] = flag;
-        }
+        // for(int j = 1; j <= m; j++)
+        // {
+        //     bool flag = true;
+        //     for(int m = 1; m <= j; m++) // change
+        //     {
+        //         if(p[m-1] != '*') // change
+        //         flag = false;
+        //     }
+        //     dp[0][j] = flag;
+        // }
+
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     for(int j = 1; j <= m; j++)
+        //     {
+        //         if(p[j-1] == '*')
+        //          dp[i][j] = dp[i-1][j] | dp[i][j-1];
+        //         else if(s[i-1] == p[j-1] || p[j-1] == '?')
+        //          dp[i][j] = dp[i-1][j-1];
+        //         else
+        //          dp[i][j] = false;
+        //     }
+        // }
+
+        // return dp[n][m];
+
+         vector<int> prev(m+1,false);
+         vector<int> curr(m+1,false);
+         prev[0] = true;
 
         for(int i = 1; i <= n; i++)
         {
+            curr[0] = false;
             for(int j = 1; j <= m; j++)
             {
+                bool flag = true;
+                for(int m = 1; m <= j; m++) // change
+                {
+                    if(p[m-1] != '*') // change
+                    flag = false;
+                }
+                prev[j] = flag;
+
                 if(p[j-1] == '*')
-                 dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                 curr[j] = prev[j] | curr[j-1];
                 else if(s[i-1] == p[j-1] || p[j-1] == '?')
-                 dp[i][j] = dp[i-1][j-1];
+                 curr[j] = prev[j-1];
                 else
-                 dp[i][j] = false;
+                 curr[j] = false;
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
