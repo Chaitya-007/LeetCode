442baa44cc9425abf8c36717d6a4376975401d5b
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-13 11:07:31 +0530

    Time: 9 ms (49.8%), Space: 13 MB (31.16%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -47,9 +47,24 @@ public:
         // vector<vector<int>> dp(n,vector<int> (m,-1));
         // return fundp(n-1,m-1,word1,word2,dp);
 
-         vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-         return fundpOpt(n,m,word1,word2,dp);
-         
+        //  vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        //  return fundpOpt(n,m,word1,word2,dp);
 
+        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(word1[i-1] == word2[j-1])
+                 dp[i][j] = dp[i-1][j-1];
+                else
+                 dp[i][j] = 1 + min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+            }
+        }
+         
+        return dp[n][m];
     }
 };
