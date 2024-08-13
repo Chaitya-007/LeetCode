ecf78434a29790baf7ec69a7e34e112e03a667ce
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-13 10:50:58 +0530

    Time: 4 ms (98.62%), Space: 8 MB (88.73%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -67,37 +67,37 @@ public:
         // }
         // return dp[n][m];
 
-        vector<double> prev(m+1,0);
-        vector<double> curr(m+1,0);
-
-        prev[0] = 1;
-        curr[0] = 1;
-
-        for(int i = 1; i <= n; i++)
-        {
-            curr[0] = 1;
-            for(int j = 1; j <= m; j++)
-            {
-                if(s[i-1] == t[j-1]) 
-                 curr[j] = prev[j-1] + prev[j];
-                else
-                 curr[j] = prev[j];
-            }
-            prev = curr;
-        }
-        return (int)prev[m];
-
         // vector<double> prev(m+1,0);
+        // vector<double> curr(m+1,0);
+
         // prev[0] = 1;
+        // curr[0] = 1;
 
         // for(int i = 1; i <= n; i++)
         // {
-        //     for(int j = m; j >= 1; j--)
+        //     // curr[0] = 1; this also works
+        //     for(int j = 1; j <= m; j++)
         //     {
         //         if(s[i-1] == t[j-1]) 
-        //          prev[j] = prev[j-1] + prev[j];
+        //          curr[j] = prev[j-1] + prev[j];
+        //         else
+        //          curr[j] = prev[j];
         //     }
+        //     prev = curr;
         // }
         // return (int)prev[m];
+
+        vector<double> prev(m+1,0);
+        prev[0] = 1;
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = m; j >= 1; j--)
+            {
+                if(s[i-1] == t[j-1]) 
+                 prev[j] = prev[j-1] + prev[j];
+            }
+        }
+        return (int)prev[m];
     }
 };
