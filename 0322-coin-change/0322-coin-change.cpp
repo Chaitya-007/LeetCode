9b5136790f7b1480f8180dfecfd848ff11ec7fe3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-17 14:11:58 +0530

    Time: 55 ms (19.62%), Space: 47.1 MB (37.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -44,8 +44,42 @@ public:
         int n = coins.size();
         // int ans = fun(n-1,amount,coins);
 
-        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
-        int ans = fundp(n-1,amount,coins,dp);
+        // vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+        // int ans = fundp(n-1,amount,coins,dp);
+
+        // Tabulation
+        vector<vector<int>> dp(n, vector<int> (amount + 1, 0));
+        for(int T = 0; T <= amount; T++)
+        {
+            if(T%coins[0] == 0)
+            {
+            dp[0][T] = T/coins[0];
+            }
+            else
+            {
+                dp[0][T] = 1e9;
+            }
+        }
+
+        for(int ind = 1; ind < n; ind++)
+        {
+            for(int target = 0; target <= amount; target++)
+            {
+                int notTake = dp[ind - 1][target];
+                int take = INT_MAX;
+
+                if(coins[ind] <= target) 
+                {
+                    take = 1 + dp[ind][target - coins[ind]]; // ************* fun(ind) => stay on the same index
+                }
+
+                 dp[ind][target] = min(take,notTake);
+            }
+        }
+
+
+
+        int ans = dp[n-1][amount];
         if(ans >= 1e9) return -1;
         return ans;
     }
