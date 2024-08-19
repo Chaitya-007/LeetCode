acbc79cf014a5dde23112ef1fd64831df469cd3c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-19 12:48:50 +0530

    Time: 0 ms (100%), Space: 20.3 MB (27.14%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    int fun(int ind, int target, vector<int> &arr)
+    {
+        if(ind == 0)
+        {
+        if(target == 0 || target%arr[0] == 0) return 1;
+            else return 0;
+        }
+
+        int notTake = fun(ind - 1, target, arr);
+        int take = 0;
+        if(arr[ind] <= target)
+        {
+            take = fun(ind, target - arr[ind], arr);
+        }
+
+        return take + notTake;
+    }
+
+     int fundp(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
+    {
+        if(ind == 0)
+        {
+        if(target == 0 || target%arr[0] == 0) return 1;
+            else return 0;
+        }
+
+        if(dp[ind][target] != -1) return dp[ind][target];
+
+        int notTake = fundp(ind - 1, target, arr, dp);
+        int take = 0;
+        if(arr[ind] <= target)
+        {
+            take = fundp(ind, target - arr[ind], arr, dp);
+        }
+
+        return dp[ind][target] = take + notTake;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        // return fun(n - 1, amount, coins);
+
+        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+        return fundp(n-1, amount, coins, dp);
+    }
+};
