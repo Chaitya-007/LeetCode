e76d137582e28415764de4df03b9deb5442f6d90
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-19 13:18:37 +0530

    Time: 14 ms (39.78%), Space: 8.9 MB (84.9%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -44,41 +44,13 @@ public:
         // vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
         // return fundp(n-1, amount, coins, dp);
 
-        vector<vector<int>> dp(n, vector<int> (amount + 1, 0));
+        // vector<vector<int>> dp(n, vector<int> (amount + 1, 0));
       
-        for(int target = 0; target <= amount; target++)
-        {
-            if(target%coins[0] == 0)
-            {
-                dp[0][target] = (target%coins[0] == 0);
-            }
-        }
-
-        for(int ind = 1; ind < n; ind++)
-        {
-            for(int target = 0; target <= amount; target++)
-            {
-                 int notTake = dp[ind - 1][target];
-                int take = 0;
-                if(coins[ind] <= target)
-                {
-                    take = dp[ind][ target - coins[ind]];
-                }
-
-                 dp[ind][target] = take + notTake;
-            }
-        }
-
-        return dp[n-1][amount];
-
-        // vector<int> prev(amount + 1, 0);
-        // vector<int> curr(amount + 1, 0);
-        // prev[0] = 1;
-        // for(int target = 1; target <= amount; target++)
+        // for(int target = 0; target <= amount; target++)
         // {
         //     if(target%coins[0] == 0)
         //     {
-        //         prev[target] = 1;
+        //         dp[0][target] = (target%coins[0] == 0);
         //     }
         // }
 
@@ -86,19 +58,46 @@ public:
         // {
         //     for(int target = 0; target <= amount; target++)
         //     {
-        //          int notTake = prev[target];
+        //          int notTake = dp[ind - 1][target];
         //         int take = 0;
         //         if(coins[ind] <= target)
         //         {
-        //             take = curr[ target - coins[ind]];
+        //             take = dp[ind][ target - coins[ind]];
         //         }
 
-        //          curr[target] = take + notTake;
+        //          dp[ind][target] = take + notTake;
         //     }
-
-        //     prev = curr;
         // }
 
-        // return prev[amount];
+        // return dp[n-1][amount];
+
+        vector<int> prev(amount + 1, 0);
+        vector<int> curr(amount + 1, 0);
+        for(int target = 0; target <= amount; target++)
+        {
+            if(target%coins[0] == 0)
+            {
+                prev[target] = (target%coins[0] == 0);
+            }
+        }
+
+        for(int ind = 1; ind < n; ind++)
+        {
+            for(int target = 0; target <= amount; target++)
+            {
+                 int notTake = prev[target];
+                int take = 0;
+                if(coins[ind] <= target)
+                {
+                    take = curr[ target - coins[ind]];
+                }
+
+                 curr[target] = take + notTake;
+            }
+
+            prev = curr;
+        }
+
+        return prev[amount];
     }
 };
