a3f072c42293182816132a4c816288ee5e7a34fc
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-20 20:32:06 +0530

    Time: 519 ms (15.6%), Space: 295 MB (8.4%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    int fun(int ind, int prev_ind, int n, vector<int> &nums)
+    {
+        if(ind == n) return 0;
+
+        int len = fun(ind+1, prev_ind, n, nums);
+
+        if(prev_ind == -1 || nums[ind] > nums[prev_ind])
+        {
+            len = max(len, 1 + fun(ind+1, ind, n, nums));
+        }
+
+        return len;
+    }
+
+    int fundp(int ind, int prev_ind, int n, vector<int> &nums, vector<vector<int>> &dp)
+    {
+        if(ind == n) return 0;
+
+        if(dp[ind][prev_ind + 1] != -1) return dp[ind][prev_ind + 1];
+
+        int len = fundp(ind+1, prev_ind, n, nums, dp);
+
+        if(prev_ind == -1 || nums[ind] > nums[prev_ind])
+        {
+            len = max(len, 1 + fundp(ind+1, ind, n, nums, dp));
+        }
+
+        return dp[ind][prev_ind + 1] = len;
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+
+        int n = nums.size();
+        // return fun(0,-1,n,nums); 
+
+        vector<vector<int>> dp(n, vector<int> (n+1,-1));
+        return fundp(0,-1,n,nums,dp);  
+    }
+};
