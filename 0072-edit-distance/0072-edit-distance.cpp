a97a894ad62017eceb89b4919aae342ce5e59e9e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-22 18:41:53 +0530

    Time: 57 ms (10.85%), Space: 82.4 MB (9.3%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,89 +1,67 @@
 class Solution {
 public:
-
-    int fun(int i, int j, string s, string t)
+    int fun(int i, int j, string s, string t, vector<vector<int>> &dp)
     {
-        if(j < 0) return i + 1;
-        if(i < 0) return j + 1;
-
-        if(s[i] == t[j])
-        return fun(i-1,j-1,s,t);
-
-        return 1 + min(fun(i-1,j-1,s,t),min(fun(i,j-1,s,t),fun(i-1,j,s,t)));
-    }
-
-    int fundp(int i, int j, string s, string t,vector<vector<int>> &dp)
-    {
-        if(j < 0) return i + 1;
+        if(j < 0) return  i + 1;
         if(i < 0) return j + 1;
 
         if(dp[i][j] != -1) return dp[i][j];
 
         if(s[i] == t[j])
-        return dp[i][j] = fundp(i-1,j-1,s,t,dp);
+        {
+            return dp[i][j] = fun(i-1,j-1,s,t,dp);
+        }
 
-        return dp[i][j] = 1 + min(fundp(i-1,j-1,s,t,dp),min(fundp(i,j-1,s,t,dp),fundp(i-1,j,s,t,dp)));
+        return dp[i][j] =  1 + min(fun(i-1,j-1,s,t,dp), min(fun(i-1,j,s,t,dp), fun(i,j-1,s,t,dp) ));
     }
 
-    int fundpOpt(int i, int j, string s, string t,vector<vector<int>> &dp)
+     int funOpt(int i, int j, string s, string t, vector<vector<int>> &dp)
     {
-        if(j == 0) return i; // very very important
-        if(i == 0) return j; // very very important
+        if(j == 0) return  i;
+        if(i == 0) return j;
 
         if(dp[i][j] != -1) return dp[i][j];
 
         if(s[i-1] == t[j-1])
-        return dp[i][j] = fundpOpt(i-1,j-1,s,t,dp);
+        {
+            return funOpt(i-1,j-1,s,t,dp);
+        }
 
-        return dp[i][j] = 1 + min(fundpOpt(i-1,j-1,s,t,dp),min(fundpOpt(i,j-1,s,t,dp),fundpOpt(i-1,j,s,t,dp)));
+        return  1 + min(funOpt(i-1,j-1,s,t,dp), min(funOpt(i-1,j,s,t,dp), funOpt(i,j-1,s,t,dp) ));
     }
 
     int minDistance(string word1, string word2) {
-     int n = word1.length();
-     int m = word2.length();
+        int n = word1.length();
+        int m = word2.length();
 
-    //  return fun(n-1,m-1,word1,word2);   
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return fun(n-1,m-1,word1,word2,dp);
 
-        // vector<vector<int>> dp(n,vector<int> (m,-1));
-        // return fundp(n-1,m-1,word1,word2,dp);
+        //  vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        // return funOpt(n,m,word1,word2,dp);
 
-        //  vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        //  return fundpOpt(n,m,word1,word2,dp);
+        // vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        // for(int i = 0; i <= n; i++)
+        // {
+        //     dp[i][0] = i; 
+        // }
 
-        // vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        // for(int i = 0; i <= n; i++) dp[i][0] = i;
-        // for(int j = 0; j <= m; j++) dp[0][j] = j;
+        // for(int j = 1; j <= m; j++)
+        // {
+        //     dp[0][j] = j;
+        // }
 
         // for(int i = 1; i <= n; i++)
         // {
         //     for(int j = 1; j <= m; j++)
         //     {
-        //         if(word1[i-1] == word2[j-1])
-        //          dp[i][j] = dp[i-1][j-1];
-        //         else
-        //          dp[i][j] = 1 + min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+        //             if(s[i-1] == t[j-1])
+        //             {
+        //                 return funOpt(i-1,j-1,s,t,dp);
+        //             }
+
+        //             return  1 + min(funOpt(i-1,j-1,s,t,dp), min(funOpt(i-1,j,s,t,dp), funOpt(i,j-1,s,t,dp) ));
         //     }
         // }
-         
-        // return dp[n][m];
-
-        vector<int> prev(m+1,0);
-        vector<int> curr(m+1,0);
-        for(int j = 0; j <= m; j++) prev[j] = j;
-
-        for(int i = 1; i <= n; i++)
-        {
-            curr[0] = i;
-            for(int j = 1; j <= m; j++)
-            {
-                if(word1[i-1] == word2[j-1])
-                 curr[j] = prev[j-1];
-                else
-                 curr[j] = 1 + min(prev[j-1],min(curr[j-1],prev[j]));
-            }
-            prev = curr;
-        }
-         
-        return prev[m];
     }
 };
