7c177971c8aec2d93b8b456ca6303de7ff3368b0
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-22 18:46:39 +0530

    Time: 4 ms (83.67%), Space: 12.8 MB (49.9%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -37,31 +37,35 @@ public:
         // vector<vector<int>> dp(n, vector<int> (m, -1));
         // return fun(n-1,m-1,word1,word2,dp);
 
-         vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
-        return funOpt(n,m,word1,word2,dp);
+        //  vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        // return funOpt(n,m,word1,word2,dp);
 
-        // vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
-        // for(int i = 0; i <= n; i++)
-        // {
-        //     dp[i][0] = i; 
-        // }
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        for(int i = 0; i <= n; i++)
+        {
+            dp[i][0] = i; 
+        }
 
-        // for(int j = 1; j <= m; j++)
-        // {
-        //     dp[0][j] = j;
-        // }
+        for(int j = 1; j <= m; j++)
+        {
+            dp[0][j] = j;
+        }
 
-        // for(int i = 1; i <= n; i++)
-        // {
-        //     for(int j = 1; j <= m; j++)
-        //     {
-        //             if(s[i-1] == t[j-1])
-        //             {
-        //                 return funOpt(i-1,j-1,s,t,dp);
-        //             }
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                    if(word1[i-1] == word2[j-1])
+                    {
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                    else
+                    {
+                    dp[i][j] =  1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]) );
+                    }
+            }
+        }
 
-        //             return  1 + min(funOpt(i-1,j-1,s,t,dp), min(funOpt(i-1,j,s,t,dp), funOpt(i,j-1,s,t,dp) ));
-        //     }
-        // }
+        return dp[n][m];
     }
 };
