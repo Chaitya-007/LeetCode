d087fe29fac6da8882c80b5ebaa0d0d9cdca747d
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-10-20 23:12:41 +0530

    Time: 7 ms (70.18%), Space: 45.1 MB (43.18%) - LeetHub

--- 2825-minimize-string-length/2825-minimize-string-length.java
+++ 2825-minimize-string-length/2825-minimize-string-length.java
@@ -2,26 +2,26 @@ class Solution {
     public int minimizedStringLength(String s) {
         HashSet<Character> ht = new HashSet<Character>();
 
-        // for(char ch : s.toCharArray())
-        // {
-        //     ht.add(ch);
-        // }
+        for(char ch : s.toCharArray())
+        {
+            ht.add(ch);
+        }
 
-        // return ht.size();
+        return ht.size();
 
         // M2
         // return (int) s.chars().distinct().count();
 
         // M3
-        String buffer = "";
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(!buffer.contains(String.valueOf(s.charAt(i))))
-            {
-                buffer += s.charAt(i);
-            }
-        }
+        // String buffer = "";
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     if(!buffer.contains(String.valueOf(s.charAt(i))))
+        //     {
+        //         buffer += s.charAt(i);
+        //     }
+        // }
 
-        return buffer.length();
+        // return buffer.length();
     }
 }
