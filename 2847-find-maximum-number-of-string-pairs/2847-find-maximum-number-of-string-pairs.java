e4d526535312fbee04ef66afc06ab92355901e76
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-10-20 21:36:21 +0530

    Time: 3 ms (86.58%), Space: 43.2 MB (81.83%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
@@ -1,29 +1,47 @@
 class Solution {
     public int maximumNumberOfStringPairs(String[] words) {
-        // HashMap<String, Integer> map = new HashMap<String, Integer>();
         int n = words.length;
-        boolean[] arr = new boolean[words.length];
-        Arrays.fill(arr,false);
+        // boolean[] arr = new boolean[words.length];
+        // Arrays.fill(arr,false);
+        // int res = 0;
+
+        // for(int i = 0; i < n; i++)
+        // {
+        //     if(arr[i])
+        //     {
+        //         continue;
+        //     }
+        //     for(int j = 0; j < n; j++)
+        //     {
+        //         if(i!=j)
+        //         {
+        //             StringBuilder sb = new StringBuilder(words[j]);
+        //             if(words[i].equals(sb.reverse().toString()))
+        //             {
+        //                 res++;
+        //                 arr[i] = true;
+        //                 arr[j] = true;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return res;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
         int res = 0;
 
-        for(int i = 0; i < n; i++)
+
+        for(int i = 0; i < n; i++ )
         {
-            if(arr[i])
+            StringBuilder sb = new StringBuilder(words[i]);
+
+            if(map.containsKey(words[i]))
             {
-                continue;
+                res++;
             }
-            for(int j = 0; j < n; j++)
+            else
             {
-                if(i!=j)
-                {
-                    StringBuilder sb = new StringBuilder(words[j]);
-                    if(words[i].equals(sb.reverse().toString()))
-                    {
-                        res++;
-                        arr[i] = true;
-                        arr[j] = true;
-                    }
-                }
+                map.put(sb.reverse().toString(),1);
             }
         }
 
