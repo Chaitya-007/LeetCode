a257028b61cdb433c14d28a0fc74883aa613ce4f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-10-20 20:09:56 +0530

    Time: 1 ms (64.74%), Space: 41.9 MB (7.64%) - LeetHub

--- 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.java
@@ -4,14 +4,14 @@ class Solution {
         String str_first = "";
         String str_second = "";
 
-        for(int i = 0; i < word1.length; i++)
+        for(int i = 0; i < word1.length; i++) // use .length for arrays => not .length() => because .length is a property not method
         {
-            str_first += word1[i];
+           str_first = str_first.concat(word1[i]); // while using concat() we have to assign it to string again
         }
 
         for(int i = 0; i < word2.length; i++)
         {
-            str_second += word2[i];
+            str_second += word2[i]; // this way is also allowed
         }
 
         if(str_first.equals(str_second))
