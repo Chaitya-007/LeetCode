14ec547206cc61a765b46b7050782abebc76515e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-10-20 21:32:20 +0530

    Time: 8 ms (62.67%), Space: 45 MB (49.57%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
@@ -1,59 +1,32 @@
 class Solution {
-    public String reversestring(String str)
-    {
-        StringBuilder st = new StringBuilder(str);
-        st.reverse();
-        return st.toString();
-    }
     public int maximumNumberOfStringPairs(String[] words) {
-        // int cnt_pair = 0;
-        // int[] visited = new int[words.length];
-        // Arrays.fill(visited,0);
+        // HashMap<String, Integer> map = new HashMap<String, Integer>();
+        int n = words.length;
+        boolean[] arr = new boolean[words.length];
+        Arrays.fill(arr,false);
+        int res = 0;
 
-        // for(int i = 0; i < words.length; i++)
-        // {
-        //     if(visited[i] == 0)
-        //     {
-        //     String revstr = reversestring(words[i]);
-            
-        //     for(int j = i + 1; j < words.length; j++)
-        //     {
-        //         if(visited[j] == 0 && revstr.equals(words[j]))
-        //         {
-        //             cnt_pair++;
-        //             visited[i] = 1;
-        //             visited[j] = 1;
-        //             break;
-        //         }
-        //     }
-
-        //     }
-
-
-        // }
-
-        // return cnt_pair;
-
-        // M 2
-        int cnt = 0;
-        Map<String,Integer> mp = new HashMap<String,Integer>();
-
-        for(String str : words)
+        for(int i = 0; i < n; i++)
         {
-            String rev = reversestring(str);
-            if(mp.containsKey(str)) // check the string of array => do not reverse it and check it
+            if(arr[i])
             {
-
-                cnt++;
-                mp.remove(rev);
+                continue;
             }
-            else
+            for(int j = 0; j < n; j++)
             {
-                mp.put(rev,1);
+                if(i!=j)
+                {
+                    StringBuilder sb = new StringBuilder(words[j]);
+                    if(words[i].equals(sb.reverse().toString()))
+                    {
+                        res++;
+                        arr[i] = true;
+                        arr[j] = true;
+                    }
+                }
             }
         }
 
-        return cnt;
-
+        return res;
     }
 }
