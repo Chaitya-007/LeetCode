6dde7fc201cdc65f62d1dbec6f853854afbea90f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-08 20:50:34 +0530

    Time: 3 ms (51.95%), Space: 42.3 MB (32.88%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -1,59 +1,30 @@
 class Solution {
-
-    public static int convert(String str, int l, int r)
-    {
-        int num = 0;
-        for(int i = l; i <= r; i++)
-        {
-            char ch = str.charAt(i);
-            int val = (int)(ch - '0');
-            num = val + (num*10);
-        }
-
-        return num;
-    }
-
-
     public String largestGoodInteger(String num) {
-        int l = 0;
-        int r = 1;
-        int n = num.length();
+        int p = 0;
+        int q = 1;
+        int r = 2;
         int maxo = -1;
+        int n = num.length();
 
-        while(l < n && r < n)
+        while(r < n)
         {
-            if(num.charAt(l) != num.charAt(r))
-            {
-                l++;
-                r++;
-            }
-            else
-            {
-                while(l < n && r < n)
-                {
-                    if((r - l) == 2 && num.charAt(l) == num.charAt(r))
-                    {
-                        int val = convert(num, l, r);
-                        if(maxo < val)
-                        {
-                            maxo = val;
-                        }
-                        l = r + 1;
-                        r = l + 1;
-                        break;
-                    }
+            char ch1 = num.charAt(p);
+            char ch2 = num.charAt(q);
+            char ch3 = num.charAt(r);
 
-                    if(num.charAt(l) == num.charAt(r))
-                    {
-                        r++;
-                    }
-                    else
-                    {
-                        l++;
-                        break;
-                    }
+            if(ch1 == ch2 && ch2 == ch3)
+            {
+                String temp = num.substring(p,r+1);
+                int val = Integer.valueOf(temp);
+                if(maxo < val)
+                {
+                    maxo = val;
                 }
             }
+
+            p++;
+            q++;
+            r++;
         }
 
         if(maxo == -1)
@@ -66,15 +37,7 @@ class Solution {
             return "000";
         }
 
-        String str = "";
-        while(maxo > 0)
-        {
-            int rem = maxo%10;
-            maxo = maxo/10;
-            char ch = (char)(rem + '0');
-            str += ch;
-        }
-
+        String str = String.valueOf(maxo);
         return str;
     }
 }
