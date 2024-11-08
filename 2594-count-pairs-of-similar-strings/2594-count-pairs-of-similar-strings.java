be5c0e9df92ea4910056222ee36114acda820049
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-08 18:32:23 +0530

    Time: 522 ms (5.13%), Space: 44.7 MB (49.64%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.java
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.java
@@ -1,33 +1,42 @@
 class Solution {
     public int similarPairs(String[] words) {
-
+        int[] visited = new int[words.length];
+        Arrays.fill(visited,0);
         int cnt = 0;
 
         for(int i = 0; i < words.length; i++)
         {
-        Set<Character> s1 = new HashSet<Character>();
+            Set<Character> hset1 = new HashSet<Character>();
+           
+                String str = words[i];
+                for(int k = 0; k < str.length(); k++)
+                {
+                    hset1.add(str.charAt(k));
+                }
+
+
+                for(int j = 0; j < words.length; j++)
+                {
+                    if(i != j)
+                    {
+                        String temp = words[j];
+                        Set<Character> hset2 = new HashSet<Character>();
+                        for(int k = 0; k < temp.length(); k++)
+                        {
+                            hset2.add(temp.charAt(k));
+                        }
+
+                        if(hset1.equals(hset2))
+                        {
+                            visited[i] = 1;
+                            visited[j] = 1;
+                            cnt++;
+                        }
+                    }
+                }
             
-            for(int k = 0; k < words[i].length(); k++)
-            {
-                s1.add(words[i].charAt(k));
-            }
-
-            for(int j = i + 1; j < words.length; j++)
-            {
-                Set<Character> s2 = new HashSet<Character>();
-
-                for(int u = 0; u < words[j].length(); u++)
-                {
-                    s2.add(words[j].charAt(u));
-                }
-
-                if(s1.equals(s2))
-                {
-                    cnt++;
-                }
-            }
         }
 
-        return cnt;
+        return cnt/2;
     }
 }
