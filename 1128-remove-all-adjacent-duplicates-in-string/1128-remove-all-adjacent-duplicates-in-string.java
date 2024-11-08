fe17021723b70ae3678fa3ff38f82135d877fec4
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-08 19:06:52 +0530

    Time: 41 ms (20.74%), Space: 45.2 MB (74.67%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,56 +1,36 @@
 class Solution {
     public String removeDuplicates(String s) {
-        
-        // Stack<Character> st = new Stack<Character>();
+        Stack<Character> st = new Stack<Character>();
 
-        // for(int i = 0; i < s.length(); i++)
-        // {
-        //     if(st.isEmpty())
-        //     {
-        //         st.push(s.charAt(i));
-        //     }
-        //     else 
-        //     {
-        //         if(st.peek() == s.charAt(i))
-        //         {
-        //             st.pop();
-        //         }
-        //         else
-        //         {
-        //             st.push(s.charAt(i));
-        //         }
-        //     }
-        // }
-
-        // StringBuilder sb = new StringBuilder("");
-
-        // while(!st.isEmpty())
-        // {
-        //     char ch = st.pop();
-        //     sb.append(ch);
-        // }
-
-        // sb.reverse();
-
-        // return sb.toString();
-
-        StringBuilder sb = new StringBuilder("");
-
-
-        for(char ch : s.toCharArray())
+        for(int i = 0; i < s.length(); i++)
         {
-            int len = sb.length();
-
-            if(len > 0 && ch == sb.charAt(len - 1))
+            char ch = s.charAt(i);
+            if(st.isEmpty())
             {
-                sb.deleteCharAt(len - 1);
+                st.push(ch);
             }
             else
             {
-                sb.append(ch);
+                if(st.peek() == ch)
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(ch);
+                }
             }
         }
 
-        return sb.toString();
+        StringBuilder sb = new StringBuilder("");
+
+        while(!st.isEmpty())
+        {
+            char ch = st.peek();
+            sb.append(ch);
+            st.pop();
+        }
+
+        return sb.reverse().toString();
     }
 }
