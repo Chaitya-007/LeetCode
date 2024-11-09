3f77ed047d2a4f71d35b6daabfd87086a8f09980
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-09 17:09:00 +0530

    Time: 1 ms (98.42%), Space: 40.8 MB (99.38%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,38 +1,36 @@
 class Solution {
-
-    public static int countchar(String str, char ch)
-    {
-        int freq = 0;
-        char[] charray = str.toCharArray();
-
-        for(char val : charray)
-        {
-            if(val == ch)
-            {
-                freq++;
-            }
-        }
-
-        return freq;
-    }
-
     public int maxScore(String s) {
-        int maxo = 0;
-    
+        int zero = 0;
+        int one = 0;
+        int totones = 0;
         int n = s.length();
-        for(int i = 0; i <= n - 2; i++)
+        for(int i = 0; i < n; i++)
         {
-            String left_part = s.substring(0,i+1);
-            String right_part = s.substring(i+1,n);
-            int leftcnt = countchar(left_part,'0');
-            int rightcnt = countchar(right_part,'1');
-            int sum = leftcnt + rightcnt;
-            if(maxo < sum)
+            if(s.charAt(i) == '1')
             {
-                maxo = sum;
+                totones++;
             }
         }
 
-        return maxo;
+        int res = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(s.charAt(i) == '0')
+            {
+                zero++;
+            }
+            else
+            {
+                one++;
+            }
+
+            if(i != (n-1))
+            {
+                res = Math.max(zero - one + totones, res);
+            }
+        }
+
+        return res;
     }
 }
