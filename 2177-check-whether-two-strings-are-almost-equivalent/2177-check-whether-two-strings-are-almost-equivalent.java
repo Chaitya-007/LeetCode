8c38acaaef4bf472937ae26e2a608ac52088b3ad
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-09 12:51:23 +0530

    Time: 1 ms (69.43%), Space: 41.8 MB (48.13%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.java
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.java
@@ -1,31 +1,29 @@
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
-
-        int[] first = new int[26];
-        int[] second = new int[26];
-        Arrays.fill(first,0);
-        Arrays.fill(second,0);
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        Arrays.fill(arr1,0);
+        Arrays.fill(arr2,0);
 
         for(int i = 0; i < word1.length(); i++)
         {
-            first[word1.charAt(i) - 'a'] += 1;
+            int ind = (int)(word1.charAt(i) - 'a');
+            arr1[ind] = arr1[ind] + 1;
         }
 
-        for(int i = 0; i < word2.length(); i++)
+        for(int j = 0; j < word2.length(); j++)
         {
-            second[word2.charAt(i) - 'a'] += 1;
+            int ind = (int) (word2.charAt(j) - 'a');
+            arr2[ind] = arr2[ind] + 1;
         }
 
         for(int i = 0; i < 26; i++)
         {
-            int diff = Math.abs(first[i] - second[i]);
-            if(diff > 3)
+            if(Math.abs(arr1[i] - arr2[i]) > 3)
             {
                 return false;
             }
         }
-
         return true;
-        
     }
 }
