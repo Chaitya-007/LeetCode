1f99a8b21398f7000a836baea1ec171379046057
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-10 16:25:27 +0530

    Time: 0 ms (100%), Space: 41.8 MB (9.13%) - LeetHub

--- 2154-minimum-moves-to-convert-string/2154-minimum-moves-to-convert-string.java
+++ 2154-minimum-moves-to-convert-string/2154-minimum-moves-to-convert-string.java
@@ -1,32 +1,23 @@
 class Solution {
     public int minimumMoves(String s) {
+        int cnt = 0;
+        int i = 0;
         int n = s.length();
         int mov = 0;
-        int i = 0;
+
         while(i < n)
         {
-            String str = s.substring(i, Math.min(n,i+3));
-
-            int ind = 0;
-            for(ind = 0; ind < str.length(); ind++)
+            if(s.charAt(i) == 'O')
             {
-                if(str.charAt(ind) == 'X')
-                {
-                    break;
-                }
-            }
-
-
-            if(ind == 0)
-            {
-                mov++;
-                i = i + 3;
+                i++;
             }
             else
             {
-                i = i + ind;
+                mov++;
+                i += 3;
             }
         }
+
         return mov;
     }
 }
