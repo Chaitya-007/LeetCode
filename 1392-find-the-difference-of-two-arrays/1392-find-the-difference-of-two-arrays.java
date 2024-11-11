584d33d75938fdcbd10fee98949b060c1eb6b2ef
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-11 22:45:45 +0530

    Time: 10 ms (79.42%), Space: 44.8 MB (98.52%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
@@ -2,8 +2,9 @@ class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         Set<Integer> hset1 = new HashSet<Integer>();
         Set<Integer> hset2 = new HashSet<Integer>();
-        Set<Integer> hset3 = new HashSet<Integer>();
-        Set<Integer> hset4 = new HashSet<Integer>();
+       
+        List<Integer> list1 = new ArrayList<Integer>();
+        List<Integer> list2 = new ArrayList<Integer>();
 
         for(int val : nums1)
         {
@@ -15,34 +16,22 @@ class Solution {
             hset2.add(val);
         }
 
-        for(int val : nums1)
+        for(int val : hset1)
         {
             if(!hset2.contains(val))
             {
-                hset3.add(val);
+                list1.add(val);
             }
         }
 
-        for(int val : nums2)
+        for(int val : hset2)
         {
             if(!hset1.contains(val))
             {
-                hset4.add(val);
+                list2.add(val);
             }
         }
 
-        List<Integer> list1 = new ArrayList<Integer>();
-        List<Integer> list2 = new ArrayList<Integer>();
-
-        for(Integer num : hset3)
-        {
-            list1.add(num);
-        }
-
-        for(Integer num : hset4)
-        {
-            list2.add(num);
-        }
 
         List<List<Integer>> res = new ArrayList<List<Integer>>();
 
