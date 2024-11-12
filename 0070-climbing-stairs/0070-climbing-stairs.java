2461c6d46902b3fdc300a8ede8b394aa08f9c67b
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-12 11:33:14 +0530

    Time: 0 ms (100%), Space: 40 MB (81.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,104 +1,24 @@
 class Solution {
 
-    // Recursive Solution => TLE
-    // public int DpOnStairs(int ind)
-    // {
-    //    if(ind == 0 || ind == 1)
-    //    {
-    //     return 1;
-    //    }
-
-    //    return (DpOnStairs(ind - 1) + DpOnStairs(ind - 2));
-    // }
-
-    // Memoizattion using map
-    // public int memo(int ind, Map<Integer,Integer> map)
-    // {
-    //     if(ind == 0 || ind == 1)
-    //     {
-    //         return 1;
-    //     }
-
-    //     if(!map.containsKey(ind))
-    //     {
-    //       map.put(ind,memo(ind - 1,map) + memo(ind - 2,map));
-    //     }
-
-    //     return map.get(ind);
-    // }
-
-    // or write this way
-//     public int memo(int ind, Map<Integer, Integer> map) {
-//     if (ind == 0 || ind == 1) {
-//         return 1;
-//     }
-
-//     if (map.containsKey(ind)) {
-//         return map.get(ind);
-//     }
-
-//     int value = memo(ind - 1, map) + memo(ind - 2, map);
-//     map.put(ind, value);
-//     return value;
-// }
-
-
-    // Memoization using table 
-    // public int memoTable(int ind,int[] dp)
-    // {
-    //     if(ind == 0 || ind == 1)
-    //     {
-    //         return 1;
-    //     }
-
-    //     if(dp[ind] != -1)
-    //     {
-    //         return dp[ind];
-    //     }
-
-    //     return dp[ind] = memoTable(ind - 1,dp) + memoTable(ind - 2,dp);
-    // }
-
-    public int climbStairs(int n) {
-        // Recusrive Solution
-    //  int result = DpOnStairs(n);
-    //  return result;   
-
-    // Memoization using Map
-    // Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-
-    // int result = memo(n,map);
-    // return result;
-
-    // Memoization using Array
-    // int[] dp = new int[n+1];
-    // Arrays.fill(dp,-1);
-    // int result = memoTable(n,dp);
-    // return result;
-
-    // Tabulation
-    // int[] dp = new int[n+1];
-    // dp[0] = 1;
-    // dp[1] = 1;
-
-    // for(int i = 2; i <= n; i++)
-    // {
-    //     dp[i] = dp[i-1] + dp[i-2];
-    // }
-
-    // return dp[n];
-
-    // Using Space Optimization
-    int prev = 1;
-    int prev2 = 1;
-
-    for(int i = 2; i <= n; i++)
+    public int fun(int ind,int[] dp)
     {
-        int curr = prev + prev2;
-        prev2 = prev;
-        prev = curr;
+        if(ind <= 1)
+        {
+            return 1;
+        }
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int left = fun(ind - 1,dp);
+        int right = fun(ind - 2,dp);
+
+        return dp[ind] = left + right;
     }
 
-    return prev;
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return fun(n,dp);
+
     }
 }
