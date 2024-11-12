ebc3b18412d608261f7c01b7067dbabe20891c4a
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-12 20:01:14 +0530

    Time: 0 ms (100%), Space: 40.7 MB (88.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,58 +1,29 @@
 class Solution {
 
-    public int fun(int ind,int[] nums)
+    public int fun(int ind, int[] nums,int[] dp)
     {
-        if(ind == 0)  return nums[0];
-        if(ind < 0) return 0;
+        if(ind == 0)
+        {
+            return nums[0];
+        }
 
-        int pick = nums[ind] + fun(ind-2,nums);
-        int not_pick = fun(ind-1,nums);
+        if(ind < 0)
+        {
+            return 0;
+        }
 
-        return Math.max(pick,not_pick);
-    }
+        if(dp[ind] != -1) return dp[ind];
 
-    public int fundp(int ind,int[] nums, int[] dp)
-    {
-        if(ind == 0)  return nums[0];
-        if(ind < 0) return 0;
+        int not_pick = fun(ind - 1,nums,dp);
+        int pick = nums[ind] + fun(ind-2,nums,dp);
 
-        if(dp[ind] != -1 ) return dp[ind];
-
-        int pick = nums[ind] + fundp(ind-2,nums,dp);
-        int not_pick = fundp(ind-1,nums,dp);
-
-        return dp[ind] = Math.max(pick,not_pick);
+        return dp[ind] = Math.max(not_pick,pick);
     }
 
     public int rob(int[] nums) {
         int n = nums.length;
-        // return fun(n-1,nums);
-
-        // Memoization
-        // int[] dp = new int[n];
-        // Arrays.fill(dp,-1);
-
-        // return fundp(n-1,nums,dp);
-
-        // Tabulation
         int[] dp = new int[n];
-        Arrays.fill(dp,0);
-        dp[0] = nums[0];
-        int neg = 0;
-
-        for(int ind = 1; ind < n; ind++)
-        {
-            int take = nums[ind];
-            if(ind > 1) 
-            {
-                take += dp[ind-2];
-            }
-            int notTake = dp[ind-1];
-            dp[ind] = Math.max(take,notTake);
-        }
-
-        return dp[n-1];
-
-
+        Arrays.fill(dp,-1);
+        return fun(n-1,nums,dp);
     }
 }
