c46d27e74c3af4c3a2f6a4a1a80588f1611d83f9
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-13 14:38:36 +0530

    Time: 7 ms (83.69%), Space: 44.9 MB (24.77%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,35 +1,36 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+
         int n = word1.length();
         int m = word2.length();
-       int[] prev = new int[m+1];
-       int[] curr = new int[m+1];  
-       Arrays.fill(prev,0);
 
-       for(int i = 1; i <= n; i++)
-       {
-        for(int j = 1; j <= m; j++)
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
+
+        for(int j = 0; j <= m; j++) prev[j] = 0;
+
+        for(int i = 1; i <= n; i++)
         {
-            if(word1.charAt(i-1) == word2.charAt(j-1))
+            curr[0] = 0;
+            for(int j = 1; j <= m; j++)
             {
-                curr[j] = 1 + prev[j-1];
+                if(word1.charAt(i-1) == word2.charAt(j-1))
+                {
+                    curr[j] = 1 + prev[j-1];
+                }
+                else
+                {
+                    curr[j] = Math.max(prev[j],curr[j-1]);
+                }
             }
-            else
-            {
-                curr[j] = Math.max(prev[j],curr[j-1]);
-            }
+
+            prev = curr.clone();
         }
 
-        int[] temp = prev;
-        prev = curr;
-        curr = temp;
-       }
+        int deletions = n - prev[m];
 
-    int len_lcs = prev[m];
-    int deletions = n - len_lcs;
-    int insertions = m - len_lcs;
-    int total = deletions + insertions;
+        int insertions = m - prev[m];
 
-    return total;
+        return (deletions + insertions);
     }
 }
