c6a7aa45bb15c3b0e7bb8b1a2d4cdfbac449c426
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-13 15:14:38 +0530

    Time: 21 ms (85.76%), Space: 50.4 MB (50.49%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,64 +1,67 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
+
         int n = str1.length();
         int m = str2.length();
-        int[][] dp = new int[n+1][m+1]; // automatically assigned to zeroes
         
+        int[][] dp = new int [n+1][m+1];
+
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
 
         for(int i = 1; i <= n; i++)
         {
             for(int j = 1; j <= m; j++)
             {
-                if(str1.charAt(i-1) == str2.charAt(j-1))
-                {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+        if(str1.charAt(i-1) == str2.charAt(j-1))
+        {
+             dp[i][j] = 1 + dp[i-1][j-1];
         }
 
-        int i = n; 
+        else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+
+            }
+        }
+        
+
+        int i = n;
         int j = m;
-        String ans = "";
+
+        StringBuilder sb = new StringBuilder("");
 
         while(i > 0 && j > 0)
         {
             if(str1.charAt(i-1) == str2.charAt(j-1))
             {
-                ans += str1.charAt(i-1);
+                sb.insert(0,str1.charAt(i-1));
                 i--;
                 j--;
             }
-            else if(dp[i-1][j] > dp[i][j-1]) // moving upwards
+            else if(dp[i-1][j] > dp[i][j-1])
             {
-                ans += str1.charAt(i-1);
+                sb.insert(0,str1.charAt(i-1));
                 i--;
             }
             else
             {
-                ans += str2.charAt(j-1);
+                sb.insert(0,str2.charAt(j-1));
                 j--;
             }
         }
 
         while(i > 0) 
         {
-            ans += str1.charAt(i-1);
+            sb.insert(0,str1.charAt(i-1));
             i--;
         }
 
         while(j > 0)
         {
-            ans += str2.charAt(j-1);
+            sb.insert(0,str2.charAt(j-1));
             j--;
         }
 
-        StringBuilder sb = new StringBuilder(ans);
-
-        return sb.reverse().toString();
-
+        return sb.toString();
     }
 }
