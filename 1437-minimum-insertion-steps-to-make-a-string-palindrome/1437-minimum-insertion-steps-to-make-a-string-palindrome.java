d6b51fb642d2b475f03f584b45a4f381b9577d5c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-13 14:35:39 +0530

    Time: 24 ms (67.65%), Space: 44.7 MB (95.05%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,24 +1,23 @@
 class Solution {
-
     public int minInsertions(String s) {
-        String s1 = s;
         StringBuilder sb = new StringBuilder(s);
-        String s2 = sb.reverse().toString();
-        int n = s1.length();
-        int m = s2.length();
-        
+        String t = sb.reverse().toString();
+        int n = s.length();
+        int m = t.length();
+
         int[] prev = new int[m+1];
         int[] curr = new int[m+1];
-        Arrays.fill(prev,0);
-        Arrays.fill(curr,0);
+
+        for(int j = 0; j <= m; j++) prev[j] = 0;
 
         for(int i = 1; i <= n; i++)
         {
+            curr[0] = 0;
             for(int j = 1; j <= m; j++)
             {
-                if(s1.charAt(i - 1) == s2.charAt(j - 1))
+                if(s.charAt(i-1) == t.charAt(j-1))
                 {
-                    curr[j] = 1 + prev[j - 1];
+                    curr[j] = 1 + prev[j-1];
                 }
                 else
                 {
@@ -26,17 +25,10 @@ class Solution {
                 }
             }
 
-            // prev = curr;**** this is not going to work in java
-            // ***************Very import you can swap array
-            int[] temp = prev;
-            prev = curr;
-            curr = temp;
+            prev = curr.clone();
         }
 
-        int longest = prev[m];
-        
-
-        int ans = n - longest;
+        int ans = n - prev[m];
         return ans;
     }
 }
