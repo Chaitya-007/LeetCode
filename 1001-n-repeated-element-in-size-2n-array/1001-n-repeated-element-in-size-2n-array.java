1ef5bba118468831afd44f4cd1e379b58dc9af1a
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-14 19:32:05 +0530

    Time: 1 ms (68.12%), Space: 44.5 MB (98.95%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.java
@@ -1,23 +1,13 @@
 class Solution {
-    public int repeatedNTimes(int[] A) {
-        // Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> hset = new HashSet<Integer>();
 
-        // for(int val : nums)
-        // {
-        //     mp.put(val, mp.getOrDefault(val,0) + 1);
-        //     if(mp.get(val) == (nums.length/2))
-        //     {
-        //         return val;
-        //     }
-        // }
+        for(int val : nums)
+        {
+            if(hset.contains(val)) return val;
+            hset.add(val);
+        }
 
-        // return -1;
-
-        int[] count = new int[10000];
-        for (int a : A)
-            if (count[a]++ == 1)
-                return a;
         return -1;
-
     }
 }
