53ea7226a69fdd5091867c3b9189570895cbece2
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-11-16 17:45:52 +0530

    Time: 1 ms (96%), Space: 41.2 MB (55.59%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,37 +1,14 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int sum = 0;
+        Set<Integer> st = new HashSet<Integer>();
         for(int val : nums)
         {
-            sum += val;
+            if(val > 0)
+            {
+                st.add(val);
+            }
         }
 
-        int opr = 0;
-
-        while(sum != 0)
-        {
-            int mino = Integer.MAX_VALUE;
-            for(int i = 0; i < nums.length; i++)
-            {
-                if( (nums[i] > 0) && (mino > nums[i]) )
-                {
-                    mino = nums[i];
-                }
-            }
-
-            for(int i = 0; i < nums.length; i++)
-            {
-                if(nums[i] > 0)
-                {
-                    nums[i] = nums[i] - mino;
-                    sum -= (mino);
-                }
-            }
-
-            opr++;
-
-        }
-
-        return opr;
+        return st.size();
     }
 }
