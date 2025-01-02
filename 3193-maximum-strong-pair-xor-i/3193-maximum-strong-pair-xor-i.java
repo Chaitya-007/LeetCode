59e681f88233dbb1d00b725e6bedfba10c073158
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-02 13:14:08 +0530

    Time: 2 ms (96.88%), Space: 43.5 MB (20.67%) - LeetHub

--- 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.java
+++ 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.java
@@ -1,24 +1,17 @@
 class Solution {
     public int maximumStrongPairXor(int[] nums) {
-        int result = 0;
-
+        int xor = 0;
         for(int i = 0; i < nums.length; i++)
         {
-            int x = nums[i];
             for(int j = i; j < nums.length; j++)
             {
-                int y = nums[j];
-                if(Math.abs(x - y) <= Math.min(x,y))
+                if(Math.abs(nums[i] - nums[j]) <= Math.min(nums[i],nums[j]))
                 {
-                    int xor = (x^y);
-                    if(result < xor)
-                    {
-                        result = xor;
-                    }
+                    xor = Math.max(xor, nums[i] ^ nums[j]);
                 }
             }
         }
 
-        return result;
+        return xor;
     }
 }
