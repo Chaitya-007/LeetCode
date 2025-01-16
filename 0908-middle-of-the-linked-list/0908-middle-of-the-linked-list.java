557b963e1c172653212a3b09d8ac03024425901d
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-16 09:13:29 +0530

    Time: 0 ms (100%), Space: 41.5 MB (20.74%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -9,23 +9,8 @@
  * }
  */
 class Solution {
-
-    public int countNodes(ListNode head)
-    {
-        int cnt = 0;
-        ListNode ptr = head;
-        while(ptr != null)
-        {
-            cnt++;
-            ptr = ptr.next;
-        }
-
-        return cnt;
-    }
-
     public ListNode middleNode(ListNode head) {
-
-        if(head == null) 
+        if(head == null)
         {
             return head;
         }
@@ -40,42 +25,5 @@ class Solution {
         }
 
         return slow;
-
-        // Brute Force
-        // int n = countNodes(head);
-        // int position = (n/2) + 1;
-
-        // ListNode temp = head;
-
-        // while(temp != null)
-        // {
-        //     position--;
-        //     if(position == 0)
-        //     {
-        //         break;
-        //     }
-
-        //     temp = temp.next;
-        // }
-
-        // return temp;
-
-        // Optimal
-        // if(head == null)
-        // {
-        //     return null;
-        // }
-
-        // ListNode slow = head;
-        // ListNode fast = head;
-
-        // while(fast != null && fast.next != null)
-        // {
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-        // }
-
-        // return slow;
-
     }
 }
