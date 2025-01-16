eb6a2b50ea4d0a49b5c56c704018f2c9ac29adce
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-16 19:39:59 +0530

    Time: 15 ms (94.09%), Space: 41.3 MB (98.64%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -71,35 +71,67 @@ class Solution {
         // }
         // return funMemoShift(n,m,text1,text2,dp);
 
-        int[][] dp = new int[n+1][m+1];
+        // int[][] dp = new int[n+1][m+1];
+
+        // for(int j = 0; j <= m; j++)
+        // {
+        //     dp[0][j] = 0;
+        // }
+
+        // for(int i = 0; i <= n; i++)
+        // {
+        //     dp[i][0] = 0;
+        // }
+        
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     for(int j = 1; j <= m; j++)
+        //     {
+
+        // if(text1.charAt(i-1) == text2.charAt(j-1))
+        // {
+        //      dp[i][j] = 1 + dp[i-1][j-1];
+        // }
+        // else
+        // {
+        // dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        // } 
+
+        //     }
+        // }
+
+        // return dp[n][m];
+
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
 
         for(int j = 0; j <= m; j++)
         {
-            dp[0][j] = 0;
+            prev[j] = 0;
         }
 
-        for(int i = 0; i <= n; i++)
-        {
-            dp[i][0] = 0;
-        }
-        
         for(int i = 1; i <= n; i++)
         {
+            curr[0] = 0;
+
             for(int j = 1; j <= m; j++)
             {
-
         if(text1.charAt(i-1) == text2.charAt(j-1))
         {
-             dp[i][j] = 1 + dp[i-1][j-1];
+             curr[j] = 1 + prev[j-1];
         }
         else
         {
-        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        curr[j] = Math.max(prev[j], curr[j-1]);
         } 
 
             }
+
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 }
