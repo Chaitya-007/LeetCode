e50a00963e8a59ec5ff16373cad7b097e24b6ee3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-16 19:30:48 +0530

    Time: 19 ms (88.17%), Space: 51.1 MB (39.93%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -64,14 +64,42 @@ class Solution {
         // }
         // return funMemo(n-1,m-1,text1,text2,dp);
 
+        // int[][] dp = new int[n+1][m+1];
+        // for(int i = 0; i < n+1; i++)
+        // {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return funMemoShift(n,m,text1,text2,dp);
+
         int[][] dp = new int[n+1][m+1];
-        for(int i = 0; i < n+1; i++)
+
+        for(int j = 0; j <= m; j++)
         {
-            Arrays.fill(dp[i],-1);
+            dp[0][j] = 0;
         }
-        return funMemoShift(n,m,text1,text2,dp);
 
+        for(int i = 0; i <= n; i++)
+        {
+            dp[i][0] = 0;
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
 
+        if(text1.charAt(i-1) == text2.charAt(j-1))
+        {
+             dp[i][j] = 1 + dp[i-1][j-1];
+        }
+        else
+        {
+        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        } 
 
+            }
+        }
+
+        return dp[n][m];
     }
 }
