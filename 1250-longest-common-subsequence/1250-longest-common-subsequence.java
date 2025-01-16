b3221d858bbda23529e4ff41acd8f45efa8f80d4
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-16 19:23:53 +0530

    Time: 22 ms (47.32%), Space: 51.1 MB (39.93%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -33,19 +33,45 @@ class Solution {
         return dp[i][j] = Math.max(funMemo(i-1,j,s,t,dp), funMemo(i,j-1,s,t,dp));
     }
 
+    public int funMemoShift(int i,int j, String s, String t,int[][]dp)
+    {
+        if(i == 0 || j == 0)
+        {
+            return 0;
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s.charAt(i-1) == t.charAt(j-1))
+        {
+            return dp[i][j] = 1 + funMemoShift(i-1,j-1,s,t,dp);
+        }
+
+        return dp[i][j] = Math.max(funMemoShift(i-1,j,s,t,dp), funMemoShift(i,j-1,s,t,dp));
+    }
 
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
 
         // return fun(n-1,m-1,text1,text2);
-        int[][] dp = new int[n][m];
 
-        for(int i = 0; i < n; i++)
+        // int[][] dp = new int[n][m];
+
+        // for(int i = 0; i < n; i++)
+        // {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return funMemo(n-1,m-1,text1,text2,dp);
+
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 0; i < n+1; i++)
         {
             Arrays.fill(dp[i],-1);
         }
-        return funMemo(n-1,m-1,text1,text2,dp);
+        return funMemoShift(n,m,text1,text2,dp);
+
+
 
     }
 }
