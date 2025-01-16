a012fa99845bf0594a6d81b9caa0383983483621
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-16 17:35:33 +0530

    Time: 1 ms (99.93%), Space: 48.7 MB (43.13%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,146 +10,35 @@
  * }
  */
 public class Solution {
-
-    public ListNode collisionPoint(ListNode small, ListNode large, int diff)
-    {
-        while(diff > 0)
-        {
-            diff--;
-            large = large.next;
-        }
-
-        while(small != large)
-        {
-            small = small.next;
-            large = large.next;
-        }
-
-        return small; // or return large
-    }
-
-
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // Naive Approach
-        // T.C -> O(n1 * logN) + O(n2 * logN)
-        // S.C -> O(N1) or O(N2)
-        // if(headA == null || headB == null)
-        // {
-        //     return null;
-        // }
-
-        // HashMap<ListNode, Integer> map = new HashMap<ListNode, Integer>();
-
-        // ListNode ptra = headA;
-        // ListNode ptrb = headB;
-
-        // while(ptra != null)
-        // {
-        //     map.put(ptra,1);
-        //     ptra = ptra.next;
-        // }
-
-        // while(ptrb != null)
-        // {
-        //     if(map.containsKey(ptrb))
-        //     {
-        //         return ptrb;
-        //     }
-        //     ptrb = ptrb.next;
-        // }
-
-        // return null;
-
-        // Using Two Approach
-        // T.C -> O(N1) + O(N2) + O(n2 - N1) + O(N2 , whichever is small let's consider N2 as small)
-        // if(headA == null || headB == null)
-        // {
-        //     return null;
-        // }
-        // int n1 = 0;
-        // int n2 = 0;
-        // ListNode tempA = headA;
-        // ListNode tempB = headB;
-
-        // while(tempA != null)
-        // {
-        //     n1++;
-        //     tempA = tempA.next;
-        // }
-
-        // while(tempB != null)
-        // {
-        //     n2++;
-        //     tempB = tempB.next;
-        // }
-
-        // tempA = headA; // Set the pointers to head again
-        // tempB = headB; // Set the pointers to head again
-
-        // if(n1 < n2)
-        // {
-        //     return collisionPoint(tempA,tempB,n2 - n1);
-        // }
-
-        // return collisionPoint(tempB,tempA,n1 - n2);
-
-        // Optimal Solution
-        // T.C -> O(N1 + N2)
-        // S.C -> O(1)
-        // if(headA == null || headB == null)
-        // {
-        //     return null;
-        // }
-
-        // ListNode t1 = headA;
-        // ListNode t2 = headB;
-
-        // while(t1 != t2)
-        // {
-        //     t1 = t1.next;
-        //     t2 = t2.next;
-
-        //     if(t1 == t2) // this also holds true for t1 == null && t2 == null
-        //     {
-        //         return t1;
-        //     }
-
-        //     if(t1 == null)
-        //     {
-        //         t1 = headB;
-        //     }
-        //     if(t2 == null)
-        //     {
-        //         t2 = headA;
-        //     }
-
-        // }
-
-        // return t1;
-
-
         if(headA == null || headB == null)
         {
             return null;
         }
 
-        ListNode t1 = headA;
-        ListNode t2 = headB;
+        ListNode slow = headA;
+        ListNode fast = headB;
 
-        while(t1 != t2)
+        while(slow != fast)
         {
-            t1 = t1.next;
-            t2 = t2.next;
+            slow = slow.next;
+            fast = fast.next;
 
-            if(t1 == t2)
+            if(slow == fast)
             {
-                return t1;
+                return slow;
             }
 
-            if(t1 == null) t1 = headA;
-            if(t2 == null) t2 = headB;
+            if(slow == null)
+            {
+                slow = headB;
+            }
+            if(fast == null)
+            {
+                fast = headA;
+            }
         }
 
-        return t1;
+        return slow;
     }
 }
