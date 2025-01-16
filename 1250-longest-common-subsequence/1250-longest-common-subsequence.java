d5f2ee5b4ac9847e5841b67e09b2eff36d671dcb
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-16 19:20:09 +0530

    Time: 24 ms (34.42%), Space: 51.3 MB (13.52%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,97 +1,51 @@
 class Solution {
 
-    public int fun(int i,int j, String s, String t,int[][] dp)
+    public int fun(int i,int j, String s, String t)
     {
-        if(i == 0 || j == 0)
+        if(i < 0 || j < 0)
+        {
+            return 0;
+        }
+
+
+        if(s.charAt(i) == t.charAt(j))
+        {
+            return 1 + fun(i-1,j-1,s,t);
+        }
+
+        return Math.max(fun(i-1,j,s,t), fun(i,j-1,s,t));
+    }
+
+    public int funMemo(int i,int j, String s, String t,int[][]dp)
+    {
+        if(i < 0 || j < 0)
         {
             return 0;
         }
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s.charAt(i-1) == t.charAt(j-1))
+        if(s.charAt(i) == t.charAt(j))
         {
-            return dp[i][j] = 1 + fun(i-1,j-1,s,t,dp);
+            return dp[i][j] = 1 + funMemo(i-1,j-1,s,t,dp);
         }
 
-        return dp[i][j] = Math.max(fun(i-1,j,s,t,dp),fun(i,j-1,s,t,dp));
+        return dp[i][j] = Math.max(funMemo(i-1,j,s,t,dp), funMemo(i,j-1,s,t,dp));
     }
+
 
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        // int[][] dp = new int [n][m];
 
-        // for(int i = 0; i < n; i++)
-        // {
-        //     for(int j = 0; j < m; j++)
-        //     {
-        //         dp[i][j] = -1;
-        //     }
-        // }
+        // return fun(n-1,m-1,text1,text2);
+        int[][] dp = new int[n][m];
 
-        // int[][] dp = new int [n+1][m+1];
-
-        // for(int i = 0; i < n+1; i++)
-        // {
-        //     for(int j = 0; j < m+1; j++)
-        //     {
-        //         dp[i][j] = -1;
-        //     }
-        // }
-        // return fun(n,m,text1,text2,dp);
-
-        // int[][] dp = new int [n+1][m+1];
-
-        // for(int j = 0; j <= m; j++) dp[0][j] = 0;
-
-        // for(int i = 0; i <= n; i++) dp[i][0] = 0;
-
-        // for(int i = 1; i <= n; i++)
-        // {
-        //     for(int j = 1; j <= m; j++)
-        //     {
-        // if(text1.charAt(i-1) == text2.charAt(j-1))
-        // {
-        //      dp[i][j] = 1 + dp[i-1][j-1];
-        // }
-
-        // else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-
-        //     }
-        // }
-        
-
-        // return dp[n][m];
-
-        int[] prev = new int[m+1];
-        int[] curr = new int[m+1];
-        // int[][] dp = new int [n+1][m+1];
-        Arrays.fill(prev,0);
-        Arrays.fill(curr,0);
-
-        for(int j = 0; j <= m; j++) prev[j] = 0;
-
-        
-
-        for(int i = 1; i <= n; i++)
+        for(int i = 0; i < n; i++)
         {
-        curr[0] = 0;
-            for(int j = 1; j <= m; j++)
-            {
-        if(text1.charAt(i-1) == text2.charAt(j-1))
-        {
-             curr[j] = 1 + prev[j-1];
+            Arrays.fill(dp[i],-1);
         }
+        return funMemo(n-1,m-1,text1,text2,dp);
 
-        else curr[j] = Math.max(prev[j],curr[j-1]);
-
-            }
-
-            prev = curr.clone();
-        }
-        
-
-        return prev[m];
     }
 }
