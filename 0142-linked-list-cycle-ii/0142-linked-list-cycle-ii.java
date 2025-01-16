8bb52ba9965ee31233b7c7df5e84f1297f9ffdc9
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-16 17:25:26 +0530

    Time: 0 ms (100%), Space: 44.5 MB (64.19%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,7 +11,6 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-
         if(head == null)
         {
             return head;
@@ -25,7 +24,7 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
 
-            if(slow == fast) 
+            if(slow == fast)
             {
                 fast = head;
                 while(slow != fast)
@@ -33,68 +32,11 @@ public class Solution {
                     slow = slow.next;
                     fast = fast.next;
                 }
-                
-                    return slow;
+
+                return slow;
             }
         }
 
         return null;
-
-        // Optimal Solution
-        // T.c -> O(N)
-        // S.C -> O(1)
-
-        // if(head == null)
-        // {
-        //     return null;
-        // }
-        // ListNode slow = head;
-        // ListNode fast = head;
-
-        // while(fast != null && fast.next != null)
-        // {
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-
-        //     if(slow == fast)
-        //     {
-        //         slow = head;
-
-        //         while(slow != fast)
-        //         {
-        //             slow = slow.next;
-        //             fast = fast.next;
-        //         }
-
-        //         return slow;
-        //     }
-        // }
-
-        // return null;
-
-        // Brute Force
-        // T.C -> O(N*logn)
-        // S.C -> O(N)
-        // if(head == null)
-        // {
-        //     return null;
-        // }
-
-        // HashMap<ListNode, Integer> map = new HashMap<ListNode,Integer>();
-        // int ind = 0;
-
-        // ListNode temp = head;
-        // while(temp != null)
-        // {
-        //     if(map.containsKey(temp))
-        //     {
-        //         return temp;
-        //     }
-        //     map.put(temp,ind);
-        //     ind++;
-        //     temp = temp.next;
-        // }
-
-        // return null;
     }
 }
