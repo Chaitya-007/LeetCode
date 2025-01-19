8e334584c9501b6f6342c1648b86ca1f923f77e5
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-19 13:04:12 +0530

    Time: 17 ms (36.43%), Space: 50.5 MB (5.13%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,41 +1,44 @@
 class Solution {
     public String frequencySort(String s) {
+        // Bucket Sorting
+
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 
         for(char ch : s.toCharArray())
         {
-            map.put(ch, map.getOrDefault(ch,0) + 1);
+            map.put(ch,map.getOrDefault(ch,0) + 1);
         }
 
-        List<Pair<Character, Integer>> list = new ArrayList<Pair<Character, Integer>>();
+        List<List<Character>> list = new ArrayList<>();
+
+        for(int i = 0; i < (s.length() + 1); i++)
+        {
+            list.add(new ArrayList<>());
+        }
 
         for(Map.Entry<Character, Integer> e : map.entrySet())
         {
-            // Pair<Character, Integer> p = new Pair<Character, Integer>(e.getKey(), e.getValue());
-            // list.add(p);
-            list.add(new Pair<>(e.getKey(),e.getValue()));
+            list.get(e.getValue()).add(e.getKey());
         }
 
-        Comparator<Pair<Character, Integer>> comparator = (p1,p2) -> 
-        {
-            return p2.getValue().compareTo(p1.getValue());
-        };
-
-        Collections.sort(list,comparator);
-
-        // System.out.println(list);
         StringBuilder sb = new StringBuilder("");
 
-        for(Pair<Character, Integer> p : list)
+        for(int i = (s.length()) ; i >= 0; i--)
         {
-            for(int i = 1; i <= p.getValue(); i++)
+            if(list.get(i).size() != 0)
             {
-                sb.append(p.getKey());
+                for(Character ch : list.get(i))
+                {
+                    for(int j = 1; j <= i; j++)
+                    {
+                        sb.append(ch);
+                    }
+                }
             }
         }
 
         return sb.toString();
 
-        
+       
     }
 }
