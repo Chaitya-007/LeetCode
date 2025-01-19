6fe571092f00e0250f6c28d28fd10e27a7885055
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-19 12:31:58 +0530

    Time: 17 ms (36.43%), Space: 45.6 MB (71.21%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -7,45 +7,35 @@ class Solution {
             map.put(ch, map.getOrDefault(ch,0) + 1);
         }
 
-        Map<Integer, List<Character> > freq = new TreeMap<Integer, List<Character> >(Collections.reverseOrder());
+        List<Pair<Character, Integer>> list = new ArrayList<Pair<Character, Integer>>();
 
         for(Map.Entry<Character, Integer> e : map.entrySet())
         {
-            int key = e.getValue();
-            char val = e.getKey();
-
-            if(freq.containsKey(key))
-            {
-                List<Character> list = freq.get(key);
-                list.add(val);
-                freq.put(key,list);
-            }
-            else
-            {
-                List<Character> list = new ArrayList<Character>();
-                list.add(val);
-                freq.put(key,list);
-            }
+            // Pair<Character, Integer> p = new Pair<Character, Integer>(e.getKey(), e.getValue());
+            // list.add(p);
+            list.add(new Pair<>(e.getKey(),e.getValue()));
         }
 
+        Comparator<Pair<Character, Integer>> comparator = (p1,p2) -> 
+        {
+            return p2.getValue().compareTo(p1.getValue());
+        };
+
+        Collections.sort(list,comparator);
+
+        // System.out.println(list);
         StringBuilder sb = new StringBuilder("");
 
-        for(Map.Entry<Integer, List<Character> > e : freq.entrySet())
+        for(Pair<Character, Integer> p : list)
         {
-            int key = e.getKey();
-            List<Character> list = e.getValue();
-
-            for(Character ch : list)
+            for(int i = 1; i <= p.getValue(); i++)
             {
-
-            for(int i = 1; i <= key; i++)
-            {
-                sb.append(ch);
-            }
-
+                sb.append(p.getKey());
             }
         }
 
         return sb.toString();
+
+        
     }
 }
