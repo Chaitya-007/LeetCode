bcba940e92c6dd351521ffc3f3adc68ee8be5c5d
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-28 17:03:38 +0530

    Time: 4 ms (55.87%), Space: 45.6 MB (53.68%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,27 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-
-        // T.C => O(N * 32)
+        // T.C => O(n*logn) + O(n/3)
         // S.C => O(1)
-        int ans = 0;
 
-        for(int bitIndex = 0; bitIndex <= 31 ; bitIndex++)
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        // Start with index 1
+        for(int i = 1; i < n; i = i + 3)
         {
-            int cnt = 0;
-            for(int i = 0; i < nums.length; i++)
+            if(nums[i-1] != nums[i])
             {
-                if((nums[i] & (1<<bitIndex)) != 0)
-                {
-                    cnt++;
-                }
-            }
-
-            if(cnt%3 != 0)
-            {
-                ans = ans | (1<<bitIndex);
+                return nums[i-1];
             }
         }
 
-        return ans;
+        return nums[n-1];
     }
 }
