0a6931fcd7f355df0457d59820bdd585ff761c0f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-30 14:12:35 +0530

    Time: 0 ms (100%), Space: 43.5 MB (9.85%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,39 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+
+        if(n == Integer.MIN_VALUE)
+        {
+            x = x*x;
+            n = n/2;
+        }
+
+        int m = n;
+
+        double ans = 1;
+
+
+
+        if(n < 0)
+        {
+            n = -n;
+        }
+
+        while(n > 0)
+        {
+            if((n&1) == 0)
+            {
+                x = x * x;
+                n = n / 2;
+            }
+            else
+            {
+                ans = ans * x;
+                n = n - 1;
+            }
+        }
+
+        if(m < 0) ans = 1.0/ans;
+
+        return ans;
     }
 }
