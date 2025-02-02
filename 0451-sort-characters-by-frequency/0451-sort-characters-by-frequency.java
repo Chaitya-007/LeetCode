1408df411eed6df5d672fc41f0d8b2a22bed365e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-02 17:07:49 +0530

    Time: 17 ms (35.76%), Space: 50.9 MB (5%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,44 +1,46 @@
 class Solution {
     public String frequencySort(String s) {
-        // Bucket Sorting
-
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 
         for(char ch : s.toCharArray())
         {
-            map.put(ch,map.getOrDefault(ch,0) + 1);
+            map.put(ch, map.getOrDefault(ch,0) + 1);
         }
 
-        List<List<Character>> list = new ArrayList<>();
+        int n = s.length();
 
-        for(int i = 0; i < (s.length() + 1); i++)
+        List<List<Character>> list = new ArrayList<>(n+1);
+
+        for(int i = 0; i <= n; i++)
         {
             list.add(new ArrayList<>());
         }
 
         for(Map.Entry<Character, Integer> e : map.entrySet())
         {
-            list.get(e.getValue()).add(e.getKey());
+            int ind = e.getValue();
+            list.get(ind).add(e.getKey());
         }
 
         StringBuilder sb = new StringBuilder("");
 
-        for(int i = (s.length()) ; i >= 0; i--)
+        for(int i = n ; i >= 0; i--)
         {
             if(list.get(i).size() != 0)
             {
                 for(Character ch : list.get(i))
                 {
-                    for(int j = 1; j <= i; j++)
-                    {
-                        sb.append(ch);
-                    }
+                for(int k = 1; k <= i; k++)
+                {
+                    sb.append(ch);
+                }
+
                 }
             }
         }
 
         return sb.toString();
 
-       
+
     }
 }
