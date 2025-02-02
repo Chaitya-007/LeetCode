fca7a92d55af3704a80b84376c18eb35d8493048
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-02 10:31:01 +0530

    Time: 135 ms (0%), Space: 45.7 MB (100%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.java
@@ -1,31 +1,44 @@
 class Solution {
-    public int maxDistance(String s, int k) {
-        int ans=0; 
-            ans=Math.max(f(s,'N','E',k),ans);
-            ans=Math.max(f(s,'N','W',k),ans);
-            ans=Math.max(f(s,'S','E',k),ans);
-            ans=Math.max(f(s,'S','W',k),ans);
-    return ans;
-        
-        
-    }
-    public int f(String s,char A,char B,int x){
-        int ans=0;
-        int a=0;
-                for(int i=0;i<s.length();i++){
-         if(s.charAt(i)==A||s.charAt(i)==B){
-             if(x>0){
-                 x--;
-                 ans++;
-             }
-             else ans--;
-         }
-            else{
-                ans++;
+
+    public int f(String s,char A, char B, int k)
+    {
+        int dis = 0;
+        int maxDis = 0;
+        for(char ch : s.toCharArray())
+        {
+            if(ch == A || ch == B) // if character is our desired direction
+            {
+                dis++;
             }
-            a=Math.max(ans,a);
-            
+            else // if the character is not of our desired direction
+            {
+                if(k > 0)
+                {
+                    k--;
+                    dis++;
+                }
+                else
+                {
+                    dis--;
+                }
+         
+            }
+
+            maxDis = Math.max(maxDis, dis);
         }
-        return a;
+
+        return maxDis;
+    }
+
+
+    public int maxDistance(String s, int k) {
+        int ans = 0;
+        ans = Math.max(f(s,'N','E',k), ans);
+        ans = Math.max(f(s,'N','W',k), ans);
+        ans = Math.max(f(s,'S','E',k), ans);
+        ans = Math.max(f(s,'S','W',k), ans);
+
+        return ans;
+        
     }
 }
