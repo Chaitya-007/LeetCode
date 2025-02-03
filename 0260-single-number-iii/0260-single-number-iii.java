b363a42cc156b38d61ead5a6d31d6b2fac99ad6b
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-03 18:54:17 +0530

    Time: 1 ms (100%), Space: 45.8 MB (44.17%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,35 +1,26 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-
-        // T.C => O(2N)
-        // S.C => O(1)
-
-        // make this as long because 
-        // if xor => -2^31
-        // and xor -1 => -2^31 - 1 will overflow
         long xor = 0;
 
         for(int val : nums)
         {
-            xor ^= val;
+            xor = xor ^ val;
         }
 
-        // removes last set bit => xor & (xor - 1)
-        // gives last set bit => (xor & (xor - 1)) ^ xor)
-        int rightMost = (int)( (xor & (xor - 1)) ^ xor);
+        int rightMost = (int)((xor & (xor - 1)) ^ (xor));
 
         int b1 = 0;
         int b2 = 0;
 
-        for(int val : nums)
+        for(int i = 0; i < nums.length; i++)
         {
-            if( (val & rightMost) != 0)
+            if ((rightMost & nums[i]) != 0)
             {
-                b1 = b1 ^ val;
+                b1 = b1 ^ nums[i];
             }
             else
             {
-                b2 = b2 ^ val;
+                b2 = b2 ^ nums[i];
             }
         }
 
