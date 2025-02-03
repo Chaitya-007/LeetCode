d0cb176cf347e4940baf4ab50052fb9def06a70f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-03 18:17:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (19.13%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -3,12 +3,12 @@ class Solution {
         List<List<Integer>> ll = new ArrayList<>();
         int n = nums.length;
 
-        for(int i = 0; i < (1<<n); i++)
+        for(int num = 0; num < (1<<n); num++)
         {
             List<Integer> list = new ArrayList<Integer>();
             for(int j = 0; j < n; j++)
             {
-                if ( (i & (1<<j)) != 0)
+                if ( (num & (1<<j)) != 0)
                 {
                     list.add(nums[j]);
                 }
