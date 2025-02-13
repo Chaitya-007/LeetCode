ebf216ad9d72cd024fab518cfc7a28dde3433eb3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-13 22:55:04 +0530

    Time: 0 ms (100%), Space: 41.1 MB (96.58%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,54 +1,58 @@
 class MyQueue {
-    Stack<Integer> s1 = new Stack<Integer>();
-    Stack<Integer> s2 = new Stack<Integer>();
+
+    Stack<Integer> s1;
+    Stack<Integer> s2;
 
     public MyQueue() {
-        
+        s1 = new Stack<Integer>();
+        s2 = new Stack<Integer>();
     }
     
     public void push(int x) {
-       if(s2.isEmpty())
-       {
         s1.push(x);
-       }
-       else
-       {
-        while(!s2.isEmpty())
-        {
-            s1.push(s2.peek());
-            s2.pop();
-        }
-
-        s1.push(x);
-       }
-        
     }
     
     public int pop() {
-        peek();
-        int x = s2.peek();
-        s2.pop();
-        return x;
-    }
-    
-    public int peek() {
-        if(s1.isEmpty())
-        {
-            return s2.peek();
-        }
 
-        while(!s1.isEmpty())
+        if(s2.isEmpty())
         {
-            s2.push(s1.peek());
-            s1.pop();
-        }
+            while(!s1.isEmpty())
+            {
+                s2.push(s1.peek());
+                s1.pop();
+            }
 
-        return s2.peek();
+            int element = s2.peek();
+            s2.pop();
+            return element;
+        }
+        
+            int element = s2.peek();
+            s2.pop();
+            return element;
+        
         
     }
     
+    public int peek() {
+        if(s2.isEmpty())
+        {
+            while(!s1.isEmpty())
+            {
+                s2.push(s1.peek());
+                s1.pop();
+            }
+
+            int element = s2.peek();
+            return element;
+
+        }
+            int element = s2.peek();
+            return element;
+    }
+    
     public boolean empty() {
-        return (s1.isEmpty() && s2.isEmpty());
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
 
