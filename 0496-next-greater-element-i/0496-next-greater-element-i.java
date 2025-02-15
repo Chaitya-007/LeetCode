b6673d0257f41efd32e66d36a3db2d218c314306
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-15 14:05:59 +0530

    Time: 3 ms (92.15%), Space: 44.2 MB (36.86%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,36 +1,31 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // Brute Force
+        // T.C -> O(2*N)
+        // S.C -> O(N) + O(N)
+
+        Stack<Integer> st = new Stack<Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = nums2.length;
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+
+            while(!st.isEmpty() && nums2[i] > st.peek())
+            {
+                st.pop();
+            }
+
+            if(st.isEmpty()) map.put(nums2[i],-1);
+            else map.put(nums2[i],st.peek());
+
+            st.push(nums2[i]);
+        }
+
         int[] arr = new int[nums1.length];
-        int len = 0;
-        
+
         for(int i = 0; i < nums1.length; i++)
         {
-            int ind = -1;
-            
-            for(int j = 0; j < nums2.length; j++)
-            {
-                if(nums1[i] == nums2[j])
-                {
-                    ind = j;
-                    break;
-                }
-            }
-            
-            int nxtGreater = -1;
-
-            for(int j = ind + 1; j < nums2.length; j++)
-            {
-                if(nums2[ind] < nums2[j])
-                {
-                    nxtGreater = nums2[j];
-                    break;
-                }
-            }
-
-            arr[len] = nxtGreater;
-            len++;
-
+           arr[i] = map.get(nums1[i]);
         }
 
         return arr;
