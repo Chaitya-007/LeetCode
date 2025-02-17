2d6d341c0631429feb73e8a66a38b9c81a293a85
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-17 20:07:12 +0530

    Time: 117 ms (13.91%), Space: 57.7 MB (41.38%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    public int[] nse(int[] arr)
+    {
+        int n = arr.length;
+        int[] ind = new int[n];
+        Stack<Integer> st = new Stack<Integer>();
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
+            {
+                st.pop();
+            }
+
+            ind[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        return ind;
+    }
+
+    public int[] pse(int[] arr)
+    {
+        int n = arr.length;
+        int[] ind = new int[n];
+        Stack<Integer> st = new Stack<Integer>();
+
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.isEmpty() && arr[i]  <= arr[st.peek()])
+            {
+                st.pop();
+            }
+
+            ind[i] = st.isEmpty() ? -1 : st.peek();
+
+            st.push(i);
+        }
+
+        return ind;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+
+        int maxo = Integer.MIN_VALUE;
+        int n = heights.length;
+        int[] next = nse(heights);
+        int[] prev = pse(heights);
+
+        for(int i = 0; i < n; i++)
+        {
+            int width = (next[i] - prev[i] - 1);
+            int area = (width * heights[i]);
+            maxo = Math.max(area, maxo);
+        }
+
+        return maxo;
+        
+    }
+}
