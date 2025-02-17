c7de816136ed69355d258147cd380840b92538a3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-17 16:37:02 +0530

    Time: 42 ms (24.53%), Space: 44.5 MB (55.17%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,27 +1,130 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
 
-        long total = 0;
-        int n = nums.length;
+    public int[] nse(int[] arr)
+    {
+        Stack<Integer> st = new Stack<Integer>();
+        int n = arr.length;
+        int[] ind = new int[n];
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            while(!st.isEmpty() &&  arr[i] <= arr[st.peek()])
+            {
+                st.pop();
+            }
+
+            ind[i] = st.isEmpty() ? n : st.peek();
+
+            st.push(i);
+        }
+
+        return ind;
+    }
+
+    public int[] psee(int[] arr)
+    {
+        Stack<Integer> st = new Stack<Integer>();
+        int n = arr.length;
+        int[] ind = new int[n];
 
         for(int i = 0; i < n; i++)
         {
-            int maxo = Integer.MIN_VALUE;
-            int mino = Integer.MAX_VALUE;
-            for(int j = i; j < n; j++)
+            while(!st.isEmpty() &&  arr[st.peek()] > arr[i])
             {
-                mino = Math.min(nums[j],mino);
-                maxo = Math.max(nums[j],maxo);
-
-            total += (maxo - mino);
+                st.pop();
             }
 
-            // System.out.println("mino = " + mino);
-            // System.out.println("maxo = " + maxo);
+            ind[i] = st.isEmpty() ? -1 : st.peek();
 
+            st.push(i);
+        }
+
+        return ind;
+    }
+
+    public int[] nge(int[] arr)
+    {
+        Stack<Integer> st = new Stack<Integer>();
+        int n = arr.length;
+        int[] ind = new int[n];
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i])
+            {
+                st.pop();
+            }
+
+            ind[i] = st.isEmpty() ? n : st.peek();
+
+            st.push(i);
+        }
+
+        return ind;
+    }
+
+    public int[] pgee(int[] arr)
+    {
+        Stack<Integer> st = new Stack<Integer>();
+        int n = arr.length;
+        int[] ind = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.isEmpty() &&  arr[st.peek()] < arr[i])
+            {
+                st.pop();
+            }
+
+            ind[i] = st.isEmpty() ? -1 : st.peek();
+
+            st.push(i);
+        }
+
+        return ind;
+    }
+
+    public long sumOfMin(int[] arr)
+    {
+        final int MOD = (int)(1e9 + 7);
+        long total = 0;
+        int n = arr.length;
+        int[] prev = psee(arr);
+        int[] next = nse(arr);
+
+        for(int i = 0; i < n; i++)
+        {
+           long left = i - prev[i];
+            long right = next[i] - i;
+
+           long contribution = (left * right) * arr[i];
+            total = (total + contribution);
         }
 
         return total;
-        
+    }
+
+    public long sumOfMax(int[] arr)
+    {
+        final int MOD = (int)(1e9 + 7);
+        long total = 0;
+        int n  = arr.length;
+        int[] prev = pgee(arr);
+        int[] next = nge(arr);
+
+        for(int i = 0; i < n; i++)
+        {
+            long left = i - prev[i];
+            long right = next[i] - i;
+
+           long contribution = (left * right) * arr[i];
+            total = (total + contribution);
+        }
+
+        return total;
+    }
+
+    public long subArrayRanges(int[] nums) {
+        return sumOfMax(nums) - sumOfMin(nums);
     }
 }
