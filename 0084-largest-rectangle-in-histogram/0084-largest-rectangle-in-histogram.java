c033613dad0d3331c16bcf91c32903d526531449
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-17 21:04:56 +0530

    Time: 69 ms (58.97%), Space: 58.2 MB (13.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,64 +1,34 @@
 class Solution {
-
-    public int[] nse(int[] arr)
-    {
-        int n = arr.length;
-        int[] ind = new int[n];
-        Stack<Integer> st = new Stack<Integer>();
-
-        for(int i = n - 1; i >= 0; i--)
-        {
-            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
-            {
-                st.pop();
-            }
-
-            ind[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-
-        return ind;
-    }
-
-    public int[] pse(int[] arr)
-    {
-        int n = arr.length;
-        int[] ind = new int[n];
-        Stack<Integer> st = new Stack<Integer>();
-
-        for(int i = 0; i < n; i++)
-        {
-            while(!st.isEmpty() && arr[i]  <= arr[st.peek()])
-            {
-                st.pop();
-            }
-
-            ind[i] = st.isEmpty() ? -1 : st.peek();
-
-            st.push(i);
-        }
-
-        return ind;
-    }
-
     public int largestRectangleArea(int[] heights) {
-
-        // T.C -> O(N)
-        // S.C -> O(2N) + O(2*N)
-
-        int maxo = Integer.MIN_VALUE;
+        Stack<Integer> st = new Stack<Integer>();
         int n = heights.length;
-        int[] next = nse(heights);
-        int[] prev = pse(heights);
+        int area = Integer.MIN_VALUE;
 
         for(int i = 0; i < n; i++)
         {
-            int width = (next[i] - prev[i] - 1);
-            int area = (width * heights[i]);
-            maxo = Math.max(area, maxo);
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i])
+            {
+                int element = heights[st.peek()];
+                int right = i;
+                st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                int curr = (element * (right - left - 1));
+                area = Math.max(area,curr);
+            }
+
+            st.push(i);
         }
 
-        return maxo;
-        
+        while(!st.isEmpty())
+        {
+            int right = n;
+            int element = heights[st.peek()];
+            st.pop();
+            int left = st.isEmpty() ? -1 : st.peek();
+            int curr = (element * (right - left - 1));
+            area = Math.max(area,curr);
+        }
+
+        return area;
     }
 }
