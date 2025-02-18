350c10b07fca37ccff65deb15458dc487f7de002
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-18 17:10:05 +0530

    Time: 16 ms (58.29%), Space: 46.6 MB (50.89%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,97 @@
+class Solution {
+
+    public int maxRect(int[] mat)
+    {
+        Stack<Integer> st = new Stack<Integer>();
+        int n = mat.length;
+        int area = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.isEmpty() && mat[st.peek()] >= mat[i])
+            {
+                int right = i;
+                int element = mat[st.peek()];
+                st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                int curr = (element)*(right - left - 1);
+                area = Math.max(curr,area);
+            }
+
+            st.push(i);
+        }
+
+        while(!st.isEmpty())
+        {
+            int right = n;
+            int element = mat[st.peek()];
+            st.pop();
+            int left = st.isEmpty() ? -1 : st.peek();
+            int curr = (element)*(right - left - 1);
+            area = Math.max(area,curr);
+        }
+
+        return area;
+
+    }
+
+
+    public int maximalRectangle(char[][] matrix) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] psm = new int[n][m];
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                psm[i][j] = (int)(matrix[i][j] - '0');
+            }
+        }
+
+        // prefix operation on psm
+
+        for(int j = 0; j < m; j++)
+        {
+            int prefix = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(psm[i][j] == 0)
+                {
+                    prefix = 0;
+                }
+                else
+                {
+                    prefix++;
+                }
+
+                psm[i][j] = prefix;
+            }
+        }
+
+        // for(int i = 0; i < n; i++)
+        // {
+        //     for(int j = 0; j < m; j++)
+        //     {
+        //         System.out.print(psm[i][j] + " ");
+        //     }
+
+        //     System.out.println();
+        // }
+
+    
+
+        int maxo = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int area = maxRect(psm[i]);
+            maxo = Math.max(maxo,area);
+        }
+
+        return maxo;
+        
+    }
+}
