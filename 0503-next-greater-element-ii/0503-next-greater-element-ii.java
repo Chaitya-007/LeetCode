2684abc1327f4dc01cb8d280065f65c6c34b4ea3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-20 18:31:46 +0530

    Time: 16 ms (64.77%), Space: 46.7 MB (17.63%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,25 +1,32 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        Stack<Integer> st = new Stack<Integer>();
-        int n = nums.length;
-        int[] res = new int[n];
 
-        for(int i = ( (2*n) - 1); i >= 0; i--)
+        // T.C -> O(4*N)
+        // S.C -> O(2*N)(stack) + O(N)(store elements)
+
+        Stack<Integer> st = new Stack<>();
+
+        int n = nums.length;
+
+        int[] ans = new int[n];
+
+        for(int i = (2*n - 1); i >= 0; i--) // -> O(2*N)
         {
-            while(!st.isEmpty() && nums[i%n] >= st.peek())
+            int ind = (i%n);
+
+            while(!st.isEmpty() && nums[ind] >= st.peek())
             {
-                st.pop();
+                st.pop(); // you are poping out at max 2*n elements (O(2*N))
             }
 
             if(i < n)
             {
-                if(st.isEmpty()) res[i] = -1;
-                else res[i] = st.peek();
+                ans[i] = st.isEmpty() ? -1 : st.peek();
             }
 
-            st.push(nums[i%n]);
+            st.push(nums[ind]); // you are pushing at max 2*n elements => (O(2*N))
         }
 
-        return res;
+        return ans;
     }
 }
