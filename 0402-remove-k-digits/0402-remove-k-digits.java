12f08deeb9009daa9566fc9071fff2dfbb3bfea5
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-21 12:46:38 +0530

    Time: 68 ms (19.96%), Space: 46 MB (29%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,13 +1,18 @@
 class Solution {
     public String removeKdigits(String num, int k) {
 
+        // Get rid of large elements
+        // Keep track of small elements
+        // T.C -> O(3*N) + O(K)
+        // S.C -> O(N)(stack) + O(N)(storing result)
+
         Stack<Character> st = new Stack<>();
 
         int n = num.length();
         if(k == n) return "0";
         int i = 0;
 
-        for(char ch : num.toCharArray())
+        for(char ch : num.toCharArray()) // O(N)
         {
             
             while(k > 0 && !st.isEmpty() && (st.peek() - '0') > (ch - '0') )
@@ -20,7 +25,7 @@ class Solution {
         }
 
 
-        while(k > 0)
+        while(k > 0) // O(k)
         {
             st.pop();
             k--;
@@ -30,14 +35,14 @@ class Solution {
 
         StringBuilder sb = new StringBuilder("");
 
-        while(!st.isEmpty())
+        while(!st.isEmpty()) // traversal + reverse => O(N)
         {
             sb.insert(0,st.peek());
             st.pop();
         }
 
         i = 0;
-        while(i < sb.length() && sb.charAt(i) == '0')
+        while(i < sb.length() && sb.charAt(i) == '0') // O(N) => if entire stack is of "0" 
         {
             i++;
         }
