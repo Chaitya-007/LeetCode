cbfed32d84393dedf82b16c1a5e64cdc15325180
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-21 11:56:48 +0530

    Time: 5 ms (87.58%), Space: 45.5 MB (33.29%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,46 +1,48 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+
         // T.C -> O(2*N)
         // S.C -> O(N)
-     Stack<Integer> st = new Stack<Integer>();
-     int n = asteroids.length;
+        Stack<Integer> st = new Stack<>();
+        int n = asteroids.length;
 
-     for(int i = 0; i < n; i++)
-     {
-        if(asteroids[i] > 0) st.push(asteroids[i]);
-        else
+        for(int i = 0; i < n; i++) // -> O(N)
         {
-            while(!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(asteroids[i]))
-            {
-                st.pop();
-            }
+            // at max stack is pushing,poping N elements throughout journer -> O(N)
 
-            if(!st.isEmpty() && st.peek() == Math.abs(asteroids[i]))
+            if(asteroids[i] > 0) st.push(asteroids[i]);
+            else
             {
-                st.pop();
-            }
-            else if(st.isEmpty() || st.peek() < 0)
-            {
-                st.push(asteroids[i]);
+                // it is sure that the current element is -ve
+
+                // if top of stack is +ve and abs(-ve) > +ve
+                while(!st.isEmpty() && st.peek() > 0 && Math.abs(asteroids[i]) > st.peek())
+                {
+                    st.pop();
+                }
+                // either the stack have been empty or top of stack -> -ve or abs(-ve) <= +ve
+                if(!st.isEmpty() && st.peek() > 0 && Math.abs(asteroids[i]) == st.peek())
+                {
+                    st.pop();
+                }
+                else
+                {
+                    if(st.isEmpty() || st.peek() < 0)
+                    {
+                        st.push(asteroids[i]);
+                    }
+                }
             }
         }
-     }   
 
-    //  List<Integer> list = new ArrayList<Integer>();
-     int[] ans = new int[st.size()];
-     n = st.size();
+        int[] ind = new int[st.size()];
 
+        for(int i = st.size() - 1; i >= 0; i--)
+        {
+            ind[i] = st.peek();
+            st.pop();
+        }
 
-     while(!st.isEmpty())
-     {
-        n = n - 1;
-        ans[n] = st.peek();
-        st.pop();
-     }
-
-     return ans;
-
-
-
+        return ind;
     }
 }
