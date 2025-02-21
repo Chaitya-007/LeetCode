b39f51c8a5733f44bc6dc3e3350022653d47a97e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-21 12:08:59 +0530

    Time: 44 ms (10.1%), Space: 44.3 MB (65.85%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,21 +1,22 @@
 class Solution {
 
-    public int[] nse(int[] arr)
+      public int[] nse(int[] arr)
     {
-        Stack<Integer> st = new Stack<Integer>();
         int n = arr.length;
+
+        Stack<Integer> st = new Stack<>();
         int[] ind = new int[n];
 
         for(int i = n - 1; i >= 0; i--)
         {
-            while(!st.isEmpty() &&  arr[i] <= arr[st.peek()])
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
             {
                 st.pop();
             }
 
             ind[i] = st.isEmpty() ? n : st.peek();
 
-            st.push(i);
+            st.push(i); 
         }
 
         return ind;
@@ -23,13 +24,13 @@ class Solution {
 
     public int[] psee(int[] arr)
     {
-        Stack<Integer> st = new Stack<Integer>();
         int n = arr.length;
+        Stack<Integer> st = new Stack<>();
         int[] ind = new int[n];
 
         for(int i = 0; i < n; i++)
         {
-            while(!st.isEmpty() &&  arr[st.peek()] > arr[i])
+            while(!st.isEmpty() && arr[st.peek()] > arr[i])
             {
                 st.pop();
             }
@@ -42,15 +43,40 @@ class Solution {
         return ind;
     }
 
+    public long sumSubarrayMins(int[] arr) {
+
+        // T.C -> O(5*N)
+        // S.C -> O(2*N) (nse -> Stack, index array) + O(2*N) (psee -> Stack, index array) + O(2*N) (prev,next)
+        int n = arr.length;
+        int[] prev = psee(arr); // -> O(2*N)
+        int[] next = nse(arr); // -> O(2*N)
+
+        long total = 0;
+
+        for(int i = 0; i < n; i++) // -> O(N)
+        {
+            long left = i - prev[i];
+            long right = next[i] - i;
+
+            long contribution =  ( ( (right * left) ) * arr[i] );
+            total = (total + contribution);
+        }
+
+        return total;
+        
+    }
+
     public int[] nge(int[] arr)
     {
-        Stack<Integer> st = new Stack<Integer>();
         int n = arr.length;
+
+        Stack<Integer> st = new Stack<>();
+
         int[] ind = new int[n];
 
         for(int i = n - 1; i >= 0; i--)
         {
-            while(!st.isEmpty() && arr[st.peek()] <= arr[i])
+            while(!st.isEmpty() && arr[i] >= arr[st.peek()])
             {
                 st.pop();
             }
@@ -65,13 +91,15 @@ class Solution {
 
     public int[] pgee(int[] arr)
     {
-        Stack<Integer> st = new Stack<Integer>();
         int n = arr.length;
+
+        Stack<Integer> st = new Stack<>();
+
         int[] ind = new int[n];
 
         for(int i = 0; i < n; i++)
         {
-            while(!st.isEmpty() &&  arr[st.peek()] < arr[i])
+            while(!st.isEmpty() &&   arr[st.peek()] < arr[i])
             {
                 st.pop();
             }
@@ -84,31 +112,10 @@ class Solution {
         return ind;
     }
 
-    public long sumOfMin(int[] arr)
+    public long sumSubarrayMaxs(int[] arr)
     {
-        final int MOD = (int)(1e9 + 7);
-        long total = 0;
         int n = arr.length;
-        int[] prev = psee(arr);
-        int[] next = nse(arr);
-
-        for(int i = 0; i < n; i++)
-        {
-           long left = i - prev[i];
-            long right = next[i] - i;
-
-           long contribution = (left * right) * arr[i];
-            total = (total + contribution);
-        }
-
-        return total;
-    }
-
-    public long sumOfMax(int[] arr)
-    {
-        final int MOD = (int)(1e9 + 7);
         long total = 0;
-        int n  = arr.length;
         int[] prev = pgee(arr);
         int[] next = nge(arr);
 
@@ -117,14 +124,15 @@ class Solution {
             long left = i - prev[i];
             long right = next[i] - i;
 
-           long contribution = (left * right) * arr[i];
+            long contribution = (left * right * arr[i]);
             total = (total + contribution);
         }
 
         return total;
     }
 
+
     public long subArrayRanges(int[] nums) {
-        return sumOfMax(nums) - sumOfMin(nums);
+        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
     }
 }
