e77b9712bbe4e85b11570f4aefa0c947a3c740c3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-21 13:05:24 +0530

    Time: 68 ms (61.67%), Space: 57.7 MB (33.86%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,68 +1,34 @@
 class Solution {
-
-     public int[] nse(int[] arr)
-    {
-        int n = arr.length;
-
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
         Stack<Integer> st = new Stack<>();
-        int[] ind = new int[n];
-
-        for(int i = n - 1; i >= 0; i--)
-        {
-            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
-            {
-                st.pop();
-            }
-
-            ind[i] = st.isEmpty() ? n : st.peek();
-
-            st.push(i); 
-        }
-
-        return ind;
-    }
-
-     public int[] pse(int[] arr)
-    {
-        int n = arr.length;
-        Stack<Integer> st = new Stack<>();
-        int[] ind = new int[n];
+        int area = 0;
 
         for(int i = 0; i < n; i++)
         {
-            while(!st.isEmpty() && arr[st.peek()] >= arr[i])
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i])
             {
+                int right = i;
+                int element = heights[st.peek()];
                 st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                int curr = (element*(right - left - 1));
+                area = Math.max(area,curr);
             }
-
-            ind[i] = st.isEmpty() ? -1 : st.peek();
 
             st.push(i);
         }
 
-        return ind;
-    }
-
-    public int largestRectangleArea(int[] heights) {
-
-        // T.C -> O(5*N)
-        // S.C -> O(2*N) + O(2*N) + O(2*N)(prev,next) -> O(6N)
-        int area = 0;
-        int[] prev = pse(heights); // T.C -> O(2*N), S.C -> O(2*N)
-        int[] next = nse(heights); // T.C -> O(2*N), S.C -> O(2*N)
-
-        int n = heights.length;
-
-        for(int i = 0; i < n; i++) // T.C -> O(N)
+        while(!st.isEmpty())
         {
-            int left = prev[i];
-            int right = next[i];
-
-            int curr = (right - left - 1) * (heights[i]);
-            area = Math.max(curr,area);
+            int right = n;
+            int element = heights[st.peek()];
+            st.pop();
+            int left = st.isEmpty() ? -1 : st.peek();
+            int curr = (element*(right - left - 1));
+            area = Math.max(area,curr);
         }
 
         return area;
-        
     }
 }
