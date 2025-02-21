bfe49165b188cf3a8afa25b1eec0ede4be30e405
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-21 20:17:43 +0530

    Time: 42 ms (13.35%), Space: 45.7 MB (6.96%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,55 +1,51 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         
+        int n = s.length();
         int l = 0;
         int r = 0;
-        int n = s.length();
-        int maxf = 0;
-        HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
-        int maxLen = 0;
-
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxfreq = 0;
+        int maxlen = 0;
 
         while(r < n)
         {
-            hmap.put(s.charAt(r), hmap.getOrDefault(s.charAt(r),0) + 1);
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
 
-            maxf = Math.max(maxf,hmap.get(s.charAt(r)));
+            maxfreq = Math.max(maxfreq, map.get(s.charAt(r)));
 
             int len = r - l + 1;
-            if((len - maxf) <= k)
-            {
-                maxLen = Math.max(maxLen,len);
-            }
-            else
-            {
 
-                if(((r - l + 1) - maxf) > k)
+            int changes = len - maxfreq;
+
+            while(changes > k)
+            {
+                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+
+                if(map.get(s.charAt(l)) == 0)
                 {
-                    hmap.put(s.charAt(l), hmap.get(s.charAt(l)) - 1);
-                     
-                    // maxf =  0;
+                    map.remove(s.charAt(l));
+                }
 
-                    if(hmap.get(s.charAt(l)) == 0)
-                    {
-                        hmap.remove(s.charAt(l));
-                    }
+                maxfreq = 0;
+                for(Integer val : map.values())
+                {
 
-                    // for(Integer val : hmap.values())
-                    // {
-                    //     maxf = Math.max(maxf,val);
-                    // }
-
-                    l++;
+                maxfreq = Math.max(maxfreq, val);
 
                 }
 
-                maxLen = Math.max(maxLen,(r - l + 1));
+                l++;
+
+                changes = (r - l + 1) - maxfreq;
             }
 
+            len = r - l + 1;
+
+            maxlen = Math.max(maxlen, len);
             r++;
         }
 
-        return maxLen;
-
+        return maxlen;
     }
 }
