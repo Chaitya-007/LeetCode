5a1fb5e9879e9ef2c16cec9dae62a8f1c53048ed
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-23 13:40:37 +0530

    Time: 10 ms (81.06%), Space: 48.3 MB (88.65%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,21 +1,30 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        // Strivers approach
-        int m = s.length;
-        int n = g.length;
         Arrays.sort(g);
         Arrays.sort(s);
+
         int l = 0;
         int r = 0;
+        int m = g.length;
+        int n = s.length;
+        int cnt = 0;
+
         while(l < m && r < n)
         {
-            if(s[l] >= g[r])
+            if(s[r] >= g[l])
+            {
+                cnt++;
+                l++;
+                r++;
+            }
+            else
             {
                 r++;
             }
-            l++;
+
         }
 
-        return r;
+        return cnt;
+
     }
 }
