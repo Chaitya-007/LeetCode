1ef2d9726022a9fb5abf16a7b504cd6c95da09cf
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-26 12:27:09 +0530

    Time: 126 ms (6.42%), Space: 46.2 MB (5.86%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public int fun(int ind,int[] nums)
+    {
+        if(ind >= (nums.length - 1)) return 0;
+
+        int mino = nums.length;
+        for(int cnt = 1; cnt <= nums[ind]; cnt++)
+        {
+            int left = 1 + fun( ind+cnt, nums);
+            mino = Math.min(left,mino);
+        } 
+
+        return mino;
+    }
+
+    public int funMemo(int ind,int[] nums,int[] dp)
+    {
+        if(ind >= (nums.length - 1)) return 0;
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int mino = nums.length;
+        for(int cnt = 1; cnt <= nums[ind]; cnt++)
+        {
+            int left = 1 + funMemo( ind+cnt, nums, dp);
+            mino = Math.min(left,mino);
+        } 
+
+        return dp[ind] = mino;
+    }
+
+    public int jump(int[] nums) {
+        // return fun(0,nums);
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+
+        return funMemo(0, nums, dp);
+    }
+}
