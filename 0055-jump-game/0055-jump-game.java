e1f0f052c4b344f031e04d906661eec9a06a0e1b
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-26 11:11:25 +0530

    Time: 1 ms (99.91%), Space: 45.9 MB (30.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,77 +1,20 @@
 class Solution {
-
-    public boolean fun(int ind, int[] nums)
-    {
-        if(ind == (nums.length) - 1)
-        {
-            return true;
-        }
-
-        for(int cnt = 1; cnt <= nums[ind]; cnt++)
-        {
-            if(ind + cnt < (nums.length))
-            {
-                if(fun(ind+cnt, nums))
-                {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-     public boolean funmemo(int ind, int[] nums, boolean[] dp)
-    {
-        if(ind == (nums.length) - 1)
-        {
-            return true;
-        }
-
-        if(dp[ind]) return dp[ind];
-
-        for(int cnt = 1; cnt <= nums[ind]; cnt++)
-        {
-            if(ind + cnt < (nums.length))
-            {
-                if(funmemo(ind+cnt, nums, dp))
-                {
-                    return dp[ind] = true;
-                }
-            }
-        }
-
-        return dp[ind] = false;
-    }
-
     public boolean canJump(int[] nums) {
-    
-        // return fun(0,nums);
         int n = nums.length;
-        boolean[] dp = new boolean[n];
-        Arrays.fill(dp,false);
+        int need = 1;
 
-        // return funmemo(0,nums,dp);
-
-        dp[n-1] = true;
-
-        for(int ind = n - 2; ind >= 0; ind--)
+        for(int i = n - 2; i >= 0; i--)
         {
-
-        for(int cnt = 1; cnt <= nums[ind]; cnt++)
-        {
-            if(ind + cnt < (n))
+            if(nums[i] >= need)
             {
-                if(dp[ind+cnt])
-                {
-                     dp[ind] = true;
-                     break;
-                }
+                need = 1;
+            }
+            else
+            {
+                need++;
             }
         }
 
-        }
-
-        return dp[0];
+        return need == 1 ? true : false;
     }
 }
