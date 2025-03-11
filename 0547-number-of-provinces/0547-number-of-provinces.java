e9e130497b9a1e2c8829a593cfc6be461211e2a4
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-11 12:04:09 +0530

    Time: 4 ms (20.09%), Space: 47.2 MB (77.88%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,55 +1,62 @@
 class Solution {
 
-    public void dfsFunc(int node, int[] vis, ArrayList<ArrayList<Integer>> adj)
+    public void dfs(int node,boolean[] vis,List<List<Integer>> adj)
     {
-        vis[node] = 1;
-        for(Integer it : adj.get(node))
+        vis[node] = true;
+
+        for(Integer val : adj.get(node))
         {
-            if(vis[it] != 1)
+            if(!vis[val])
             {
-                dfsFunc(it,vis,adj);
+                dfs(val,vis,adj);
             }
         }
     }
 
+
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int vertices = isConnected.length;
 
-        int V = isConnected.length;
+        List<List<Integer>> adj = new ArrayList<>();
 
-        for(int i = 0; i < (V+1); i++)
+        for(int i = 0; i <= vertices; i++)
         {
-            adj.add(new ArrayList<>());
+            adj.add(new ArrayList<Integer>());
         }
 
-        for(int i = 0; i < isConnected.length; i++)
+        int n = isConnected.length;
+        int m = isConnected[0].length;
+
+        for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < isConnected[i].length; j++)
+            for(int j = 0; j < m; j++)
             {
-                if(i != j && isConnected[i][j] == 1)
-                {
-                    int u = i + 1;
-                    int v = j + 1;
-                    adj.get(u).add(v);
-                    adj.get(v).add(u);
-                }
+                // if(i != j)
+                // {
+                    if(isConnected[i][j] == 1)
+                    {
+                        int u = i+1;
+                        int v = j+1;
+                        adj.get(u).add(v);
+                        adj.get(v).add(u);
+                    }
+                // }
             }
         }
 
-        int[] vis = new int[V+1];
-        Arrays.fill(vis,0);
+        boolean[] vis = new boolean[vertices+1];
         int cnt = 0;
 
-        for(int node = 1; node <= V; node++)
+        for(int i = 1; i <= vertices; i++)
         {
-            if(vis[node] != 1)
+            if(!vis[i])
             {
-                dfsFunc(node,vis,adj);
                 cnt++;
+                dfs(i,vis,adj);
             }
         }
 
         return cnt;
-
+        
     }
 }
