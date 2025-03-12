d4a02348c3791d08d61b83b92ab06c7cde9fb8b3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-12 07:13:52 +0530

    Time: 1 ms (47.64%), Space: 45.2 MB (31.83%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,78 +1,45 @@
-class Pair
-{
-    int row;
-    int col;
-
-    public Pair(int row_,int col_)
-    {
-        this.row = row_;
-        this.col = col_;
-    }
-
-    public int getRow()
-    {
-        return row;
-    }
-
-    public int getCol()
-    {
-        return col;
-    }
-}
-
-
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+    public void dfs(int row,int col, int[][] image,int[][] ans, int color, int orgColor,int[] delRow,int[] delCol)
+    {
+        ans[row][col] = color;
         int n = image.length;
         int m = image[0].length;
 
-        if(image[sr][sc] == color) return image;
+        for(int i = 0; i < 4; i++)
+        {
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
 
-        Queue<Pair> q = new LinkedList<>();
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == orgColor && ans[nrow][ncol] != color)
+            {
+                dfs(nrow,ncol,image,ans,color,orgColor,delRow,delCol);
+            }
+        }
 
-        // int[][] vis = new int[n][m];
 
-        int[][] grid = new int[n][m];
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+        int n = image.length;
+        int m = image[0].length;
+
+        int[][] ans = new int[n][m];
 
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
-                grid[i][j] = image[i][j];
-                // vis[i][j] = -1;
+                ans[i][j] = image[i][j];
             }
         }
-
-        q.offer(new Pair(sr,sc));
-        // vis[sr][sc] = color;
 
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
+        
+        dfs(sr,sc,image,ans,color,image[sr][sc],delRow,delCol);
 
-        int orgColor = image[sr][sc];
-        grid[sr][sc] = color;
-
-        while(!q.isEmpty())
-        {
-            int r = q.peek().getRow();
-            int c = q.peek().getCol();
-            q.poll();
-
-            for(int i = 0; i < 4; i++)
-            {
-                int nrow = r + delRow[i];
-                int ncol = c + delCol[i];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == orgColor)
-                {
-                    grid[nrow][ncol] = color;
-                    q.offer(new Pair(nrow,ncol));
-                }
-            }
-        }
-
-        return grid;
-
-
+        return ans;
     }
 }
