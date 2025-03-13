2d504e6a2ce3ec4c95b57fbcc0e091635e4ac92e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-13 13:04:51 +0530

    Time: 9 ms (59.39%), Space: 60.4 MB (21.44%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -20,6 +20,10 @@ class Solution {
     }
 
     public int numEnclaves(int[][] grid) {
+
+        // T.C -> O(N*M*4) + O(N) + O(N)
+        // S.C -> O(N-*M
+        
         int n = grid.length;
         int m = grid[0].length;
 
