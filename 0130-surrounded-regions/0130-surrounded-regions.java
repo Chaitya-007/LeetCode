17b2ad4a2dd6793af05db1b0b4bc093ea350a3e5
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-13 12:22:36 +0530

    Time: 1 ms (99.59%), Space: 45.8 MB (28.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,79 +1,59 @@
-class Pair
-{
-    int row;
-    int col;
-
-    public Pair(int row_, int col_)
-    {
-        this.row = row_;
-        this.col = col_;
-    }
-
-    public int getRow()
-    {
-        return row;
-    }
-
-    public int getCol()
-    {
-        return col;
-    }
-}
-
 class Solution {
+
+    public void dfs(int row, int col, boolean[][] visited, char[][] board, int[] delRow, int[] delCol)
+    {
+        visited[row][col] = true;
+
+        int n = board.length;
+        int m = board[0].length;
+
+        for(int i = 0; i < 4; i++)
+        {
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && board[nrow][ncol] == 'O' && !visited[nrow][ncol])
+            {
+                dfs(nrow,ncol,visited,board,delRow,delCol);
+            }
+        }
+    }
+
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
 
-        boolean[][] visited = new boolean[n][m];
-        Queue<Pair> q = new LinkedList<>();
-
-
-        for(int i = 0; i < n; i++)
-        {
-
-            for(int j = 0; j < m; j++)
-            {
-               
-                if(board[i][j] == 'O')
-                {
-
-                if( (i == 0 || i == n - 1) && !visited[i][j])
-                {
-                    visited[i][j] = true;
-                    q.offer(new Pair(i,j));
-                }
-
-                if( (j == 0 || j == m - 1) && !visited[i][j] )
-                {
-                    visited[i][j] = true;
-                    q.offer(new Pair(i,j));
-                }
-                }
-
-
-            }
-        }
-
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
 
-        while(!q.isEmpty())
+        boolean[][] visited = new boolean[n][m];
+
+        for(int j = 0; j < m; j++)
         {
-            int row = q.peek().getRow();
-            int col = q.peek().getCol();
-            q.poll();
-
-            for(int i = 0; i < 4; i++)
+            if(board[0][j] == 'O' && !visited[0][j])
             {
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
+                dfs(0,j,visited,board,delRow,delCol);
+            }
 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && board[nrow][ncol] == 'O' && !visited[nrow][ncol])
-                {
-                    q.offer(new Pair(nrow,ncol));
-                    visited[nrow][ncol] = true;
-                }
+            if(board[n-1][j] == 'O' && !visited[n-1][j])
+            {
+
+                dfs(n-1,j,visited,board,delRow,delCol);
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            if(board[i][0] == 'O' && !visited[i][0])
+            {
+                dfs(i,0,visited,board,delRow,delCol);
+
+            }
+
+            if(board[i][m-1] == 'O' && !visited[i][m-1])
+            {
+                dfs(i,m-1,visited,board,delRow,delCol);
+
             }
         }
 
@@ -87,8 +67,5 @@ class Solution {
                 }
             }
         }
-
-
-       
     }
 }
