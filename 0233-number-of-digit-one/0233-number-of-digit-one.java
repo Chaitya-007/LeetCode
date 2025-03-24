566db4bdbfbe638ec6e2b96ac6d94402554e1ad7
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-24 17:31:28 +0530

    Time: 0 ms (100%), Space: 40.5 MB (51.61%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.java
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -1,17 +1,17 @@
 class Solution {
 
-    int[][][][] dp = new int[10][2][10][2];
+    int[][][] dp = new int[10][2][10];
 
-    public int fun(int ind, int tight, int cnt, int validnum, String s)
+    public int fun(int ind, int tight, int cnt, String s)
     {
         if(ind == s.length())
         {
             return cnt;
         }
 
-        if(dp[ind][tight][cnt][validnum] != -1)
+        if(dp[ind][tight][cnt] != -1)
         {
-            return dp[ind][tight][cnt][validnum];
+            return dp[ind][tight][cnt];
         }
 
         int ans = 0;
@@ -22,36 +22,11 @@ class Solution {
         {
             int newTight = (tight == 1 && (d == limit)) ? 1 : 0;
 
-            if(validnum == 0 && d == 0) // adding leading zeroes
-            {
-                ans += fun(ind + 1, newTight, cnt, 0, s);
-            }
-            else if(validnum == 0 && d > 0)
-            {
-                if(d == 1)
-                {
-                    ans += fun(ind + 1, newTight, cnt + 1, 1, s);
-                }
-                else 
-                {
-                    ans += fun(ind + 1, newTight, cnt, 1, s);
+           ans += fun(ind + 1, newTight, cnt + (d == 1 ? 1 : 0), s);
 
-                }
-            }
-            else if(validnum == 1)
-            {
-                if(d == 1)
-                {
-                    ans += fun(ind + 1, newTight, cnt + 1, 1, s);
-                }
-                else
-                {
-                    ans += fun(ind + 1, newTight, cnt, 1, s);
-                }
-            }
         }
 
-        return dp[ind][tight][cnt][validnum] = ans;
+        return dp[ind][tight][cnt] = ans;
     }
 
     public int countDigitOne(int n) {
@@ -64,15 +39,14 @@ class Solution {
             {
                 for(int k = 0; k < 10; k++)
                 {
-                    for(int m = 0; m < 2; m++)
-                    {
-                        dp[i][j][k][m] = -1;
-                    }
+                    
+                        dp[i][j][k]= -1;
+                    
                 }
             }
         }
 
-        return fun(0,1,0,0,h);
+        return fun(0,1,0,h);
         
     }
 }
