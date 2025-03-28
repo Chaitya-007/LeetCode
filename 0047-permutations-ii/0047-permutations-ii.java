500d2a1b6902577a48889311d36d43b9307fb69c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-28 19:42:10 +0530

    Time: 1 ms (99.74%), Space: 45.2 MB (31.85%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,6 +1,6 @@
 class Solution {
 
-     public void solve(int ind, int[] nums, HashSet<List<Integer>> ans, boolean[] vis, List<Integer> list)
+     public void solve(int ind, int[] nums, List<List<Integer>> ans, boolean[] vis, List<Integer> list)
     {
         if(ind == nums.length)
         {
@@ -11,6 +11,12 @@ class Solution {
 
         for(int i = 0; i < nums.length; i++)
         {
+
+            if(i > 0 && nums[i] == nums[i-1] && !vis[i-1])
+            {
+                continue;
+            }
+
             if(!vis[i])
             {
                 vis[i] = true;
@@ -26,13 +32,16 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
 
         int n = nums.length;
-        HashSet<List<Integer>> ans = new HashSet<>();
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
         boolean[] vis = new boolean[n];
         List<Integer> list = new ArrayList<>();
 
         solve(0,nums,ans,vis,list);
-        List<List<Integer>> res = new ArrayList<>(ans);
-        return res;
+        
+        return ans;
         
     }
 }
