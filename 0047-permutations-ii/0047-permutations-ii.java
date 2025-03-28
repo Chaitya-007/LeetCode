8b2fa6e527eeae86e6d16787adb97a07c8d4b59f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-28 22:26:58 +0530

    Time: 3 ms (44.04%), Space: 45 MB (46.34%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -28,18 +28,49 @@ class Solution {
         }
     }
 
+     public void solve(int ind, int[] nums, List<List<Integer>> ans, HashMap<Integer, Integer> map, List<Integer> list)
+    {
+        if(ind == nums.length)
+        {
+            List<Integer> seq = new ArrayList<>(list);
+            ans.add(seq);
+            return;
+        }
+
+       for(Map.Entry<Integer, Integer> e : map.entrySet())
+       {
+            if(e.getValue() > 0)
+            {
+                map.put(e.getKey(), map.get(e.getKey()) - 1);
+                list.add(e.getKey());
+                solve(ind + 1, nums, ans, map, list);
+                map.put(e.getKey(), map.get(e.getKey()) + 1);
+                list.remove(ind);
+            }
+       }
+    }
+
 
     public List<List<Integer>> permuteUnique(int[] nums) {
 
         int n = nums.length;
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] vis = new boolean[n];
+        // boolean[] vis = new boolean[n];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int val : nums)
+        {
+            map.put(val, map.getOrDefault(val,0) + 1);
+        }
+
+
         List<Integer> list = new ArrayList<>();
 
-        solve(0,nums,ans,vis,list);
+        // solve(0,nums,ans,vis,list);
+        solve(0,nums,ans,map,list);
         
         return ans;
         
