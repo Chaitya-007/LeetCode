1491d3343dd28764664d770b350044dea535fa49
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-31 13:21:26 +0530

    Time: 1 ms (99.87%), Space: 43.5 MB (77.74%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,13 +2,13 @@ class Solution {
 
     public void fun(int ind, int[] nums, List<Integer> list, List<List<Integer>> ans)
     {
+        ans.add(new ArrayList<>(list));
 
         for(int i = ind; i < nums.length; i++)
         {
             if(i > ind && nums[i] == nums[i-1]) continue;
 
             list.add(nums[i]);
-            ans.add(new ArrayList<>(list));
             fun(i + 1, nums, list, ans);
             list.remove(list.size() - 1);
         }
@@ -20,7 +20,6 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
 
         List<Integer> list = new ArrayList<>();
-        ans.add(new ArrayList<>(list));
 
         fun(0,nums,list,ans);
 
