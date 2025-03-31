d80c9e9349a58e75b6ba739e34aa77774b416978
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-31 14:36:22 +0530

    Time: 1 ms (92.94%), Space: 44.9 MB (19.47%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -22,13 +22,47 @@ class Solution {
         }
     }
 
+
+    public void opt(int ind, int[] nums, List<List<Integer>> ans)
+    {
+
+        if(ind == nums.length)
+        {
+            List<Integer> list = new ArrayList<>();
+            for(int val : nums)
+            {
+                list.add(val);
+            }
+
+            ans.add(new ArrayList<>(list));
+
+            return;
+        }
+
+        
+        for(int i = ind; i < nums.length; i++)
+        {
+            swap(ind, i, nums);
+            opt(ind + 1, nums, ans);
+            swap(ind, i, nums);
+        }
+    }
+
+    public void swap(int ind, int i, int[] nums)
+    {
+        int temp = nums[ind];
+        nums[ind] = nums[i];
+        nums[i] = temp;
+    }
+
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         boolean[] vis = new boolean[n];
         List<Integer> list = new ArrayList<>();
 
-        solve(0,nums,ans,vis,list);
+        // solve(0,nums,ans,vis,list);
+        opt(0,nums,ans);
         return ans;
     }
 }
