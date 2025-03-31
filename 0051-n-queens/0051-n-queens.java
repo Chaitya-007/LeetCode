d3519d8fd2b011fb6de48eb5bfe07ee10edd7769
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-03-31 16:27:20 +0530

    Time: 1 ms (99.81%), Space: 45 MB (56.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -65,10 +65,42 @@ class Solution {
         }
     }
 
+    public void opt(int col, List<List<String>> ans, char[][] board, int n, int[] leftrow, int[] lowerdia, int[] upprdia)
+    {
+
+        if(col == n)
+        {
+            List<String> solution = new ArrayList<>();
+
+            for(char[] ch : board)
+            {
+                solution.add(new String(ch));
+            }
+
+            ans.add(solution);
+            return;
+        }
+
+
+        for(int row = 0; row < n; row++)
+        {
+            if(leftrow[row] == 0 && lowerdia[row + col] == 0 && upprdia[n - 1 + col - row] == 0)
+            {
+                board[row][col] = 'Q';
+                leftrow[row] = 1;
+                lowerdia[row + col] = 1;
+                upprdia[n - 1 + col - row] = 1;
+                opt(col + 1, ans, board, n, leftrow, lowerdia, upprdia);
+                board[row][col] = '.';
+                leftrow[row] = 0;
+                lowerdia[row + col] = 0;
+                upprdia[n - 1 + col - row] = 0;
+            }
+        }
+    }
+
     public List<List<String>> solveNQueens(int n) {
      char[][] board = new char[n][n];
-
-     
 
      for(int i = 0; i < n; i++)
      {
@@ -77,7 +109,13 @@ class Solution {
 
      List<List<String>> ans = new ArrayList<>();
 
-     solve(0, ans, board, n);
+    //  solve(0, ans, board, n);
+
+    int[] leftrow = new int[n];
+    int[] lowerdia = new int[(2*n) - 1];
+    int[] upprdia = new int[(2*n) - 1];
+
+     opt(0, ans, board, n, leftrow, lowerdia, upprdia);
 
      return ans;
     }
