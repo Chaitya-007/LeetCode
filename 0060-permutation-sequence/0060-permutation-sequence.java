da190529073623c6f90b2052f1a74133b0583458
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-04-01 21:58:03 +0530

    Time: 1 ms (94.83%), Space: 41 MB (78.91%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,74 +1,48 @@
 class Solution {
 
-
-    public void nextPermutation(int[] nums)
+    public int fact(int num)
     {
-        int n = nums.length;
-        int i = n - 1;
-        while(i - 1 >= 0 && nums[i-1] > nums[i])
+        int fact = 1;
+
+        for(int cnt = 1; cnt <= num; cnt++)
         {
-            i--;
+            fact *= cnt;
         }
 
-
-        if(i > 0)
-        {
-            int breakpoint = i - 1;
-            int j = n - 1;
-            while(nums[breakpoint] > nums[j])
-            {
-                j--;
-            }
-            int temp = nums[j];
-            nums[j] = nums[breakpoint];
-            nums[breakpoint] = temp;
-
-            int l = i;
-            int r = n - 1;
-            while(l < r)
-            {
-                int t = nums[l];
-                nums[l] = nums[r];
-                nums[r] = t;
-                l++;
-                r--;
-            }
-        }
-
-        // System.out.println(Arrays.asList(nums));
+        return fact;
     }
+
 
     public String getPermutation(int n, int k) {
 
-        if(n == 1) 
-        {
-            char ch = (char)(k + '0');
-            StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
 
-            return sb.append(ch).toString();
-        }
-
-        StringBuilder sb = new StringBuilder("");
-        int[] nums = new int[n];
+        List<Integer> list = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
 
         for(int i = 1; i <= n; i++)
         {
-            nums[i - 1] = i;
+            list.add(i);
         }
 
-        for(int i = 1; i <= k - 1; i++)
-        {
-            nextPermutation(nums);
-            // for(int val : nums)
-            // {
-            //     System.out.print(val + " ");
-            // }
-            // System.out.println();
-        }
+        k = k - 1;
+        int temp = n - 1;
+        int div = fact(temp);
+        int ind = k/div;
 
-        for(int val : nums)
+        while(sb.length() != n)
         {
-            sb.append(val);
+
+            Integer num = list.get(ind);
+            list.remove(ind);
+            // ans.add(num);
+            sb.append(num);
+
+            k = k%div;
+            temp--;
+            div = fact(temp);
+            ind = k/div;
+            
         }
 
         return sb.toString();
