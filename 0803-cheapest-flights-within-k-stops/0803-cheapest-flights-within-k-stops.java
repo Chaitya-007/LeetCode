8f314c7ac02d158e99877baf0d74e0646608a606
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-04-10 18:50:36 +0530

    Time: 8 ms (35.38%), Space: 44.3 MB (70.25%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -0,0 +1,110 @@
+
+class DisNodeK
+{
+    int dis;
+    int node;
+    int k;
+
+    public DisNodeK(int k,int n,int d)
+    {
+        this.dis = d;
+        this.node = n;
+        this.k = k;
+    }
+
+    public int getDis()
+    {
+        return dis;
+    }
+
+    public int getNode()
+    {
+        return node;
+    }
+
+    public int getK()
+    {
+        return k;
+    }
+}
+
+class Pair
+{
+    int v;
+    int w;
+
+    public Pair(int v,int w)
+    {
+        this.v = v;
+        this.w = w;
+    }
+
+    public int getV()
+    {
+        return v;
+    }
+
+    public int getW()
+    {
+        return w;
+    }
+}
+
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < flights.length; i++)
+        {
+            adj.get(flights[i][0]).add(new Pair(flights[i][1],flights[i][2]));
+        }
+
+        Comparator<DisNodeK> cmp = (p,q) ->
+        {
+            return Integer.compare(p.getDis(),q.getDis());
+        };
+
+        // PriorityQueue<DisNodeK> pq = new PriorityQueue<>(cmp);
+        Queue<DisNodeK> pq = new LinkedList<>();
+        pq.offer(new DisNodeK(0,src,0));
+
+        int[] dis = new int[n];
+        Arrays.fill(dis,Integer.MAX_VALUE);
+        dis[src] = 0;
+
+        while(!pq.isEmpty())
+        {
+            DisNodeK p = pq.poll();
+            int node = p.getNode();
+            int distance = p.getDis();
+            int newK = p.getK();
+
+            if( newK > k)
+            {
+                continue;
+            }
+
+            for(Pair adjNode : adj.get(node))
+            {
+                int adjv = adjNode.getV();
+                int edgeWt = adjNode.getW();
+                 
+                    if(distance + edgeWt < dis[adjv] && newK <= k)
+                    {
+                        dis[adjv] = distance + edgeWt;
+                        pq.offer(new DisNodeK(newK+1,adjv,dis[adjv]));
+                    }
+                
+
+            }
+        }
+
+        return dis[dst] == Integer.MAX_VALUE ? -1 : dis[dst];
+        
+    }
+}
