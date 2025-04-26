e6fb0c2fd5d72e977eb6214ef31b2cb6aafd1d81
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-04-26 18:41:45 +0530

    Time: 24 ms (12.25%), Space: 45 MB (13.68%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,89 +1,103 @@
-class CustomNode
+class Disjoint
 {
-    int val;
-    int row;
-    int col;
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
 
-    public CustomNode(int v,int r, int c)
+    public Disjoint(int n)
     {
-        this.val = v;
-        this.row = r;
-        this.col = c;
+        for(int i = 0; i < n; i++)
+        {
+            parent.add(i);
+            size.add(1);
+        }
     }
 
-    public int getVal()
+    public int findUpar(int node)
     {
-        return val;
+        if(node == parent.get(node))
+        {
+            return node;
+        }
+
+        int updPar = findUpar(parent.get(node));
+        parent.set(node,updPar);
+        return updPar;
     }
 
-    public int getRow()
+    public void unionBySize(int u,int v)
     {
-        return row;
-    }
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
 
-    public int getCol()
-    {
-        return col;
+        if(size.get(ulp_u) < size.get(ulp_v))
+        {
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+        }
+        else
+        {
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u, size.get(ulp_v) + size.get(ulp_u));
+        }
     }
 }
 
-
 class Solution {
-    public int swimInWater(int[][] grid) 
-    {
+    public int swimInWater(int[][] grid) {
+        List<Integer> positions = new ArrayList<>();
+
         int n = grid.length;
-        int[][] dis = new int[n][n];
 
         for(int i = 0; i < n; i++)
         {
-            Arrays.fill(dis[i],Integer.MAX_VALUE);
+            for(int j = 0; j < n; j++)
+            {
+                positions.add((i*n + j));
+            }
         }
 
-        dis[0][0] = grid[0][0];
-
-        Comparator<CustomNode> cmp = (p,q) -> 
+        Comparator<Integer> cmp = (p,q) ->
         {
-            return Integer.compare(p.getVal(),q.getVal());
+            int row = p/n;
+            int col = p%n;
+
+            int nextrow = q/n;
+            int nextcol = q%n;
+
+            // Sorting in Increasing Order
+            return Integer.compare(grid[row][col],grid[nextrow][nextcol]);
+
         };
 
-        PriorityQueue<CustomNode> pq = new PriorityQueue<>(cmp);
-        pq.offer(new CustomNode(grid[0][0],0,0));
+        Collections.sort(positions,cmp);
+
+        Disjoint ds = new Disjoint(n*n);
 
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
 
-        while(!pq.isEmpty())
+        for(Integer position : positions)
         {
-            CustomNode c = pq.poll();
-            int val = c.getVal();
-            int row = c.getRow();
-            int col = c.getCol();
+            int row = position/n;
+            int col = position%n;
 
             for(int i = 0; i < 4; i++)
             {
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n)
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && grid[row][col] > grid[nrow][ncol])
                 {
-                    int maxo = Math.max(val,grid[nrow][ncol]);
-                    if(dis[nrow][ncol] > maxo)
-                    {
-                        dis[nrow][ncol] = maxo;
-                        if(nrow == n - 1 && ncol == n - 1)
-                        {
-                            return dis[n-1][n-1];
-                        }
-                        pq.offer(new CustomNode(dis[nrow][ncol],nrow,ncol));
-                    }
-
+                    ds.unionBySize(position, nrow*n + ncol);
                 }
-
             }
-        
+
+            if(ds.findUpar(0) == ds.findUpar(n*n - 1))
+            {
+                return grid[row][col];
+            }
         }
 
-            return dis[n-1][n-1];
-
+        return -1;
     }
 }
