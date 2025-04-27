80fc3bf68988bc87500eb5151197188bd3f75153
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-04-27 13:52:21 +0530

    Time: 125 ms (18.65%), Space: 111 MB (92.28%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -2,37 +2,74 @@ class Solution {
 
     int timer = 1;
 
-    public void dfs(int node,int parent,boolean[] vis,List<List<Integer>> adj,List<List<Integer>> ans,int[] startingTime,int[] lowest)
+       public void dfs(int node,int parent,boolean[] vis,List<List<Integer>> adj,List<List<Integer>> ans,int[] startingTime,int[] lowest)
     {
         vis[node] = true;
-        startingTime[node] =  lowest[node] = timer;
+        startingTime[node] = timer;
+        lowest[node] = timer;
         timer++;
-       
 
         for(Integer adjNode : adj.get(node))
         {
-            if(adjNode == parent) continue;
-
             if(!vis[adjNode])
             {
                 dfs(adjNode,node,vis,adj,ans,startingTime,lowest);
-                lowest[node] = Math.min(lowest[node], lowest[adjNode]);
-                if(lowest[adjNode] > startingTime[node])
-                {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(adjNode);
-                    list.add(node);
-                    ans.add(new ArrayList<>(list));
-                }
-            }
-            else
-            {
-                // You are coming by visiting all the node via dfs
-                lowest[node] = Math.min(lowest[node],lowest[adjNode]);
             }
         }
 
+        if(parent != -1)
+        {
+        for(Integer adjNode : adj.get(node))
+        {
+            if(adjNode != parent)
+            {
+                lowest[node] = Math.min(lowest[node], lowest[adjNode]);
+            }
+        }
+
+        if(lowest[node] > startingTime[parent])
+        {
+            List<Integer> list = new ArrayList<>();
+            list.add(node);
+            list.add(parent);
+            ans.add(new ArrayList<>(list));
+        }
+
+        }
+
     }
+
+    // public void dfs(int node,int parent,boolean[] vis,List<List<Integer>> adj,List<List<Integer>> ans,int[] startingTime,int[] lowest)
+    // {
+    //     vis[node] = true;
+    //     startingTime[node] =  lowest[node] = timer;
+    //     timer++;
+       
+
+    //     for(Integer adjNode : adj.get(node))
+    //     {
+    //         if(adjNode == parent) continue;
+
+    //         if(!vis[adjNode])
+    //         {
+    //             dfs(adjNode,node,vis,adj,ans,startingTime,lowest);
+    //             lowest[node] = Math.min(lowest[node], lowest[adjNode]);
+    //             if(lowest[adjNode] > startingTime[node])
+    //             {
+    //                 List<Integer> list = new ArrayList<>();
+    //                 list.add(adjNode);
+    //                 list.add(node);
+    //                 ans.add(new ArrayList<>(list));
+    //             }
+    //         }
+    //         else
+    //         {
+    //             // You are coming by visiting all the node via dfs
+    //             lowest[node] = Math.min(lowest[node],lowest[adjNode]);
+    //         }
+    //     }
+
+    // }
 
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         List<List<Integer>> adj = new ArrayList<>();
