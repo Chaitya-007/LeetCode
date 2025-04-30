f722069c11af70692082d557e20885eedb291f11
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-04-30 16:03:25 +0530

    Time: 498 ms (68.46%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,13 @@
 # Write your MySQL query statement below
 select w1.id from Weather w1 join Weather w2 where datediff(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+
+-- Components
+-- DATEDIFF(date1, date2):
+
+-- This function calculates the difference in days between date1 and date2.
+-- The result is an integer representing the number of days.
+-- The order of the arguments matters:
+-- DATEDIFF(w1.recordDate, w2.recordDate) means it calculates w1.recordDate - w2.recordDate.
+-- = 1:
+
+-- This condition ensures the difference
