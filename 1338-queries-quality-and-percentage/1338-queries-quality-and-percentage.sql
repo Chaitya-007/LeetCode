006928bf4294ad1493a07caa38bf66ef6be31133
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-05-06 15:41:23 +0530

    Time: 396 ms (49.21%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -2,6 +2,6 @@
 
 select q.query_name
 ,round( (sum(q.rating/q.position))/count(*), 2) as quality
-,round( avg ( if(q.rating < 3,1,0) ) * 100.0 , 2) as poor_query_percentage
+,round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2)  as poor_query_percentage
 from Queries q
 group by q.query_name;
