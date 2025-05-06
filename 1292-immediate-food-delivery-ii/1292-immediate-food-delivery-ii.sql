8dc19d90efeacf17fa9216f5647ef9958958bf19
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-05-06 17:28:36 +0530

    Time: 641 ms (69.19%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
--- select ( sum(if( (d.order_date limit 1) = (d.customer_pref_delivery_date limit 1),1,0))*100 )/(select count(distinct customer_id) from Delivery) as immediate_percentage
+-- # Write your MySQL query statement below
 select round( avg( if(d.order_date = d.customer_pref_delivery_date,1,0) )*100.0, 2) as immediate_percentage
 from Delivery d
 where (d.customer_id,d.order_date) in (
@@ -7,4 +6,3 @@ where (d.customer_id,d.order_date) in (
     from delivery
     group by customer_id
 );
-
