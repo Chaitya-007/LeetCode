07b40cf04cb709e578b91dda4f9dfd770e39046f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-05-08 11:23:43 +0530

    Time: 676 ms (55.77%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -4,7 +4,7 @@
 select e.employee_id
 ,e.name
 ,count(*) as reports_count
-,round(avg(f.age)) as average_age
+,round(avg(f.age),0) as average_age
 from Employees e
 inner join Employees f
 on e.employee_id = f.reports_to
