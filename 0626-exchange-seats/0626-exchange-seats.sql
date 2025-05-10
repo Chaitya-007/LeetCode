c885b40021121465eb86522a08fe01825a4cf25c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-05-10 19:17:05 +0530

    Time: 321 ms (96.35%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,40 +1,13 @@
 # Write your MySQL query statement below
 
 
-
-select *
-from
-(
-
-select s.id, 
-        case 
-        when r.id is not null
-        then r.student
-        else s.student
-        end  as student
-from Seat s
-left join Seat r
-on s.id%2 != 0 and s.id = r.id - 1
-where s.id%2 != 0
-
-union
-
 select 
-    case 
-    when r.id is not null
-    then r.id
-    else null
-    end as id,
-    case 
-    when r.id is not null
-    then s.student
-    else null
-    end as student
-from Seat s
-left join Seat r
-on s.id%2 != 0 and s.id = r.id - 1
-where s.id%2 != 0
-) as q
-where id is not null
-order by id;
 
+    case 
+    when id%2 != 0 and id + 1 in (select id from Seat) then id + 1
+    when id%2 = 0 then id - 1
+    else id
+    end as id,
+    student
+from Seat
+order by id;
