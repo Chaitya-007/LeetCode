483f8bdfadc4660f29e5336becbe858aad9d1511
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-05-12 17:24:33 +0530

    Time: 706 ms (81.84%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -1,13 +1,11 @@
 # Write your MySQL query statement below
 
-select distinct product_name,amt as unit
-from
-(
 
-select p.product_name,sum(unit) over(partition by o.product_id ) as amt
+select p.product_name, sum(unit) as unit
 from Orders o
 left join Products p
 on o.product_id = p.product_id
 where substr(o.order_date,1,7) = '2020-02' 
-) as q
-where amt >= 100;
+group by o.product_id
+having sum(unit) >=100;
+
