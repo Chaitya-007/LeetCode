37cf94e2bb7f730b4ce8e62643fe2c4c423bc11c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-06-09 13:11:19 +0530

    Time: 333 ms (97.07%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select * 
 from Patients
-where locate(" DIAB1",conditions) <> 0 or locate("DIAB1",conditions) = 1;
+where conditions like 'DIAB1%' or conditions like '% DIAB1%';
