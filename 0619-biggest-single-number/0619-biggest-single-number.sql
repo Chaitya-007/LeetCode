f23889c0a76ccbdb1b6f179aee4a13d8e72bc10f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-06-10 10:22:09 +0530

    Time: 432 ms (60.2%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
+
 select max(num) as num
-from MyNumbers
-where (num) in (
+from (
+
 select num
 from MyNumbers
 group by num
 having count(*) = 1
-
-)
+) as q;
