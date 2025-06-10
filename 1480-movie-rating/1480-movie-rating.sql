ba76c3a28ffaecd5e3b8b3194079e99f9ff89499
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-06-10 13:16:26 +0530

    Time: 1564 ms (11.64%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,28 +1,23 @@
 # Write your MySQL query statement below
 
-
--- Enclosing each query in parentheses tells MySQL to treat them 
--- as separate subqueries before combining the results with UNION. This corrects the syntax error.
-
 (select u.name as results
-from MovieRating mr 
-left join Users u
-on mr.user_id = u.user_id
-group by mr.user_id
-order by 
-    count(*) desc,
-    u.name asc
-limit 1)
+from MovieRating mr
+inner join Users u
+on u.user_id = mr.user_id
+group by u.name
+order by count(*) desc,u.name asc
+limit 0,1)
+
 
 union all
+
 
 (select m.title as results
 from MovieRating mr
 inner join Movies m
 on mr.movie_id = m.movie_id
-where left(created_at,7) = '2020-02'
-group by mr.movie_id
-order by 
-    avg(rating) desc,
-    m.title asc
-    limit 1);
+where left(mr.created_at,7) = '2020-02'
+group by m.title
+order by avg(rating) desc, m.title asc
+limit 0,1);
+
