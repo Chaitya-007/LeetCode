398f9d5f89de9b9cc7de65fb52b271c3384ee08a
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-06-10 20:50:27 +0530

    Time: 396 ms (25%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -1,20 +1,12 @@
-# Write your MySQL query statement below
-
-select c.visited_on,
-(
-    select sum(amount)
+with cte as (
+    select visited_on,
+    sum(amount) as daily_amount
     from Customer
-    where visited_on between date_sub(c.visited_on,interval 6 day) and c.visited_on
-) as amount,
-round((
-    select sum(amount)/7
-    from Customer
-    where visited_on between date_sub(c.visited_on,interval 6 day) and c.visited_on
-),2) as average_amount
-from Customer c
-where c.visited_on >= 
-(
-    select date_add(min(visited_on),interval 6 day) from Customer
+    group by visited_on
 )
-group by c.visited_on
-order by c.visited_on;
+
+select visited_on,
+    sum(daily_amount) over(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) as amount,
+    round(avg(daily_amount) over(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2) as average_amount
+from cte 
+limit 1000 offset 6
