1a99cbcb23352049908b709f6be3f8b6af094495
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-06-10 12:11:02 +0530

    Time: 1633 ms (31.99%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,32 +1,20 @@
 # Write your MySQL query statement below
-
-select category, accounts_count
-from 
-(
-
-select 'Low Salary' as category,count(*) as accounts_count
+select 'Low Salary' as category,
+count(*) as accounts_count
 from Accounts
 where income < 20000
 
-union 
+union
 
-
-select 'Average Salary' as category,count(*) as accounts_count
+select 'Average Salary' as category,
+count(*) as accounts_count
 from Accounts
 where income between 20000 and 50000
 
 union
 
-select 'High Salary' as category,count(*) as accounts_count
+select 'High Salary' as category,
+count(*) as accounts_count
 from Accounts
-where income > 50000
-) as q
-order by 
-case  
-    when category = 'High Salary' then 1
-    when category = 'Low Salary' then 2
-    when category = 'Average Salary' then 3
-    else 4
-end;
-
+where income > 50000;
 
